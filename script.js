6091a8541a6e928f74c80cb3a8dc81906363c858
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-09 16:54:30 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = function () {
+    let black = document.querySelector('.color');
+    black.classList = 'color selected';
+}
+
 function createTable() {
     for ( let i = 0; i < 25; i += 1) {
         let spot = document.createElement('li');
