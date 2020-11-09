488aed816605ab70599f1c73776604ba2c899f55
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-09 18:21:55 -0300

    fixando code climate

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-//preenchendo quadro de pixels
-let pixelBoard = document.getElementById('pixel-board')
+//  preenchendo quadro de pixels
+const pixelBoard = document.getElementById('pixel-board')
 
-for(let index = 0; index < 25; index +=1){
-    let pixelDiv = document.createElement('div');
-    pixelDiv.className = 'pixel';
+for (let index = 0; index < 25; index += 1) {
+  let pixelDiv = document.createElement('div');
+  pixelDiv.className = 'pixel';
 
-    pixelBoard.appendChild(pixelDiv)
+  pixelBoard.appendChild(pixelDiv)
 }
 
-//pintando
-let colorsArray = document.getElementsByClassName('color');
+//  pintando
+const colorsArray = document.getElementsByClassName('color');
 
-//classe selected
-//preto
+//  classe selected
+//  preto
 
-    colorsArray[0].addEventListener("click",function(){
-        if(colorsArray[1].className === "color red selected"){
-            colorsArray[1].className = 'color red'
+colorsArray[0].addEventListener("click",function() {
+  if (colorsArray[1].className === "color red selected") {
+    colorsArray[1].className = 'color red'
         }
-        if(colorsArray[2].className === "color green selected"){
-            colorsArray[2].className = 'color green'
+  if (colorsArray[2].className === "color green selected") {
+    colorsArray[2].className = 'color green'
         }
-        if(colorsArray[3].className === "color blue selected"){
-            colorsArray[3].className = 'color blue'
+  if (colorsArray[3].className === "color blue selected") {
+    colorsArray[3].className = 'color blue'
         }
-        colorsArray[0].className = 'color black selected'
+  colorsArray[0].className = 'color black selected'
     })
 
-//vermelho
+//  vermelho
 
-colorsArray[1].addEventListener("click",function(){
-    if(colorsArray[0].className === "color black selected"){
-        colorsArray[0].className = 'color black'
+colorsArray[1].addEventListener("click",function() {
+  if (colorsArray[0].className === "color black selected") {
+    colorsArray[0].className = 'color black'
     }
-    if(colorsArray[2].className === "color green selected"){
-        colorsArray[2].className = 'color green'
+  if (colorsArray[2].className === "color green selected") {
+    colorsArray[2].className = 'color green'
     }
-    if(colorsArray[3].className === "color blue selected"){
-        colorsArray[3].className = 'color blue'
+  if (colorsArray[3].className === "color blue selected") {
+    colorsArray[3].className = 'color blue'
     }
-    colorsArray[1].className = 'color red selected';
+  colorsArray[1].className = 'color red selected';
 })
 
-//verde
+//  verde
 
-colorsArray[2].addEventListener("click",function(){
-    if(colorsArray[0].className === "color black selected"){
-        colorsArray[0].className = 'color black'
+colorsArray[2].addEventListener("click",function() {
+  if (colorsArray[0].className === "color black selected") {
+    colorsArray[0].className = 'color black'
     }
-    if(colorsArray[1].className === "color red selected"){
-        colorsArray[1].className = 'color red'
+  if (colorsArray[1].className === "color red selected") {
+    colorsArray[1].className = 'color red'
     }
-    if(colorsArray[3].className === "color blue selected"){
-        colorsArray[3].className = 'color blue'
+  if (colorsArray[3].className === "color blue selected") {
+    colorsArray[3].className = 'color blue'
     }
-    colorsArray[2].className = 'color green selected';
+  colorsArray[2].className = 'color green selected';
 })
 
-//azul
+//  azul
 
-colorsArray[3].addEventListener("click",function(){
-    if(colorsArray[0].className === "color black selected"){
-        colorsArray[0].className = 'color black'
+colorsArray[3].addEventListener("click",function() {
+  if (colorsArray[0].className === "color black selected") {
+    colorsArray[0].className = 'color black'
     }
-    if(colorsArray[1].className === "color red selected"){
-        colorsArray[1].className = 'color red'
+  if (colorsArray[1].className === "color red selected") {
+    colorsArray[1].className = 'color red'
     }
-    if(colorsArray[2].className === "color green selected"){
-        colorsArray[2].className = 'color green'
+  if (colorsArray[2].className === "color green selected") {
+    colorsArray[2].className = 'color green'
     }
-    colorsArray[3].className = 'color blue selected';
+  colorsArray[3].className = 'color blue selected';
 })
 
-//colorindo os pixels
-let pixelsArray = document.getElementsByClassName('pixel');
-for(let index = 0; index < pixelsArray.length; index +=1){
-    pixelsArray[index].addEventListener('click', function(){
-      
-            var elem = document.getElementsByClassName('selected')[0];
-            var elemColor = window.getComputedStyle(elem, null).getPropertyValue("background-color");
+//  colorindo os pixels
+const pixelsArray = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixelsArray.length; index += 1) {
+  pixelsArray[index].addEventListener('click', function() {
+    var elem = document.getElementsByClassName('selected')[0];
+    var elemColor = window.getComputedStyle(elem, null).getPropertyValue("background-color");
         
-            pixelsArray[index].style.backgroundColor = elemColor;
+    pixelsArray[index].style.backgroundColor = elemColor;
     })
 }
 
-//bibliografia para resolver o requisito acima: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+//      bibliografia para resolver o requisito acima: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 
-let button = document.getElementById('clear-board');
+const button = document.getElementById('clear-board');
 button.addEventListener('click',function(){
-    for(let index = 0; index < pixelsArray.length; index +=1){
-        pixelsArray[index].style.backgroundColor = "white"
+  for (let index = 0; index < pixelsArray.length; index += 1) {
+    pixelsArray[index].style.backgroundColor = "white"
     }
 })
+
