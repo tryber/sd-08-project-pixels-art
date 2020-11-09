677a269ed8a7b4ee5deb866d7d76a304812a59b1
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-09 18:43:19 -0300

    funçao que adiciona cores as paletas e cria as paletas dinamicamente

---
+++ script.js
@@ -0,0 +1,24 @@
+// Creating color boxes without colors 
+function createColorPallete(n) {
+	const colorPaletteContainer = document.getElementById('color-palette');
+	
+	for (let index = 0; index < n; index += 1) {
+        const createColorElement = document.createElement('li');
+		createColorElement.className = "color";
+        createColorElement.innerHTML = "color" + (index + 1);
+        colorPaletteContainer.appendChild(createColorElement);
+	};
+};
+createColorPallete(4);
+
+// Coloring palette boxes
+function createColorPaletteColors() {
+    const colorPalette = document.querySelectorAll('.color');
+    const colorsArray = ['red', 'blue', 'green', 'yellow'];
+
+    for (let index = 0; index < colorPalette.length; index +=1) {
+        colorPalette[index].style.backgroundColor = colorsArray[index];
+        colorPalette[index].style.color = colorsArray[index];
+    };
+};
+createColorPaletteColors()
