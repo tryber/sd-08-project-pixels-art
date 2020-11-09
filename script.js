a4f1c92364096f0ed7c85ac67064cf6a8943aaee
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-09 17:45:54 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function addTable() {
-    let table = document.querySelector('.table');
+  let table = document.querySelector('.table');
 
-    for (let index = 0; index < 5; index += 1) {
-        for (let index2 = 0; index2 < 5; index2 += 1) {
-            let tableDiv = document.createElement('tr');
-            tableDiv.className = 'pixel';
-            table.appendChild(tableDiv);
-        }
+  for (let index = 0; index < 5; index += 1) {
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      let tableDiv = document.createElement('tr');
+        tableDiv.className = 'pixel';
+        table.appendChild(tableDiv);
     }
+  }
 }
 
 addTable();
