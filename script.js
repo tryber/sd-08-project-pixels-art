0efa2c227e842635cf5317780d684272955a8b21
Rosiele David <rosieleserpa@hotmail.com>
2020-11-09 15:53:36 -0300

    requisito 2: adiciona paleta de cores

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function title() {
 }
 
 title();
+
+const paleteList = document.querySelector('#color-palette');
+
+function addItemPalette() {
+  for (let index = 0; index < 4; index += 1) {
+    const colorItem = document.createElement('li');
+    colorItem.className = 'color';
+    paleteList.appendChild(colorItem);
+  }
+}
+
+addItemPalette();
+
+const itemPalette = document.querySelectorAll('.color');
+
+function addColorPalette(colors) {
+  for (let index = 0; index < itemPalette.length; index += 1) {
+    const color = itemPalette[index];
+    color.style.backgroundColor = colors[index];
+  }
+}
+
+addColorPalette(['black', 'red', 'green', 'orange']);
