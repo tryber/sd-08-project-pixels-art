af652f53cb1c97d7fabe48e75d731c07d44d4f9f
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-09 18:46:17 -0300

    Adição do requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // Paleta de cores - Adicionando as cores de forma dinâmica para evitar encadeamento de classes
-let paleta = document.querySelectorAll('.color');
-paleta[0].style.backgroundColor = 'black';
-paleta[1].style.backgroundColor = '#f7be4a';
-paleta[2].style.backgroundColor = '#ab8333';
-paleta[3].style.backgroundColor = '#6b5220';
+function addPalletCOlor() {
+    let paleta = document.querySelectorAll('.color');
+    paleta[0].style.backgroundColor = 'black';
+    paleta[1].style.backgroundColor = '#f7be4a';
+    paleta[2].style.backgroundColor = '#ab8333';
+    paleta[3].style.backgroundColor = '#6b5220';
+}
+// Adicionando eventos para selecionar a cor da paleta
+
+let p = document.getElementsByClassName('color')
+for (let i = 0; i<=3; i += 1) {
+    p[i].addEventListener('click',function() {
+    p[i].classList.add('selected');
+    indexClicked = i;
+    for(j = 0; j<=3; j += 1) {
+        if(j != indexClicked) {
+            p[j].className = 'color';
+        }
+    } 
+    let color = p[indexClicked].style.backgroundColor;
+    });
+}
+
+
 
 // criando quadro de pixels
 
 let pixelBoard = document.getElementById('pixel-board');
-function generatePixel(){
+function generatePixel() {
     for( let j = 0; j < 25; j += 1) {
         let colorizedPixel = document.createElement('div');
         colorizedPixel.className = 'pixel';
@@ -17,11 +36,10 @@ function generatePixel(){
 }
 
 
-// Adicionando eventos para selecionar cor da paleta
 
 
 
 
 
-
+addPalletCOlor();
 generatePixel();
