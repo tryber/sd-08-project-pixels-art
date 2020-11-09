a8692e5dc83631f456abb52273ba60ff5675a32b
Lucas Gama <lucasbg@protonmail.com>
2020-11-09 15:29:06 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,17 @@
+let pxBoard = document.getElementById("pixel-board");
+let size = 5;
+
+function fillBoard() {
+  for (let row = 0; row < size; row += 1) {
+    let linha = document.createElement("ul");
+    for (let column = 0; column < size; column += 1) {
+      let column = document.createElement("li");
+      column.className = "pixel";
+      column.style.backgroundColor = "white";
+      linha.appendChild(column);
+    }
+    pxBoard.appendChild(linha);
+  }
+}
+
+fillBoard();
