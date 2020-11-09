10b6d89cf641152435b750d1e858152cf4e4dea0
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2020-11-09 17:57:52 -0300

    requisito 4 - adiciona quadro de pixel 5 por 5

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const palleteColorsList = document.querySelector('#color-palette').firstElementChild;
-let colorArray = ['black', 'red', 'green', 'blue', 'yellow'];
-for (let i = 0; i < colorArray.length; i += 1) {
-  let colorElement = document.createElement('li');
-  colorElement.className = 'color';
-  colorElement.style.backgroundColor = colorArray[i];
-  palleteColorsList.appendChild(colorElement);
+function createPalette() {
+  const palleteColorsList = document.querySelector('#color-palette');
+  const colorArray = ['black', 'red', 'green', 'blue'];
+  for (let i = 0; i < colorArray.length; i += 1) {
+    const colorElement = document.createElement('li');
+    colorElement.className = 'color';
+    colorElement.style.backgroundColor = colorArray[i];
+    palleteColorsList.appendChild(colorElement);
+  }
 }
+
+createPalette();
+
+const board = document.querySelector('#pixel-board');
+for(let index = 1; index <= 5; index += 1) {
+  const lineBoardElement = document.createElement('div');
+  lineBoardElement.id = `line ${index}`;
+  lineBoardElement.className = 'lines';
+
+  for (let i = 0; i < 5; i += 1) {
+    const squarePixel = document.createElement('div');
+    squarePixel.className = 'pixel';
+    lineBoardElement.appendChild(squarePixel);
+  }
+
+  board.appendChild(lineBoardElement);
+}
+
+
+
