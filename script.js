3a988848f7ebcec87b1fbfdb0da4d4bb83789fb5
Victor Breno <victor.brns2@gmail.com>
2020-11-09 17:40:54 -0300

    conclusão dos requesitos 6 e 7

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Função para definir a cor selecionada e atribuir a classe 'color' para a que tinha a classe 'selected'.
+function selecionado (botaoSelecionado) {
+  let antigoSelecionado = document.querySelector('.selected');
+  antigoSelecionado.className = 'color';
+  botaoSelecionado.target.classList.remove('color');
+  botaoSelecionado.target.classList.add('selected');
+}
+
+
+
 //Criando quadrados modelo de cores para paleta.
 let paletaDeCor = document.getElementById('color-palette');
 let arrayDeCores = ['green', 'yellow', 'blue'];
@@ -11,12 +21,14 @@ for (let index = 0; index < 4; index += 1) {
   corDaPaleta.style.borderColor = ('black');
   if (index == 0){
     corDaPaleta.style.backgroundColor = ('black');
+    corDaPaleta.className = ('selected');
   } else {
     corDaPaleta.style.backgroundColor = (arrayDeCores[index - 1]);
   } 
   corDaPaleta.style.width = ('40px');
   corDaPaleta.style.height = ('40px');
   corDaPaleta.style.margin = ('5px');
+  corDaPaleta.addEventListener('click', selecionado);
   paletaDeCor.appendChild(corDaPaleta);
 }
 
@@ -30,7 +42,7 @@ for (let primeiroIndex = 0; primeiroIndex < 5; primeiroIndex += 1) {
   for (let segundoIndex = 0; segundoIndex < 5; segundoIndex += 1) {
     let celulaLinhaTabela = document.createElement('td');
     celulaLinhaTabela.className = ('pixel');
-    celulaLinhaTabela.style.backgroundColor = ('yellow');
+    celulaLinhaTabela.style.backgroundColor = ('white');
     celulaLinhaTabela.style.border = ('solid');
     celulaLinhaTabela.style.borderWidth = ('1px');
     celulaLinhaTabela.style.width = ('40px');
