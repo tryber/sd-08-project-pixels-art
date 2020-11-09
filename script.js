30dc36d51aa767017fe94db2ad6336a4efd540b6
Tandy <tandyfw@gmail.com>
2020-11-09 15:09:11 -0300

    Requisitos 4 e 5 concluidos

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ function initialColors () {
   colors[2].style.backgroundColor = 'yellow';
   colors[3].style.backgroundColor = 'green';
 }
-
 initialColors();
+
+function pixelBoard () {
+  let divBoard = document.querySelector('#pixel-board');
+  for (let index = 1; index < 26; index += 1) {
+    let divPixel = document.createElement('div');
+    divPixel.className = 'pixel';
+    divBoard.appendChild(divPixel);
+  }
+}
+pixelBoard();
