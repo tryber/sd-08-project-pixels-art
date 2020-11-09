92b61857b0f368d9e0a49005d7cf2968dba32dac
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2020-11-09 16:27:49 -0400

    Adicionado parte de selecionar

--- script.js
+++ script.js
@@ -30,3 +30,36 @@ function createGrade () {
 }
 createGrade()
 
+
+
+function selectColor(){
+  var pixels = document.querySelectorAll('.color')
+
+  for( i = 0; i < pixels.length; i++){
+    selected = Boolean;
+    pixels[i].addEventListener('click', (event)=>{
+      if(event.target.className === 'color selected'){
+        event.target.className = 'color'
+        selected = false
+      }else if(event.target.className === 'color' && selected == false){
+        event.target.classList.toggle('selected')
+        selected = true
+        wathColor()
+      }
+    })
+
+    function wathColor(cor){
+      for( i = 0; i < pixels.length; i++){
+      if(pixels[i].className === 'color selected'){
+        var cor = pixels[i].style.backgroundColor
+        return cor
+      }
+    }}
+  }
+  return cor = wathColor()
+}
+selectColor()
+
+function paint(){
+
+}
