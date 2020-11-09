b2285ee0babdc09e1eee46c594271089262fd063
Pedro Alves <ped.alm2709@gmail.com>
2020-11-09 18:57:28 -0300

    tentativa requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-.
+function createDiv() {
+  let pallete = document.getElementById('color-palette');
+
+  for (let i = 1; i < 5; i+=1) {
+    let colorDiv = document.createElement('div');
+    colorDiv.className = 'color';
+    pallete.appendChild(colorDiv);
+  }
+}
+createDiv();
+
+
+
+
+  
