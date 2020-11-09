133018a937b1e84c04c7cd76ae317b5424a269ee
Max <gmaxgomes@gmail.com>
2020-11-09 15:18:47 -0300

    Requisitos 1, 2 e 3

---
+++ index.js
@@ -0,0 +1,17 @@
+function setColorTwo(color) {
+  let colorTwo = document.querySelectorAll('.color')[1];
+  colorTwo.style.backgroundColor = color;
+}
+setColorTwo('rgb(15, 163, 107)');
+
+function setColorThree(color) {
+  let colorThree = document.querySelectorAll('.color')[2];
+  colorThree.style.backgroundColor = color;
+}
+setColorThree('yellow');
+
+function setColorFour(color) {
+  let colorFour = document.querySelectorAll('.color')[3];
+  colorFour.style.backgroundColor = color;
+}
+setColorFour('red');
