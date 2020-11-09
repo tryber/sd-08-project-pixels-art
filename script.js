31d1e4d620126f12d2e225a7c8015844b36411c5
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-09 18:09:19 -0300

    alteração 2.0 requisito 5

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function addTable() {
-  let table = document.querySelector('.table');
+  const table = document.querySelector('#pixel-board');
 
   for (let index = 0; index < 5; index += 1) {
     for (let index2 = 0; index2 < 5; index2 += 1) {
-      let tableDiv = document.createElement('tr');
-        tableDiv.className = 'pixel';
-        table.appendChild(tableDiv);
+      const tableDiv = document.createElement('tr');
+      tableDiv.className = 'pixel';
+      table.appendChild(tableDiv);
     }
   }
 }
