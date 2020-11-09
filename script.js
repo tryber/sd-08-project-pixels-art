6daf36b91685774ba87bbbbeaf5322f47b59edf1
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-09 20:52:09 -0300

    verification of hipoteses

--- script.js
+++ script.js
@@ -2,22 +2,25 @@
 function toggleColor() {
 
 
-
     const blackCC = document.querySelector ('#black')
     blackCC.className = "color selected"
 
-   window.onload = blackCC.addEventListener('click', function (event) {
-    const selected = document.querySelector('.selected')
 
-    if (selected === null ) {
-    event.target.className ="color selected"
+//    window.onload = blackCC.addEventListener('click', function (event) {
+//     const selected = document.querySelector('.selected')
+
+//     if (selected === null ) {
+//     event.target.className ="color selected"
     
-} else {
-    event.target.className = "color" 
+// } else {
+//     event.target.className = "color" 
+// }
+
+//     }
+
+//     )}
 }
 
-    }
-
-    )}
-
   window.onload = toggleColor();
+
+
