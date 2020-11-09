59fe69cdbc136c93f8f041e7819a3dd7c2b2960e
Blucilha <blucilha.nascimento@gmail.com>
2020-11-09 16:12:17 -0300

    Todas as cores da paleta sao diferentes

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.getElementById("preta").style.backgroundColor = "black";
-
+document.getElementById("amarelo").style.backgroundColor = "yellow";
+document.getElementById("cinza").style.backgroundColor = "grey";
+document.getElementById("vermelho").style.backgroundColor = "red";
