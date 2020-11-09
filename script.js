9799927dbe0cfc7ba68000b3b92bfa1425edb328
emassaki <massakihirayama@gmail.com>
2020-11-09 16:20:23 -0300

    Arrumando os requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Cria a paleta de cores
 const colorPaletteContainer = document.getElementById('color-palette');
 const colors = ['black', 'red', 'green', 'blue'];
-for (let paletteColor = 0; paletteColor < colors.length; paletteColor += 1) {
+for (let color = 0; color < colors.length; color += 1) {
   const colorPalette = document.createElement('div');
   colorPalette.className = 'color';
-  colorPalette.style.backgroundColor = colors[paletteColor];
+  if (colors[color] === 'black') {
+    colorPalette.className = 'color selected';
+  }
+  colorPalette.style.backgroundColor = colors[color];
   colorPaletteContainer.appendChild(colorPalette);
 }
 
@@ -15,9 +18,9 @@ for (let row = 0; row < boardSize; row += 1) {
   const pixelRow = document.createElement('div');
   pixelRow.className = 'pixel-row';
   pixelBoard.appendChild(pixelRow);
-  let pixelCells = document.getElementsByClassName('pixel-row');
+  const pixelCells = document.getElementsByClassName('pixel-row');
   for (let cell = 0; cell < boardSize; cell += 1) {
-    let pixelCellContainer = pixelCells[row];
+    const pixelCellContainer = pixelCells[row];
     const pixelCell = document.createElement('div');
     pixelCell.className = 'pixel';
     pixelCellContainer.appendChild(pixelCell);
