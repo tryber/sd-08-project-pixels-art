da4d0e8ca30b01de7c8f8df80dccb6066e666521
jnpefo <jnpefo@gmail.com>
2020-11-09 20:18:10 -0300

    Pausa

---
+++ script.js
@@ -0,0 +1,8 @@
+let selecionado = document.querySelectorAll(".color");
+
+for (let i = 0; i <selecionado.length; i += 1){
+    let  atual = selecionado[i];
+    if(atual.className("selecte")){
+
+    }
+}
