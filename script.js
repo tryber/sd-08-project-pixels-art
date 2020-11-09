84910edc3936cfb84a9a5ca1145a6c81578e2d24
Tiago Lunardi <tiagaoalb@gmail.com>
2020-11-09 19:20:36 -0300

    updated script and style

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ let getColor = document.getElementsByClassName('color');
 let blackColor = document.getElementById('black');
 
 let createPixelBoard = () => {
-  for (let i = 0; i < 25; i += 1) {
+  for (let i = 0; i < 5; i += 1) {
     const createPixel = document.createElement('div');
-    createPixel.className = 'pixel';
     getPixelBoard.appendChild(createPixel);
+    for (let j = 0; j < 5; j += 1) {
+      const createPixel = document.createElement('div');
+      getPixelBoard.appendChild(createPixel);
+      createPixel.className = 'pixel';
+    }
   }
 };
 createPixelBoard();
 
 let setBlackColor = () => {
   blackColor.classList.add('selected');
-}
+};
 setBlackColor();
