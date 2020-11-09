fabd7f0caab3de7296f9848ed63c409fdc4a8a80
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-09 17:50:23 -0300

    adicionando requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-function coresPaleta () {
+
+// Requisitos 2 e 3
+
 const elementosPaleta = document.getElementsByClassName("color");
 
-elementosPaleta[0].style.backgroundColor = "black";
-elementosPaleta[1].style.backgroundColor = "orange";
-elementosPaleta[2].style.backgroundColor = "yellow";
-elementosPaleta[3].style.backgroundColor = "cyan";
+for (index = 0; index < elementosPaleta.length; index += 1) {
+  const coresDisponiveis = ["black", "purple", "blue", "cyan"];
+
+  for (cont = 0; cont < coresDisponiveis.length; cont +=1) {
+    elementosPaleta[index].style.backgroundColor = coresDisponiveis[index];
+  }
 }
 
-coresPaleta ();
+
+// Requisitos 4 e 5
+const n = 5;
+const board = document.getElementById("pixel-board");
+
+  for (let index = 0; index < n; index += 1) {
+    let addLines = document.createElement("div");
+
+    addLines.className = "lines";
+    board.appendChild(addLines);
+
+    const boardElements = document.getElementsByClassName("lines");
+
+    for (let cont = 0; cont < n; cont += 1) {
+      const spacesContainer = boardElements[index];
+      let boardSpace = document.createElement("div");
+      boardSpace.className = "pixel";
+
+      spacesContainer.appendChild(boardSpace);
+      }
+
+    }
+
+
+
