901c2769bb3b4a5217de7ad4e16d3befaa3cc0b7
Luiz Reina <luizreeina@gmail.com>
2020-11-09 15:54:05 -0300

    corrigindo codeclimate

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-let cores = ['#000000']
-geraPaletaDeCores()
+let cores = ['#000000'];
+geraPaletaDeCores();
 
 function geraPaletaDeCores() {
-  let paletaDeCores = document.getElementById('color-palette')
+  let paletaDeCores = document.getElementById('color-palette');
 
   for (let index = 0; index < 4; index += 1) {
-    let divCor = document.createElement('div')
-    divCor.className = 'color'
-    paletaDeCores.append(divCor)
+    const divCor = document.createElement('div');
+    divCor.className = 'color';
+    paletaDeCores.append(divCor);
   }
 
-  geraCores()
+  geraCores();
 }
 
 function geraCores() {
   
-  let cor = document.querySelectorAll('#color-palette .color')
+  const cor = document.querySelectorAll('#color-palette .color');
 
-  cor[0].style.backgroundColor = '#000000'
-  cor[0].className = 'selected color'
+  cor[0].style.backgroundColor = '#000000';
+  cor[0].className = 'selected color';
 
   for (let index = 1; index < 4; index += 1) {
-    cor[index].style.backgroundColor = '#' + (Math.floor(Math.random()*16777215).toString(16))
-    let cor2 = cor[index].style.backgroundColor
-    cores.push(cor2)
+    cor[index].style.backgroundColor = '#' + (Math.floor(Math.random()*16777215).toString(16));
+    let cor2 = cor[index].style.backgroundColor;
+    cores.push(cor2);
   }
 
-  evitaCorRepetida()
+  evitaCorRepetida();
 }
 
 function evitaCorRepetida() {
   for (let index = 0; index < cores.length; index += 1) {
-    let pos = index + 1
+    let pos = index + 1;
 
     for (pos; pos < cores.length; pos += 1) {
       if (cores[index] == cores[pos] || cores[index] == '#FFFFFF' || cores[index] == '') {
-        geraCores()
+        geraCores();
       }
     }
   }
 }
 
-geraPixelBoard()
+geraPixelBoard();
 
 function geraPixelBoard() {
-  let pixelBoard = document.getElementById('pixel-board')
+  const pixelBoard = document.getElementById('pixel-board');
 
   for (let index = 0; index < 5; index += 1) { 
-    let newTr = document.createElement('tr')     
-    pixelBoard.append(newTr)
-    
+    const newTr = document.createElement('tr');   
+    pixelBoard.append(newTr);
+
     for (let index = 0; index < 5; index += 1) {
-      let pixel = document.createElement('div')
-      pixel.className = 'pixel'
-      pixel.style.backgroundColor = '#FFFFFF'
-      pixelBoard.lastChild.append(pixel)
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.style.backgroundColor = '#FFFFFF';
+      pixelBoard.lastChild.append(pixel);
     }
   }
 }
