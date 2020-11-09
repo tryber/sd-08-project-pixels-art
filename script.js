adee9f6b09e8cdbc113ecd759137565d0d309bf9
victor Farias <victorfarias.tv@gmail.com>
2020-11-09 17:03:56 -0300

    Adicionado setimo requisito do projeto

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 window.onload = function() {
-  let pixelInicial = document.querySelector('.black');
-  pixelInicial.className += " selected"
+  let pixelInicial = document.querySelector('#black');
+  pixelInicial.className = "color selected";
+  pixelClass()
+};
+
+function pixelClass() {
+  let pixelColor = document.querySelectorAll('.color');
+  for (let i = 0; i < pixelColor.length; i += 1) {
+    pixelColor[i].addEventListener('click', function(event) {
+      let pixelSelected = document.querySelector(".color.selected");
+      pixelSelected.className = "color";
+      event.target.className = "color selected";
+    })
+  }
 }
+  
