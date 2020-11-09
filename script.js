804aef0d38b40c1e9d2ea49cc1b397caaae6409a
MuriloBatista <git@github.com:MuriloBatista>
2020-11-09 20:44:35 -0300

    Adiciona 7º requisito

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+//Pagina inicia com a cor black selecionada
 const corIni = document.querySelectorAll('.color');
 corIni[0].classList.add('selected');
 
+//Add listener para paleta de cores
+const paleta = document.getElementById('color-palette');
+paleta.addEventListener('click', setColor);
 
- 
+//Remove seletor
+function selectionRem() {
+    for (let index = 0; index < corIni.length; index += 1) {
+      corIni[index].className = "color";
+    }
+}
+
+//Adiciona seletor
+function setColor(event) {
+    selectionRem();
+    event.target.className = "color selected";
+}
