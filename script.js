13aa8e69df1ec3d0a67571ccc5c7d7233b6ecfde
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-09 18:58:17 -0300

    60 issues restantes

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ for (let index = 0; index < 25; index += 1) {
   const pixelDiv = document.createElement('div');
   pixelDiv.className = 'pixel';
 
-  pixelBoard.appendChild(pixelDiv)
-};
+  pixelBoard.appendChild(pixelDiv);
+}
 
 //  pintando
 const colorsArray = document.getElementsByClassName('color');
@@ -14,7 +14,7 @@ const colorsArray = document.getElementsByClassName('color');
 //  classe selected
 //  preto
 
-colorsArray[0].addEventListener('click', function() {
+colorsArray[0].addEventListener('click', function () {
   if (colorsArray[1].className === 'color red selected') {
     colorsArray[1].className = 'color red';
   }
@@ -25,12 +25,12 @@ colorsArray[0].addEventListener('click', function() {
     colorsArray[3].className = 'color blue';
   }
   colorsArray[0].className = 'color black selected';
-  }
-)
+}
+);
 
 //  vermelho
 
-colorsArray[1].addEventListener('click', function() {
+colorsArray[1].addEventListener('click', function () {
   if (colorsArray[0].className === 'color black selected') {
     colorsArray[0].className = 'color black';
   }
@@ -41,12 +41,12 @@ colorsArray[1].addEventListener('click', function() {
     colorsArray[3].className = 'color blue';
   }
   colorsArray[1].className = 'color red selected';
-  }
-)
+}
+);
 
 //  verde
 
-colorsArray[2].addEventListener('click', function() {
+colorsArray[2].addEventListener('click', function () {
   if (colorsArray[0].className === 'color black selected') {
     colorsArray[0].className = 'color black';
   }
@@ -57,40 +57,40 @@ colorsArray[2].addEventListener('click', function() {
     colorsArray[3].className = 'color blue';
   }
   colorsArray[2].className = 'color green selected';
-  }
-)
+}
+);
 
 //  azul
 
-colorsArray[3].addEventListener('click', function() {
+colorsArray[3].addEventListener('click', function () {
   if (colorsArray[0].className === 'color black selected') {
     colorsArray[0].className = 'color black';
   }
   if (colorsArray[1].className === 'color red selected') {
     colorsArray[1].className = 'color red';
   }
-  if (colorsArray[2].className === "color green selected") {
+  if (colorsArray[2].className === 'color green selected') {
     colorsArray[2].className = 'color green';
   }
   colorsArray[3].className = 'color blue selected';
-  }
-)
+}
+);
 
 //  colorindo os pixels
 const pixelsArray = document.getElementsByClassName('pixel');
 for (let index = 0; index < pixelsArray.length; index += 1) {
-  pixelsArray[index].addEventListener('click', function() {
-    let elem = document.getElementsByClassName('selected')[0];
-    let elemColor = window.getComputedStyle(elem, null).getPropertyValue('background-color');
+  pixelsArray[index].addEventListener('click', function () {
+    const elem = document.getElementsByClassName('selected')[0];
+    const elemColor = window.getComputedStyle(elem, null).getPropertyValue('background-color');
     pixelsArray[index].style.backgroundColor = elemColor;
-    });
-};
+  })
+}
 
 //      bibliografia para resolver o requisito acima: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 
 const button = document.getElementById('clear-board');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   for (let index = 0; index < pixelsArray.length; index += 1) {
     pixelsArray[index].style.backgroundColor = 'white';
-    }
-});
+  }
+})
