a4a684b167eb6f7b0c59df5a2f0c09858f7ff2ff
MuriloBatista <git@github.com:MuriloBatista>
2020-11-09 18:13:28 -0300

    Adiciona 5º e 6º requisitos

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
+const corIni = document.querySelectorAll('.color');
+corIni[0].classList.add('selected');
 
-function iniciar() {
-        
-    
-   let colorDef = document.getElementsByClassName('color');
 
-    for(let index = 0; index < colorDef.length; index += 1){
-        switch (index){
-            case '0':
-                colorDef[index].style = "background-color: black;";
-                break;
-            case '1':
-                colorDef[index].style = "background-color: red;";
-                break;
-            case '2':
-               colorDef[index].style = "background-color: green;";
-               break;
-            case '3':
-                colorDef[index].style = "background-color: yellow;";
-                break;
-            }
-
-    }
-}
-iniciar();
+ 
