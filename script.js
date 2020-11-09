109686d5b90453476c75c2f92751a4c72d79f400
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2020-11-09 18:07:43 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colors = ['black', 'red', 'green', 'blue'];
 
-colors.forEach((color, index) => {
+colors.forEach((color) => {
     const paletteColors = document.createElement('div');
 
     paletteColors.className = 'color';
@@ -8,3 +8,22 @@ colors.forEach((color, index) => {
 
     document.querySelector('#color-palette').appendChild(paletteColors);
 });
+
+const pixelsLineAndColumn = [5, 5];
+
+for (let lineNumber = 1; lineNumber <= pixelsLineAndColumn[0]; lineNumber += 1) {
+    const line = document.createElement('div');
+
+    line.className = 'line';
+
+    for (let columnNumber = 1; columnNumber <= pixelsLineAndColumn[1]; columnNumber += 1) {
+        const column = document.createElement('div');
+
+        column.className = 'pixel';
+        column.style.backgroundColor = 'white';
+
+        line.appendChild(column);
+    }
+
+    document.querySelector('#pixel-board').appendChild(line);
+}
