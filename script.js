cfb6963f8dd2a0bb148ac85ba3da650232a53ede
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-09 19:31:27 -0300

    remove funçao na criaçao das divs com java script

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function createDivs() {
     for (index = 0; index < 4; index += 1) {
     let colorPalette = document.getElementById('color-palette');
     let divColor = document.createElement('div');
     colorPalette.appendChild(divColor);
     divColor.className = 'color';
-}
-}
-createDivs();
+
 
 function createColor() {
 let palette = ["black", "blue", "green", "yellow"];
