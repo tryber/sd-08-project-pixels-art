f2f92b30cee63007943a1ea35035dc9369354978
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-09 18:20:31 -0300

    :rockt: Paleta e Tabelas prontas

---
+++ script.js
@@ -0,0 +1,8 @@
+// Paleta de cores 
+
+// cores da paleta 
+
+document.querySelector('#color-pallete').children[0].style.backgroundColor = 'black';
+document.querySelector('#color-pallete').children[1].style.backgroundColor = 'red';
+document.querySelector('#color-pallete').children[2].style.backgroundColor = 'blue';
+document.querySelector('#color-pallete').children[3].style.backgroundColor = 'yellow';
