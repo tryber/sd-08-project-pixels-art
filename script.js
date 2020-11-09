eb83a51b1546bde9eb6f37d7a41c9f43c6ed3e12
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-11-09 16:51:29 -0300

    Requisito 8 e 9Criando botão

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-var paleta = document.querySelector("#color-palette")
-console.log(paleta)
-
-paleta.addEventListener('click', function(event){
-  if (event.target.className !== 'color selected') { 
-    let paletaSelecionada = document.querySelector(".selected")    
+let paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', function (event){
+  if (event.target.className !== 'color selected') {
+    const paletaSelecionada = document.querySelector('.selected')
     event.target.className = 'color selected';
     paletaSelecionada.className = 'color';
   }
 } )
 
-let paletas = document.querySelectorAll('.color')
-console.log(paletas);
-let pixels = document.querySelector("#pixel-board");
-console.log(pixels);
-let teste = document.querySelector("#cor2")
-console.log(window.getComputedStyle(teste, null).getPropertyValue("background-color"));
-
-pixels.addEventListener('click', function(event){
+let paletas = document.querySelectorAll('.color');
+let pixels = document.querySelector('#pixel-board');
+let teste = document.querySelector('#cor2');
+pixels.addEventListener('click', function (event){
   for (i = 0; i< paletas.length; i++) {
     console.log(paletas[i].style.backgroundColor)
     if (paletas[i].className === 'color selected') {
       event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue("background-color")
     }
+  }
+})
+
+let pixelsList = document.querySelectorAll('.pixel');
+console.log(pixelsList);
+let botaoLimpeza = document.querySelector('#clear-board');
+
+botaoLimpeza.addEventListener('click', function () {
+  for (i=0; i<pixelsList.length; i++) {
+    pixelsList[i].style.backgroundColor = 'white'
   }
 })
