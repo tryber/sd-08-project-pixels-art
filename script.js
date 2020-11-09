390232a9f5997b501da82b9106582327f374e328
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-09 17:21:38 -0300

    Sending requirement 7

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 window.onload = function () {
   const pixel = document.getElementsByClassName('color');
-  pixel[0].classList.add('selected');
+  const colors = ['black', 'blue', 'red', 'yellow'];
+
+  function setupColorPalette() {
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].style.backgroundColor = colors[i];
+    }
+  }
+  setupColorPalette();
+
+  function selectColor(color) {
+    for (let i = 0; i < pixel.length; i += 1) {
+      if (pixel[i].style.backgroundColor === color) {
+        pixel[i].classList.add('selected');
+      }
+    }
+  }
+  selectColor('black');
+
+  function eventPalette() {
+    for (let i = 0; i < pixel.length; i += 1) {
+      pixel[i].addEventListener('click', function () {
+        const colorSelected = document.querySelector('.selected');
+        colorSelected.classList.remove('selected');
+        pixel[i].classList.add('selected');
+      });
+    }
+  }
+  eventPalette();
 };
