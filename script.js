8991d003c54c68ec3a5857828d02cf118dc0350f
emassaki <massakihirayama@gmail.com>
2020-11-09 15:57:19 -0300

    Adicionando requisitos 3 e 4 - cores da paleta e o quadro de pixels

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let colorPaletteContainer = document.getElementById('color-palette');
-let colors = ['black', 'red', 'green', 'blue'];
+// Cria a paleta de cores
+let colorPaletteContainer = document.getElementById("color-palette");
+let colors = ["black", "red", "green", "blue"];
 for (let backColor of colors) {
-  let colorPalette = document.createElement('div');
-  colorPalette.className = 'color';
+  let colorPalette = document.createElement("div");
+  colorPalette.className = "color";
   colorPalette.style.backgroundColor = backColor;
-  colorPaletteContainer.appendChild(colorPalette)
+  colorPaletteContainer.appendChild(colorPalette);
+}
+
+// Cria o quadro de pixels
+let boardSize = 5;
+let pixelBoard = document.getElementById('pixel-board');
+for (let row = 0; row < boardSize; row += 1) {
+  let pixelRow = document.createElement('div');
+  pixelRow.className = 'pixel-row';
+  pixelBoard.appendChild(pixelRow);
+  let pixelCells = document.getElementsByClassName('pixel-row');
+  for (let cell = 0; cell < boardSize; cell += 1) {
+    let pixelCellContainer = pixelCells[row];
+    let pixelCell = document.createElement('div');
+    pixelCell.className = 'pixel';
+    pixelCellContainer.appendChild(pixelCell);
+  }
 }
