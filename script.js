953d54ad6db5a0915150fdfca5b643f9e8628791
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2020-11-09 20:54:53 -0300

    adding requisit four

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //creation of the divs in the color palette
 function createDivColor() {
-  const divColor = document.getElementById("color-palette");
+  let divColor = document.getElementById("color-palette");
   for (let i = 0; i < 4; i += 1) {
     let divChild = document.createElement("div");
     divChild.className = "color";
@@ -14,3 +14,17 @@ function createDivColor() {
   divChild[3].style.background = "green";
 }
 createDivColor();
+//creation of the pixels in the pixels board
+function createSquaresBox (){
+    const squareBox = document.getElementById('pixel-board');
+for (let i = 0; i < 5; i += 1) { // number of lines
+  const row = document.createElement('div');
+  for (let j = 0; j < 5; j += 1) { // number of pixels
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    row.appendChild(pixel);//Pixel são filhos da linha.
+  }
+  squareBox.appendChild(row);//As linhas são filhas do Pixel-board.
+}
+}
+createSquaresBox();
