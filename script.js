cf69c8ebb7c5e41fe69191ed2e454346f6b913f4
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-11-09 19:27:19 -0300

    code climate

--- script.js
+++ script.js
@@ -1,57 +1,21 @@
-window.onload = () => {
-  let board = document.getElementById("pixel-board");
-  let palette = document.getElementById("color-palette");
-  let clearButton = document.getElementById("clear-board");
-  let updateSizeButton = document.getElementById("generate-board");
-
-  let size = 5;
-
-  generatePalette(palette);
-  generateBoard(size, board);
-
-  clearButton.addEventListener("click", event => {
-    let pixels = document.getElementsByClassName("pixel");
-
-    for (pix of pixels) {
-      pix.style.backgroundColor = "white";
-    }
-  });
-
-  updateSizeButton.addEventListener("click", event => {
-    let newSize = document.getElementById("board-size").value;
-
-    if (!newSize) {
-      alert("Board inválido!");
-    } else {
-      board.innerHTML = "";
-      if (newSize < 5) {
-        newSize = 5;
-      } else if (newSize > 50) {
-        newSize = 50;
-      }
-      generateBoard(newSize, board);
-    }
-  });
-}
-
 function generatePalette(palette) {
   for (let i = 0; i < 4; i += 1) {
-    let color = document.createElement("div");
-    color.addEventListener("click", event => {
-      for (colPixel of palette.children) {
-        colPixel.className = "color";
+    let color = document.createElement('div');
+    color.addEventListener('click', (event) => {
+      for (let colPixel of palette.children) {
+        colPixel.className = 'color';
       }
-      event.target.className = "color selected";
+      event.target.className = 'color selected';
     });
 
     if (palette.children.length === 0) {
-      color.className = "color selected";
-      color.style.backgroundColor = "black";
+      color.className = 'color selected';
+      color.style.backgroundColor = 'black';
       palette.appendChild(color);
     } else {
       let colGen = generateRandomColor();
 
-      color.className = "color";
+      color.className = 'color';
       color.style.backgroundColor = `rgb(${colGen})`;
       palette.appendChild(color);
     }
@@ -67,17 +31,17 @@ function generateBoard(size, board) {
 
 function createLine(size) {
   let currentColor;
-  let line = document.createElement("tr");
-  line.className = "pixel-line";
+  let line = document.createElement('tr');
+  line.className = 'pixel-line';
 
   for (let i = 0; i < size; i += 1) {
-    let pixel = document.createElement("td");
-    pixel.className = "pixel";
-    pixel.style.backgroundColor = "white";
-    pixel.addEventListener('click', event => {
-      currentColor = document.getElementsByClassName("selected")[0].style.backgroundColor;
+    let pixel = document.createElement('td');
+    pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
+    pixel.addEventListener('click', (event) => {
+      currentColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
       event.target.style.backgroundColor = currentColor;
-    })
+    });
 
     line.appendChild(pixel);
   }
@@ -88,4 +52,38 @@ function createLine(size) {
 function generateRandomColor() {
   return `${Math.ceil(Math.random() * 254)} , ${Math.ceil(Math.random() * 254)}
    , ${Math.ceil(Math.random() * 254)}`;
+}
+
+window.onload = () => {
+  const board = document.getElementById('pixel-board');
+  const palette = document.getElementById('color-palette');
+  const clearButton = document.getElementById('clear-board');
+  const updateSizeButton = document.getElementById('generate-board');
+  const size = 5;
+
+  generatePalette(palette);
+  generateBoard(size, board);
+
+  clearButton.addEventListener('click', () => {
+    const pixels = document.getElementsByClassName('pixel');
+
+    for (let pix of pixels) {
+      pix.style.backgroundColor = 'white';
+    }
+  });
+  updateSizeButton.addEventListener('click', () => {
+    let newSize = document.getElementById('board-size').value;
+
+    if (!newSize) {
+      alert('Board inválido!');
+    } else {
+      board.innerHTML = '';
+      if (newSize < 5) {
+        newSize = 5;
+      } else if (newSize > 50) {
+        newSize = 50;
+      }
+      generateBoard(newSize, board);
+    }
+  });
 }
