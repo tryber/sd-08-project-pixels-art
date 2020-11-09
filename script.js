08a691083892d5be34606b207b90a5efb59ee132
Silvio Dayube <scarige@gmail.com>
2020-11-09 16:26:16 -0300

    Refactoring to add color definition to JS
    Also temporarily removes window.onload

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-window.onload = function () {
-  const blackColor = document.querySelector('.black');
-  blackColor.classList.add('selected');
-};
+// Sets reference select color picker divs
+const blackColor = document.querySelector('.black');
+const firstColor = document.querySelector('.first');
+const secondColor = document.querySelector('.second');
+const thirdColor = document.querySelector('.third');
+
+// Sets a nodeList wth all color picker divs
+const colorList = document.querySelectorAll('.color');
+
+//Sets and select blackColor
+blackColor.style.backgroundColor = 'black';
+blackColor.classList.add('selected');
+
+//Sets other colors
+firstColor.style.backgroundColor = 'blue';
+secondColor.style.backgroundColor = 'red';
+thirdColor.style.backgroundColor = 'gray';
+
+
+function removeSelection () {
+  for (let i = 0; i < colorList.length; i += 1) {
+    colorList[i].classList.remove('selected');
+  }
+}
+
+function parseSelection () {
+  for (let i = 0; i < colorList.length; i += 1) {
+    if (colorList[i].classList.contains('selected')) {
+      return colorList[i].style.backgroundColor;
+    }
+  }
+}
+
+let selectedColor = parseSelection();
