24e0eff821aab8c9b45e8144d3b5ae22064a4bdc
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-09 15:11:44 -0300

    Resolve requisito obrigatório 4

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function colorPalette () {
     colorLocal[1].style.backgroundColor = "yellow";
     colorLocal[2].style.backgroundColor = "green";
     colorLocal[3].style.backgroundColor = "red";
-
 }
 colorPalette();
+
+function createPixels (){
+    for (let i = 0; i < 25; i += 1){
+        let pixelLocal = document.getElementById('pixel-board');
+        let divPixels = document.createElement('div');
+        divPixels.className = "pixel";
+        divPixels.backgroundColor = "white";
+        pixelLocal.appendChild(divPixels);
+    }
+}
+createPixels();
