86f64ff2d23b10bff179581071ad95f70f55db01
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-11-09 16:40:46 -0300

    Requisito 7: Pintando a paleta ... Continuação

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ let pixels = document.querySelector("#pixel-board");
 console.log(pixels);
 let teste = document.querySelector("#cor2")
 console.log(window.getComputedStyle(teste, null).getPropertyValue("background-color"));
+
+pixels.addEventListener('click', function(event){
+  for (i = 0; i< paletas.length; i++) {
+    console.log(paletas[i].style.backgroundColor)
+    if (paletas[i].className === 'color selected') {
+      event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue("background-color")
+    }
+  }
+})
