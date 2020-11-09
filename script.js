b69134f340354b3435be138c158b278a272966a8
Pedro Amaral <amaralph1995@gmail.com>
2020-11-09 17:02:12 -0300

    Req 4, 5* - corrige quadro de pixels

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let paletaCores = document.querySelectorAll('.color');
+    let corPreta = paletaCores[0];
+    corPreta.className += ' selected';
+    console.log(corPreta)
+}
