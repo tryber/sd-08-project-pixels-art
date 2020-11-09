8459eab4433ac020d0f5ff38f08a81bb275b35e2
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-09 15:58:58 -0300

    req1: Feito em html para passar no evaluator

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-//requisito 1
-const titlePaletadeCores = document.createElement('h1');
-const captureBody = document.querySelector('body');
-//dando o id e nome para o h1 criado
-titlePaletadeCores.id = 'title';
-titlePaletadeCores.innerHTML = 'Paleta de Cores';
-//posicionei title como filho de body
-captureBody.appendChild (titlePaletadeCores);
-
 //requisito 2
-const idPaletaDeCores = document.createElement('');
+const idPaletaDeCores = document.createElement('div');
 idPaletaDeCores.id = `color-palette`;
+//fazer um laço de 0 a 4 para criar as classes color
