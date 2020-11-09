354f4f1c98c06a89f0fe285c0075c52690880616
rafasysop <rafaelcostamoura@gmail.com>
2020-11-09 16:48:44 -0300

    mudando a posição do Limpar

--- script.js
+++ script.js
@@ -18,16 +18,23 @@ function getRandomColor() {
   return color;
 }
 
+function mudaCor(evt) {
+  const removeSelected = document.querySelector('.selected');
+  removeSelected.className = 'color';
+  evt.target.className = 'color selected';
+
+}
+
 function criaPalette() {
   // Cria as Divs filha de color-Pallet
   for (let i = 1; i <= qtdPallet; i += 1) {
     const divChildPallet = document.createElement('div');
     divChildPallet.className = 'color';
-    if (i == 1){
+    if (i === 1) {
       divChildPallet.style.backgroundColor = primeiroPallet;
       divChildPallet.className = 'color selected';
 
-    }else{
+    } else {
       divChildPallet.style.backgroundColor = getRandomColor();
     }
     divChildPallet.addEventListener('click', mudaCor);
@@ -36,45 +43,32 @@ function criaPalette() {
 }
 criaPalette();
 
+function preenchePixel(evt) {
+  const corBG = document.querySelector('.selected').style.backgroundColor;
+  evt.target.style.backgroundColor = corBG;
+}
+function limpar() {
+  const todosPixels = document.querySelectorAll('.pixel');
+  for (let rmPixel = 0; rmPixel < todosPixels.length; rmPixel += 1) {
+    todosPixels[rmPixel].style.backgroundColor = '#FFFFFF';
+  }
+}
+const btnClear = document.createElement('button');
+btnClear.id = 'clear-board';
+btnClear.innerHTML = 'Limpar';
+btnClear.addEventListener('click', limpar);
+document.querySelector('#clear').appendChild(btnClear);
 function criaPixel() {
   for (let i = 0; i < 5; i += 1) {
     const divChildLinePixel = document.createElement('div');
-
-    for (let coluna=0;coluna<5;coluna +=1) {
+    for (let coluna = 0; coluna < 5; coluna += 1) {
       const divChildColPixel = document.createElement('div');
       divChildColPixel.className = 'pixel';
       divChildLinePixel.appendChild(divChildColPixel);
       divChildColPixel.addEventListener('click', preenchePixel);
     }
-
     divPixel.appendChild(divChildLinePixel);
-
-
   }
 }
-criaPixel()
 
-function mudaCor(evt) {
-  const removeSelected = document.querySelector('.selected');
-  removeSelected.className = 'color';
-  evt.target.className = 'color selected';
-
-}
-
-function preenchePixel(evt) {
-  const corBG = document.querySelector('.selected').style.backgroundColor ;
-  evt.target.style.backgroundColor = corBG;
-}
-const btnClear = document.createElement('button');
-btnClear.id = 'clear-board';
-btnClear.innerHTML = 'Limpar';
-btnClear.addEventListener('click', limpar)
-document.body.appendChild(btnClear);
-
-function limpar() {
-  const todosPixels = document.querySelectorAll('.pixel');
-  for (let rmPixel = 0; rmPixel < todosPixels.length ; rmPixel+=1){
-    todosPixels[rmPixel].style.backgroundColor = '#FFFFFF';
-  }
-
-}
+criaPixel();
