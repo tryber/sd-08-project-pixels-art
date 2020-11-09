c9c876cd3d793ff603d82f2dbe879da8ad6fd05c
Vitor Nunes <vitornunes200@gmail.com>
2020-11-09 19:25:17 -0300

    Requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function(){
+window.onload = carregaPixels;
+
+function carregaPixels(){
     let table = document.querySelector('#tabela');
     let count = 1;
     for(let col = 1; col <= 5; col++){
@@ -17,5 +19,63 @@ window.onload = function(){
         coluna.appendChild(box);
         }
     }
+    let pintar;
+
+    let botao = document.getElementById("clear-board");
+    botao.addEventListener("click", clear);
+
+    let pintarPreto = document.getElementById("black")
+    pintarPreto.addEventListener("click", function(){ 
+        pintar = "black";
+        removeClass();
+        pintarPreto.className += " selected";
+    })
     
+    let pintarVermelho = document.getElementById("red")
+    pintarVermelho.addEventListener("click", function(){ 
+        pintar = "red";
+        removeClass();
+        pintarVermelho.className += " selected";
+    })
+
+    let pintarAzul = document.getElementById("blue")
+    pintarAzul.addEventListener("click", function(){ 
+        pintar = "blue";
+        removeClass();
+        pintarAzul.className += " selected";
+    })
+
+    let pintarAmarelo = document.getElementById("yellow")
+    pintarAmarelo.addEventListener("click", function(){ 
+        pintar = "yellow";
+        removeClass();
+        pintarAmarelo.className += " selected";
+    })
+
+    
+     let box = document.getElementsByClassName('pixel');
+     Array.from(box).forEach(function(box){
+         box.addEventListener("click", function(){
+            box.style.backgroundColor = pintar;
+        })
+    })
+     
+    function removeClass(){
+
+        pintarPreto.classList.remove("selected");
+        pintarVermelho.classList.remove("selected");
+        pintarAzul.classList.remove("selected");
+        pintarAmarelo.classList.remove("selected");
+    }
+
+    
+
+    function clear(){
+        let box = document.getElementsByClassName('pixel');
+         Array.from(box).forEach(function(box){
+                box.style.backgroundColor = "white";
+        })
+    }
 }
+
+
