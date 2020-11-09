f97129862ed687a32556739096f03c7f863ce93d
rafasysop <rafaelcostamoura@gmail.com>
2020-11-09 15:23:24 -0300

    Enviando requisitos 4 ao 6

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 let qtdPallet = 4;
 let primeiroPallet = 'black';
 let divPallet = document.querySelector('#color-palette');
+let divPixel = document.querySelector('#pixel-board');
+let colorSelected = primeiroPallet;
 
 function getRandomColor() {
   //Função para gerar cor Aleatória
@@ -20,10 +22,11 @@ function getRandomColor() {
 function criaPalette(){
   //Cria as Divs filha de color-Pallet
   for(i=1;i<=qtdPallet;i+=1){
-    let divChildPallet = document.createElement('div')
+    let divChildPallet = document.createElement('div');
     divChildPallet.className = 'color';
     if(i == 1){
       divChildPallet.style.backgroundColor = primeiroPallet;
+      divChildPallet.className = 'color selected';
     }else{
       divChildPallet.style.backgroundColor = getRandomColor();
     }
@@ -31,3 +34,19 @@ function criaPalette(){
   }
 }
 criaPalette();
+
+function criaPixel(){
+  for(i=0;i<5;i+=1){
+    let divChildLinePixel = document.createElement('div');
+
+    for(coluna=0;coluna<5;coluna +=1){
+      let divChildColPixel = document.createElement('div');
+      divChildColPixel.className = 'pixel';
+      divChildColPixel.style.display = 'inline-block';
+      divChildLinePixel.appendChild(divChildColPixel);
+    }
+    divPixel.appendChild(divChildLinePixel);
+
+  }
+}
+criaPixel()
