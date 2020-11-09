5ea5a228b5e1ff95376c6ce44442091c903e70cb
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-11-09 18:23:25 -0300

    adicionando bonus 10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-  const paletas = document.querySelectorAll('.color');
-  const pixels = document.querySelectorAll('.pixel');
-  const botaoLimpar = document.querySelector('#clear-board');
+  let paletas = document.querySelectorAll('.color');
+  let botaoLimpar = document.querySelector('#clear-board');
+  let botaoGerarQuadro = document.querySelector('#generate-board');
 
   function deselecionaPaletas() {
     for (let index = 0; index < paletas.length; index += 1) {
@@ -24,19 +24,56 @@ window.onload = function () {
     elemento.target.style.backgroundColor = corSelecionada;
   }
 
+  function selecionarPixels() {
+    let pixels = document.querySelectorAll('.pixel');
+
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].addEventListener('click', pintarPixel);
+    }
+  }
+
   for (let index = 0; index < paletas.length; index += 1) {
     paletas[index].addEventListener('click', selecionaPaleta);
   }
 
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', pintarPixel);
-  }
-
   function limparPixels() {
+    let pixels = document.querySelectorAll('.pixel');
+
     for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
     }
   }
 
+  function criarQuadro(n) {
+    console.log('entrou');
+    let sectionBoard = document.querySelector('#pixel-board');
+    let paiQuadro = sectionBoard.parentNode;
+    paiQuadro.removeChild(sectionBoard);
+    let sectionBoardCreate = document.createElement('section');
+    sectionBoardCreate.id = 'pixel-board';
+    for (let index = 0; index < n; index += 1) {
+      let sectionLinha = document.createElement('section');
+      sectionLinha.className = 'linha';
+      for (let j = 0; j < n; j++) {
+        let divPixel = document.createElement('div');
+        divPixel.className = 'pixel';
+        sectionLinha.appendChild(divPixel);
+      }
+      sectionBoardCreate.appendChild(sectionLinha);
+    }
+    paiQuadro.appendChild(sectionBoardCreate);
+    selecionarPixels();
+  }
+
+  function gerarQuadro() {
+    let tamanhoQuadro = document.querySelector('#board-size').value;
+    if (tamanhoQuadro == '') {
+      alert('Board inválido!');
+    } else {
+      criarQuadro(tamanhoQuadro);
+    }
+  }
+  selecionarPixels();
   botaoLimpar.addEventListener('click', limparPixels);
+  botaoGerarQuadro.addEventListener('click', gerarQuadro);
 };
