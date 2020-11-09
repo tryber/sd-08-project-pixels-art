45c60b3da00a707d178e282d204679882a91a4fe
Bruno Martins <bruno.martins217@gmail.com>
2020-11-09 20:48:06 -0300

    Adicionando Requisito 7

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function firstBlack(){
     element.className = 'color selected'
 }
 firstBlack()
+
+function selecionaCor(){
+    let elemento = document.getElementsByClassName('color')
+    let elemento2 = document.querySelector('.selected')
+    for (i = 0; i < elemento.length; i += 1){
+        let corSelecionada = elemento[i]
+        corSelecionada.addEventListener('click', function(){
+            let elemento2 = document.querySelector('.selected')
+            if(corSelecionada.className === 'color'){
+                corSelecionada.className = 'color selected'
+                elemento2.className = 'color'
+            }else{
+                corSelecionada.className = 'color'
+                
+            }
+        })
+    }
+}
+selecionaCor()
