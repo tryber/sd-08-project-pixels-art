752ca285db4bb7d65364e7dfe2f9b8affb8e09b0
JRGCast <joao.ralph1@gmail.com>
2020-11-09 17:59:08 -0300

    prototipo da pagina ja feito, começando as funçoes de escolha de cor e pintura do pixel

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let coresFundo = ['black','red','blue','green']
-let palette = document.getElementById('color-palette');
+function paletaCores() {
+  let coresFundo = ['black', 'red', 'blue', 'green'];
+  let palette = document.getElementById('color-palette');
 
-for (let i = 0; i < coresFundo.length; i += 1){
+  for (let i = 0; i < coresFundo.length; i += 1) {
     let quadradosCores = document.createElement('li');
+    quadradosCores.className = 'color';
     quadradosCores.style.backgroundColor = coresFundo[i];
-    palette.appendChild(quadradosCores)
+    palette.appendChild(quadradosCores);
+  }
 }
+paletaCores();
 
+function quadroPixels() {
+  // let tamanhoDefinicao = prompt('quantos quadrados você quer?');
+  // let tamanhoDefinido = Number(tamanhoDefinicao);
+  let linhaQuadrados = 4;
+  let colunaQuadrados = 5;
+  let squareContainer = document.getElementById('pixel-board');
 
-let tamanhoDefinicao = prompt('quantos quadrados você quer?');
-let tamanhoDefinido = Number(tamanhoDefinicao);
-let squareContainer = document.getElementById('pixel-board');
+  for (let i = 0; i < linhaQuadrados; i += 1) {
+    let linhaSquares = document.createElement('tr');
+    squareContainer.appendChild(linhaSquares);
 
-for (let i = 0; i < tamanhoDefinido; i += 1) {
-  let squares = document.createElement('li');
-  squareContainer.appendChild(squares);
+    for (let i = 0; i < colunaQuadrados; i += 1) {
+      let colunaSquares = document.createElement('td');
+      squareContainer.appendChild(colunaSquares);
+    }
+  }
 }
+quadroPixels();
