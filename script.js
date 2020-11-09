282522663dcd153363f91af3d575dbec6b12a853
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-09 18:56:41 -0300

    acerta requisito de altura e comprimento do board

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function createBoard(num) {
-  const pixelBoard = document.getElementById('pixel-board');
 
-  for (let index = 1; index <= num; index += 1) {
+const pixelBoard = document.getElementById('pixel-board');
+
+function generateBoard(num) {
+    for (let index = 1; index <= num; index += 1) {
     let divLine = document.createElement('div');
-    divLine.className = 'lines';
     pixelBoard.appendChild(divLine);
 
     for (let index = 1; index <= num; index += 1) {
@@ -12,8 +12,8 @@ function createBoard(num) {
       divLine.appendChild(pixelUnit); 
     }
   }
-}
-createBoard(5);
+};
+generateBoard(5);
 
 function createPalette() {
   let firstColor = document.getElementById('firstColor');
@@ -43,14 +43,14 @@ selectedColor();
 
 function fillPixelColor() {
   let pixelUnit = document.querySelector('#pixel-board');
-  
+  pixelUnit.addEventListener('click', changeColor);
 
   function changeColor(event) {
     let selectedColor = document.querySelector('.selected');
     event.target.style.backgroundColor = selectedColor.style.backgroundColor;
   };
 
-  pixelUnit.addEventListener('click', changeColor);
+  
 }
 fillPixelColor();
 
