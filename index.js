90ebcf7352c8880bd58d0e5a0a0b728297dffd96
Max <gmaxgomes@gmail.com>
2020-11-09 15:35:20 -0300

    fix

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+/* Selecione as cores das paletas de cores */
 function setColorTwo(color) {
   let colorTwo = document.querySelectorAll('.color')[1];
   colorTwo.style.backgroundColor = color;
 }
-setColorTwo('rgb(15, 163, 107)');
+setColorTwo();
 
 function setColorThree(color) {
   let colorThree = document.querySelectorAll('.color')[2];
   colorThree.style.backgroundColor = color;
 }
-setColorThree('yellow');
+setColorThree();
 
 function setColorFour(color) {
   let colorFour = document.querySelectorAll('.color')[3];
   colorFour.style.backgroundColor = color;
 }
-setColorFour('red');
+setColorFour();
