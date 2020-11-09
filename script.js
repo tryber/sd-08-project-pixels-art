d9e2bef06e59844e4da25cdfc6efa1a830df4222
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-11-09 18:11:36 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const pixel = document.querySelector('#pixel-board');
-const color = document.querySelectorAll('.color');
+const pixel = document.querySelector("#pixel-board");
+const color = document.querySelectorAll(".color");
 for (let coluna = 0; coluna < 5; coluna += 1) {
   for (let linha = 0; linha < 5; linha += 1) {
-    let div = document.createElement('div');
-    div.className = 'pixel';
-    div.style.backgroundColor = '#fff';
-    div.style.width = '40px'
-    div.style.height = '40px'
-    div.style.marginBottom = '10px'
-    div.style.border = '1px solid black'
+    let div = document.createElement("div");
+    div.className = "pixel";
+    div.style.backgroundColor = "white";
+    div.style.width = "40px";
+    div.style.height = "40px";
+    div.style.marginBottom = "10px";
+    div.style.border = "1px solid black";
     pixel.appendChild(div);
   }
   window.onload = function () {
-    addEventListener('click', function (evt) {
-        if(evt.target.className == 'color') {
-            for (let index = 0; index < color.length; index += 1) {
-                color[index].className = 'color';
-            }
-            evt.target.className = 'color selected'
+    addEventListener("click", function (evt) {
+      if (evt.target.className == "color") {
+        for (let index = 0; index < color.length; index += 1) {
+          color[index].className = "color";
         }
-    })
-  }
+        evt.target.className = "color selected";
+      }
+    });
+    addEventListener('click', function (evt) {
+      for (let index = 0; index < color.length; index += 1) {
+        // let colorPixel = color[index].className;
+        if (color[index].className == 'color selected' && evt.target.className == 'pixel') {
+            evt.target.style.backgroundColor = getComputedStyle(color[index]).backgroundColor;
+        }
+      } 
+    });
+  };
 }
