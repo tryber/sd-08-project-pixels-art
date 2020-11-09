7c412fbf46790517c7010b9d987d14ff7c408135
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-09 14:50:07 -0300

    Resolve requisito obrigatório 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function colorPalette () {
+    const colorLocal = document.querySelectorAll(".color");
+    colorLocal[0].style.backgroundColor = "black";
+    colorLocal[1].style.backgroundColor = "yellow";
+    colorLocal[2].style.backgroundColor = "green";
+    colorLocal[3].style.backgroundColor = "red";
+
+}
+colorPalette();
