f033da65aeb59fadf09516fc1d19bfc82475cb70
Ediberto Oliveira <edibertooliveira@aol.com>
2020-11-09 16:53:50 -0300

    feat: question 7, 8

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let colorSelected = document.querySelector(".selected").style.backgroundColor;
+let colorSelected = document.querySelector('.selected').style.backgroundColor;
+
+function backgroundColor(target, color) {
+  return (target.style.backgroundColor = color);
+}
+function removeSelectedAll() {
+  document.querySelectorAll('.color').forEach((element) => {
+    removeSelected(element);
+  });
+}
+function removeSelected(target) {
+  return target.classList.remove('selected');
+}
+function addedSelected(target) {
+  removeSelectedAll();
+  colorSelected = target.style.backgroundColor;
+  return target.classList.add('selected');
+}
+function capturedElementEvents(target, event, callback) {
+  const currentElement = document.getElementById(target);
+  currentElement.addEventListener(event, (element) => {
+    const elementEvent = element.path[0];
+    callback(elementEvent, colorSelected);
+  });
+}
+
+capturedElementEvents('pixel-board', 'click', backgroundColor, false);
+capturedElementEvents('color-palette', 'click', addedSelected);
