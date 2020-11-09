bc686b61e1a27c4750fd87bffe08b2ee39932d20
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-09 18:59:04 -0300

    req 6 check

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function toggleColor() {
+
+
+
+    const blackCC = document.querySelector ('#black')
+    blackCC.className = "color selected"
+
+   window.onload = blackCC.addEventListener('click', function (event) {
+    const selected = document.querySelector('.selected')
+
+    if (selected === null ) {
+    event.target.className ="color selected"
+    
+} else {
+    event.target.className = "color" 
+}
+
+    }
+
+    )}
+
+  window.onload = toggleColor();
