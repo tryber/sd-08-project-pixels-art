75105a663ca0dfce748c0998b74ac615777ed0d5
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-09 19:13:23 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    const colorInit = document.querySelector('.black');
+    colorInit.className = 'color black select';
+}
+
+
+
 function addTable() {
   const table = document.querySelector('#pixel-board');
 
@@ -11,3 +18,19 @@ function addTable() {
 }
 
 addTable();
+
+// function colorSelect() {
+//   const black = document.querySelector('.black');
+//   const red = document.querySelector('.red');
+//   const green = document.querySelector('.green');
+//   const blue = document.querySelector('.blue');
+//   const colorPalette = document.querySelector('#color-palette');
+
+//   colorPalette.addEventListener('click', function(event) {
+//     const nome = event.target.className;
+//     if ()
+//   });
+
+// }
+
+// colorSelect();
