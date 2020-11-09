94c2e20651e7ffa647209fa1e70c0e0b68293ea7
Daniel <danieldsrd@gmail.com>
2020-11-09 15:34:23 -0300

    Criando os Pixels

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function palettaColor () {
     document.querySelectorAll('.color')[3].style.background = 'blue';
 }
 palettaColor();
+
+function createPixels () {    
+    let divlPixelBoard = document.querySelectorAll('#pixel-board');
+    for (let i = 0; i < divlPixelBoard.length; i +=1) {
+        for (let j = 0; j < divlPixelBoard.length; j +=1){
+            let divPixel = document.createElement('div');
+            divPixel.className = 'pixel';
+            divlPixelBoard[i].appendChild(divPixel);
+        }
+    }
+}
+createPixels();
