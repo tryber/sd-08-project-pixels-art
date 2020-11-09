1f6a1259e2d8be8f002f7ed485bd64a4fe9feb27
antonioarieiro <sfgfffs@gmail.com>
2020-11-09 16:09:00 -0300

    adicionando as cores

---
+++ script.js
@@ -0,0 +1,8 @@
+function colorPaletBackground (){
+    let colorBkgd = document.querySelectorAll('.color');
+    colorBkgd.style.border = "thick solid #0000FF";
+    
+    
+    
+}
+colorPaletBackground()
