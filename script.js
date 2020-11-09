ff859d17e8e0bbab20a97baba410dafc939d1941
mateuscoury <72469989+mateuscoury@users.noreply.github.com>
2020-11-09 17:23:33 -0300

    exercicio 4

--- script.js
+++ script.js
@@ -55,16 +55,120 @@ function createPaleteColorEle(){
 createPaleteColorEle()
 
 
+
+
 function createQuadroPixel(){
-    let getposi = document.querySelector("#pixel-board")
+    
+    let getposi1 = document.querySelector("#div1")
+    
+    let getposi2 = document.querySelector("#div2")
+    
+    let getposi3 = document.querySelector("#div3")
+    
+    let getposi4 = document.querySelector("#div4")
+    
+    let getposi5 = document.querySelector("#div5")
+    
+    
+    
     for(let i = 0; i < 5 ; i++){
-        for(let i1 = 0 ; i1 < 5 ; i1 ++){
-            
-        }
+
         let createpixel = document.createElement("div")
         createpixel.className = "pixel"
         
-        getposi.appendChild(createpixel)
+        
+        getposi1.appendChild(createpixel)
+        
+        
+
+    } 
+    for(let i = 0; i < 5 ; i++){
+
+        let createpixel = document.createElement("div")
+        createpixel.className = "pixel"
+       
+        
+        getposi2.appendChild(createpixel)
+        
+        
+
     }
+    for(let i = 0; i < 5 ; i++){
+
+        let createpixel = document.createElement("div")
+        createpixel.className = "pixel"
+       
+        
+        getposi3.appendChild(createpixel)
+        
+        
+
+    }
+    for(let i = 0; i < 5 ; i++){
+
+        let createpixel = document.createElement("div")
+        createpixel.className = "pixel"
+        
+        
+        getposi4.appendChild(createpixel)
+        
+        
+
+    }
+    for(let i = 0; i < 5 ; i++){
+
+        let createpixel = document.createElement("div")
+        createpixel.className = "pixel"
+      
+        
+        getposi5.appendChild(createpixel)
+        
+        
+
+    }
+   
+
 }
 createQuadroPixel()
+
+
+/* 
+function createtable() {
+    let getpostable = document.querySelector("#table")
+    let tr = document.querySelector("#td1")
+    
+    
+    for(let i = 0 ; i  < 5 ; i ++){
+let createcel = document.createElement("td")
+createcel.className = "pixel"
+tr.appendChild(createcel)
+getpostable.appendChild(tr)
+
+   }
+   for(let i = 0 ; i  < 5 ; i ++){
+    let createcel = document.createElement("td")
+    createcel.className = "pixel"
+    tr.appendChild(createcel)
+    getpostable.appendChild(tr)
+    
+       }
+       for(let i = 0 ; i  < 5 ; i ++){
+        let createcel = document.createElement("td")
+        createcel.className = "pixel"
+        tr.appendChild(createcel)
+        getpostable.appendChild(tr)
+        
+           }
+   
+   
+}
+createtable()
+
+function paintpallete(){
+let getposicaopal = document.querySelector()
+
+
+
+}
+paintpallete()
+*/ 
