517ce3d0c6ab3d736b2ce316771ccb2a30424a20
Daniel <marcio.daniel@msn.com>
2020-11-09 19:10:16 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const cores = ['black', 'red', ' blue', 'green'];
 const divColorPalette = document.getElementById("color-palette");
 
-
+// const body = document.body;
 
 for (let i in cores) {
   let diviCores = document.createElement('div');
-  diviCores.style =`background-color:${cores[i]};`;
-  diviCores.className ='color';
+  diviCores.style = `background-color:${cores[i]};`;
+  diviCores.className = 'color';
   divColorPalette.appendChild(diviCores);
+}
+
+
+const pixelBoard = document.getElementById('pixel-board');
+
+for(let i =1; i<=25; i++){
+  const divSpan = document.createElement('div');
+  divSpan.className = 'pixel';
+  pixelBoard.appendChild(divSpan);
 }
