c59788bbb563dfac2518770403509ca0f15cd8d4
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-09 15:59:29 -0300

    requisito 7 classe selected

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//preenchendo quadro de pixels
 let pixelBoard = document.getElementById('pixel-board')
 
 for(let index = 0; index < 25; index +=1){
@@ -6,3 +7,67 @@ for(let index = 0; index < 25; index +=1){
 
     pixelBoard.appendChild(pixelDiv)
 }
+
+//pintando
+let colorsArray = document.getElementsByClassName('color');
+
+//classe selected
+//preto
+
+    colorsArray[0].addEventListener("click",function(){
+        if(colorsArray[1].className === "color red selected"){
+            colorsArray[1].className = 'color red'
+        }
+        if(colorsArray[2].className === "color green selected"){
+            colorsArray[2].className = 'color green'
+        }
+        if(colorsArray[3].className === "color blue selected"){
+            colorsArray[3].className = 'color blue'
+        }
+        colorsArray[0].className = 'color black selected'
+    })
+
+//vermelho
+
+colorsArray[1].addEventListener("click",function(){
+    if(colorsArray[0].className === "color black selected"){
+        colorsArray[0].className = 'color black'
+    }
+    if(colorsArray[2].className === "color green selected"){
+        colorsArray[2].className = 'color green'
+    }
+    if(colorsArray[3].className === "color blue selected"){
+        colorsArray[3].className = 'color blue'
+    }
+    colorsArray[1].className = 'color red selected';
+})
+
+//verde
+
+colorsArray[2].addEventListener("click",function(){
+    if(colorsArray[0].className === "color black selected"){
+        colorsArray[0].className = 'color black'
+    }
+    if(colorsArray[1].className === "color red selected"){
+        colorsArray[1].className = 'color red'
+    }
+    if(colorsArray[3].className === "color blue selected"){
+        colorsArray[3].className = 'color blue'
+    }
+    colorsArray[2].className = 'color green selected';
+})
+
+//azul
+
+colorsArray[3].addEventListener("click",function(){
+    if(colorsArray[0].className === "color black selected"){
+        colorsArray[0].className = 'color black'
+    }
+    if(colorsArray[1].className === "color red selected"){
+        colorsArray[1].className = 'color red'
+    }
+    if(colorsArray[2].className === "color green selected"){
+        colorsArray[2].className = 'color green'
+    }
+    colorsArray[3].className = 'color blue selected';
+})
