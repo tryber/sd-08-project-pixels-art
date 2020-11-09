4897fc1b427d8d0bfdc20545e38b5c310d9eaf3a
Tandy <tandyfw@gmail.com>
2020-11-09 14:40:54 -0300

    Requisito 2 e 3 concluidos

---
+++ script.js
@@ -0,0 +1,9 @@
+function initialColors () {
+  let colors = document.querySelectorAll('.color');
+  colors[0].style.backgroundColor = 'black';
+  colors[1].style.backgroundColor = 'blue';
+  colors[2].style.backgroundColor = 'yellow';
+  colors[3].style.backgroundColor = 'green';
+}
+
+initialColors();
