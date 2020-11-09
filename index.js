fb7489fef94502d81d07e796a4c757f6f7f56cc6
Max <gmaxgomes@gmail.com>
2020-11-09 18:07:19 -0300

    Requisito 7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function startLoad() {
+function startLoad() {              /* Implement window.onload */
   let pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index +=1) {
     pixels[index].style.backgroundColor = 'white';
@@ -9,6 +9,46 @@ function startLoad() {
 
 }
 startLoad();
+
+/**/
+
+function setPixelColor(color) {
+
+  let pixel = document.getElementsByClassName('pixel')[0];
+  pixel.style.backgroundColor = color;
+
+}
+
+
+function selectColor() {
+  let colorPalette = document.getElementById('color-palette');
+
+  colorPalette.addEventListener('click', function (event) {
+    removesSelected();
+
+    let color = event.target;
+    color.classList.add('selected');
+
+    colorPalette.classList.remove('selected');
+  });
+
+
+
+}
+selectColor();
+
+function removesSelected() {
+
+  let color = document.querySelectorAll('.selected');
+
+  for (let index = 0; index < color.length; index += 1) {
+
+    color[index].classList.remove('selected');
+
+  }
+}
+
+
 
 /* Selecione as cores das paletas de cores */
 function setColorTwo(color) {
@@ -31,10 +71,5 @@ setColorFour();
 
 /**/
 
-function setPixelColor(color) {
 
-  let pixel = document.getElementsByClassName('pixel')[0];
-  pixel.style.backgroundColor = color;
-
-}
 
