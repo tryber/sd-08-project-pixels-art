cc4728253826c98421dc72386d11f0b9b3c30070
Victor Breno <victor.brns2@gmail.com>
2020-11-09 16:21:09 -0300

    correção dos requesitos 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let arrayDeCores = ['green', 'yellow', 'blue'];
 for (let index = 0; index < 4; index += 1){
   let corDaPaleta = document.createElement('div');
   corDaPaleta.className = ('color');
-  corDaPaleta.style.display = ('table-row');
+  corDaPaleta.style.display = ('flex');
   corDaPaleta.style.border = ('solid');
   corDaPaleta.style.borderWidth = ('1px');
   corDaPaleta.style.borderColor = ('black');
@@ -12,7 +12,11 @@ for (let index = 0; index < 4; index += 1){
     corDaPaleta.style.backgroundColor = ('black');
   } else {
     corDaPaleta.style.backgroundColor = (arrayDeCores[index - 1]);
-  }
+  } 
+  corDaPaleta.style.width = ('40px');
+  corDaPaleta.style.height = ('40px');
+  corDaPaleta.style.margin = ('5px');
   paletaDeCor.appendChild(corDaPaleta);
 
 }
+
