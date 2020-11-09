d7b1d98c85455491e955df92b8cf373136e527ef
siebenei5 <siebenei7@gmail.com>
2020-11-09 20:25:35 -0300

    js For Pixels

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+for(let i = 0; i < 5; i += 1)
+{
+    for(let j = 0; j < 5; j += 1)
+    {
+        let div = document.createElement("div");
+        div.className = "pixel";
+        div.style.backgroundColor = "white";
+
+        document.getElementById("pixel-board").appendChild(div);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+document.getElementById("color-palette").addEventListener("click", selectColor)
+
+
+
+function selectColor()
+{
+    if()
+}
+
+document.getElementById("pinta-pixel").style.backgroundColor = "black";
+*/
