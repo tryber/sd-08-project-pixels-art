bf234912b326bb770ecc6c0829142c398b1a6b0c
rafasysop <rafaelcostamoura@gmail.com>
2020-11-09 16:18:25 -0300

    Enviando requisitos 7 ao 9

--- script.js
+++ script.js
@@ -13,40 +13,69 @@ function getRandomColor() {
   for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
   }
-  if(color == "#FFFFFF"){
+  if(color == "#FFFFFF") {
     getRandomColor()
   }
   return color;
 }
 
-function criaPalette(){
+function criaPalette() {
   //Cria as Divs filha de color-Pallet
-  for(i=1;i<=qtdPallet;i+=1){
+  for(i=1;i<=qtdPallet;i+=1) {
     let divChildPallet = document.createElement('div');
     divChildPallet.className = 'color';
     if(i == 1){
       divChildPallet.style.backgroundColor = primeiroPallet;
       divChildPallet.className = 'color selected';
+
     }else{
       divChildPallet.style.backgroundColor = getRandomColor();
     }
+    divChildPallet.addEventListener('click', mudaCor);
     divPallet.appendChild(divChildPallet);
   }
 }
 criaPalette();
 
-function criaPixel(){
-  for(i=0;i<5;i+=1){
+function criaPixel() {
+  for(i=0;i<5;i+=1) {
     let divChildLinePixel = document.createElement('div');
 
-    for(coluna=0;coluna<5;coluna +=1){
+    for(coluna=0;coluna<5;coluna +=1) {
       let divChildColPixel = document.createElement('div');
       divChildColPixel.className = 'pixel';
-      divChildColPixel.style.display = 'inline-block';
       divChildLinePixel.appendChild(divChildColPixel);
+      divChildColPixel.addEventListener('click', preenchePixel);
     }
+
     divPixel.appendChild(divChildLinePixel);
+
 
   }
 }
 criaPixel()
+
+function mudaCor(evt) {
+  let removeSelected = document.querySelector('.selected');
+  removeSelected.className = 'color';
+  evt.target.className = 'color selected';
+
+}
+
+function preenchePixel(evt) {
+  let corBG = document.querySelector('.selected').style.backgroundColor ;
+  evt.target.style.backgroundColor = corBG;
+}
+let btnClear = document.createElement('button');
+btnClear.id = 'clear-board';
+btnClear.innerHTML = 'Limpar';
+btnClear.addEventListener('click', limpar)
+document.body.appendChild(btnClear);
+
+function limpar() {
+  let todosPixels = document.querySelectorAll('.pixel');
+  for(let rmPixel = 0; rmPixel < todosPixels.length ; rmPixel+=1){
+    todosPixels[rmPixel].style.backgroundColor = '#FFFFFF';
+  }
+
+}
