54a59e80b621194878a4b3de7a169abc840fde89
Victor Leone de Oliveira <vileoleone@gmail.com>
2020-11-09 14:18:09 -0300

    cores de paleta de forma dinâmica para atender ao code climate

---
+++ script.js
@@ -0,0 +1,7 @@
+// Paleta de cores - Adicionando as cores de forma dinâmica para evitar encadeamento de classes
+let paleta = document.querySelectorAll(".color");
+console.log("hello")
+paleta[0].style.backgroundColor = "black";
+paleta[1].style.backgroundColor = "#f7be4A";
+paleta[2].style.backgroundColor = "#ab8333";
+paleta[3].style.backgroundColor = "#6b5220";
