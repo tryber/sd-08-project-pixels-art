a7d4638b1435b4221014368bfc17b33ef0ad5c87
Felipe Franca <felipefrancafj@gmail.com>
2020-11-09 15:51:25 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const paletaDeCores = document.getElementById("color-palette");
-const tabelaDeCores = ["blue", "pink", "green", "red", "yellow"];
+const tabelaDeCores = ["blue", "pink", "green", "red"];
 for (let itens = 0; itens < tabelaDeCores.length; itens += 1) {
   let color = document.createElement("div");
   color.className = "color";
   color.style.background = tabelaDeCores[itens];
   color.style.display = "inline-block";
+  color.style.width = '100px'
   paletaDeCores.appendChild(color);
   let text = document.createElement("p");
   text.innerHTML = tabelaDeCores[itens];
+  text.style.textAlign = 'center'
   color.appendChild(text);
 }
