2d9466888488f0803f494279e371ca6e4ef1101d
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-09 15:34:01 -0300

    req1: realizei 100% atraves do javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+//requisito 1
+const titlePaletadeCores = document.createElement('h1');
+const captureBody = document.querySelector('body');
+//dando o id e nome para o h1 criado
+titlePaletadeCores.id = 'title';
+titlePaletadeCores.innerHTML = 'Paleta de Cores';
+//posicionei title como filho de body
+captureBody.appendChild (titlePaletadeCores);
+
+//requisito 2
+const idPaletaDeCores = document.createElement('');
+idPaletaDeCores.id = `color-palette`;
