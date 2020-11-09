a7ff9bafd02d6b945eedcb7a898deb0a7044898c
Rafael <rafaelmaltez89@gmail.com>
2020-11-09 19:04:34 -0300

    refatora função criaPixels para aceitar valor de parametro

--- script.js
+++ script.js
@@ -8,24 +8,31 @@ function criaCores(){
     cor.className = 'color'; 
     }
     let cores = document.getElementsByClassName('color');
+    cores[0].className = 'color selected';
     for (let index =1; index <4; index +=1){
       cores[index].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
       // copiei a função de numero aleatorio do site https://www.w3schools.com/js/js_random.asp
     };
+
   }
   
 
 criaCores();
 
-function criaPixels(){
+function criaPixels(numeroDePixels){
   const board = document.getElementById('pixel-board');
-  for (let index = 0; index < 25; index += 1){
+  for (let index = 0; index < Math.pow(numeroDePixels, 2); index += 1){
     let pixel = document.createElement('div');
     pixel.className = 'pixel';       
     board.appendChild(pixel);
   }
+  board.style.maxWidth = (numeroDePixels * 40) + 'px';
 }
-criaPixels();
+criaPixels(5);
+//função Math.pow retirada de https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
+
+
+
 
 function selecionaCor(){
   const cores = document.querySelectorAll('.color');  
@@ -66,8 +73,3 @@ function limpaQuadro(){
 }
 limpaQuadro();
 
-setTimeout(() => {
-  let body = document.querySelector('body');
-  let random = Math.random() * 255;
-  body.backgroundColor = `rgb(${random},${random},${random})`;
-}, 200);
