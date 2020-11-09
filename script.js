bc3585f1e05c298e59006dd177b44199eee689bb
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-11-09 16:32:06 -0300

    finishing mandatory tasks

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 window.onload = function() {
   let board = document.getElementById("pixel-board");
   let palette = document.getElementById("color-palette");
-  let color;
-  let pixel;
-  let line;
-  let selected;
-  let currentColor;
+  let clearButton = document.getElementById("clear-board");
 
   let size = 5;
 
+  generatePalette(palette);
+  generateBoard(size, board);
+
+  clearButton.addEventListener("click", event => {
+    let pixels = document.getElementsByClassName("pixel");
+
+    for (pix of pixels) {
+      pix.style.backgroundColor = "white";
+    }
+  })
+}
+
+function generatePalette(palette) {
   for (let i = 0; i < 4; i += 1) {
-    color = document.createElement("div");
+    let color = document.createElement("div");
     color.addEventListener("click", event => {
-      for (let colPixel of palette.children) {
+      for (colPixel of palette.children) {
         colPixel.className = "color";
       }
       event.target.className = "color selected";
-
-      selected = document.getElementsByClassName("selected")[0];
-      currentColor = selected.style.backgroundColor;
     });
 
     if (palette.children.length === 0) {
@@ -26,31 +32,43 @@ window.onload = function() {
       color.style.backgroundColor = "black";
       palette.appendChild(color);
     } else {
-      let colGen = `${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)}`;
+      let colGen = generateRandomColor();
 
       color.className = "color";
       color.style.backgroundColor = `rgb(${colGen})`;
       palette.appendChild(color);
     }
   }
+}
 
-  selected = document.getElementsByClassName("selected")[0];
-  currentColor = selected.style.backgroundColor;
-
+function generateBoard(size, board) {
   for (let i = 0; i < size; i += 1) {
-    line = document.createElement("div");
-    line.className = "pixel-line";
-
-    for (let i = 0; i < size; i += 1) {
-      pixel = document.createElement("div");
-      pixel.className = "pixel";
-      pixel.style.backgroundColor = "white";
-      pixel.addEventListener('click', event => {
-        event.target.style.backgroundColor = currentColor;
-      })
-      line.appendChild(pixel);
-    }
-
+    let line = createLine(size);
     board.appendChild(line);
   }
+}
+
+function createLine(size) {
+  let currentColor;
+  let line = document.createElement("div");
+  line.className = "pixel-line";
+
+  for (let i = 0; i < size; i += 1) {
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.backgroundColor = "white";
+    pixel.addEventListener('click', event => {
+      currentColor = document.getElementsByClassName("selected")[0].style.backgroundColor;
+      event.target.style.backgroundColor = currentColor;
+    })
+
+    line.appendChild(pixel);
+  }
+
+  return line;
+}
+
+function generateRandomColor() {
+  return `${Math.ceil(Math.random() * 254)} , ${Math.ceil(Math.random() * 254)}
+   , ${Math.ceil(Math.random() * 254)}`;
 }
