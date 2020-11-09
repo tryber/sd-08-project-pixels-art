7bc2a7396b23f7403c5796c32ad61f0009816a37
Fabio Ferreira <fabio35547@gmail.com>
2020-11-09 20:44:54 -0300

    Requisito 10 - concluido

--- script.js
+++ script.js
@@ -34,44 +34,78 @@ window.onload = function () {
 
   // Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
   const btnClear = document.getElementById('clear-board');
-  const pixelColorList = document.querySelectorAll('.pixel');
-  
+    
   btnClear.addEventListener('click', clearBoard);
 
   function clearBoard() {
+    const pixelColorList = document.querySelectorAll('.pixel');
     for (let color = 0; color < pixelColorList.length; color += 1) {
       pixelColorList[color].style.backgroundColor = "white";
     }
   }
   // Fim do exercício 9
 
+  // Faça o quadro de pixels ter seu tamanho definido pelo usuário.
+  const btnGenerateBoard = document.getElementById('generate-board');
+  const txtBoardSize = document.getElementById('board-size');
+ 
+  btnGenerateBoard.addEventListener('click', generateBoard);
+
+  function generateBoard() {
+    if (txtBoardSize.value === '' || txtBoardSize.value < 5 || txtBoardSize.value > 50) {
+      alert('Board inválido!');
+    } else {
+      removeBoard();
+      createBoard(txtBoardSize.value);
+      clearBoard();
+    }
+    
+  }
+
+  function removeBoard() {
+    let size = pixelBoard.children.length;
+    for (let index = 0; index < size; index += 1) {
+      pixelBoard.removeChild(pixelBoard.firstElementChild);
+    }
+  }
+  //Fim do exercício 10
 };
 
 // A página deve possuir uma paleta de quatro cores distintas.
 const colorPaletteContainer = document.getElementById('color-palette');
 const colors = ['black', 'red', 'green', 'blue'];
-for (let paletteColor = 0; paletteColor < colors.length; paletteColor += 1) {
-  let colorPalette = document.createElement('div');
-  colorPalette.id = `color-pallet${paletteColor}`  
-  colorPalette.className = 'color';
-  colorPalette.style.backgroundColor = colors[paletteColor];
-  colorPaletteContainer.appendChild(colorPalette);
+
+function createColorPalette(colors) {
+  for (let paletteColor = 0; paletteColor < colors.length; paletteColor += 1) {
+    let colorPalette = document.createElement('div');
+    colorPalette.id = `color-pallet${paletteColor}`  
+    colorPalette.className = 'color';
+    colorPalette.style.backgroundColor = colors[paletteColor];
+    colorPaletteContainer.appendChild(colorPalette);
+  }
 }
+
+createColorPalette(colors);
 // Fim do exercício 2
 
 // A página deve possuir um quadro de pixels, com 25 pixels.
 const boardSize = 5;
 const pixelBoard = document.getElementById('pixel-board');
-for (let row = 0; row < boardSize; row += 1) {
-  let pixelRow = document.createElement('div');
-  pixelRow.className = 'pixel-row';
-  pixelBoard.appendChild(pixelRow);
-  let pixelCells = document.getElementsByClassName('pixel-row');
-  for (let cell = 0; cell < boardSize; cell += 1) {
-    let pixelCellContainer = pixelCells[row];
-    let pixelCell = document.createElement('div');
-    pixelCell.className = 'pixel';
-    pixelCellContainer.appendChild(pixelCell);
+
+function createBoard(boardSize) {
+  for (let row = 0; row < boardSize; row += 1) {
+    let pixelRow = document.createElement('div');
+    pixelRow.className = 'pixel-row';
+    pixelBoard.appendChild(pixelRow);
+    let pixelCells = document.getElementsByClassName('pixel-row');
+    for (let cell = 0; cell < boardSize; cell += 1) {
+      let pixelCellContainer = pixelCells[row];
+      let pixelCell = document.createElement('div');
+      pixelCell.className = 'pixel';
+      pixelCellContainer.appendChild(pixelCell);
+    }
   }
 }
+
+createBoard(boardSize);
 //Fim do exercício 4
