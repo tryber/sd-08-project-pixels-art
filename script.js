54d7745e4459fa9c546906db5e728a718a4baa80
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-09 15:13:21 -0300

    corrigindo requisito 40px

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function inputTinta1(){
-  console.log("botão preto clicado")
+function inputTinta1() {
+  console.log("botão preto clicado");
   document.getElementById("pinta-pixel").style.backgroundColor = "black";
 }
 
-function inputTinta2(){
-  console.log("botão azul clicado")
+function inputTinta2() {
+  console.log("botão azul clicado");
   document.getElementById("pinta-pixel").style.backgroundColor = "blue";
 }
 
-function inputTinta3(){
-  console.log("botão vermelho clicado")
+function inputTinta3() {
+  console.log("botão vermelho clicado");
   document.getElementById("pinta-pixel").style.backgroundColor = "red";
 }
 
-function inputTinta4(){
-  console.log("botão amarelo clicado")
+function inputTinta4() {
+  console.log("botão amarelo clicado");
   document.getElementById("pinta-pixel").style.backgroundColor = "yellow";
 }
 
-function limpar(){
-  console.log("botão limpar clicado")
+function limpar() {
+  console.log("botão limpar clicado");
   document.getElementById("pinta-pixel").style.backgroundColor = "white";
 }
