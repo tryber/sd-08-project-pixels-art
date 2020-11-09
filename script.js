3ac8659b65ab3ea1a8fdfa4bc816e4ffb3620db6
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-09 16:35:13 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,9 @@
+function createTable() {
+    for ( let i = 0; i < 25; i += 1) {
+        let spot = document.createElement('li');
+        spot.classList = 'pixel';
+        spot.style.backgroundColor = 'white';
+        document.querySelector('#pixel-board').appendChild(spot);
+    }
+}
+createTable();
