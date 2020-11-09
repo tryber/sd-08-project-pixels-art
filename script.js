3bddd9c88761131eae938fab4c9ea9b14d77566a
Daniel Leon <daniel.radialista@gmail.com>
2020-11-09 20:37:38 -0300

    desafio 4

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ function createPalletColors() {
         let colors = ['black', 'blue', 'orange', 'green'];
         divChild.style.backgroundColor = colors[i];
         divColorPallet.appendChild(divChild);
-
     }
-
-
 }
 
 createPalletColors();
+
+
+function createSquares() {
+    let squares = document.getElementById('pixel-board');
+    for (let i = 0; i < 5; i += 1) { // quantidade de linhas
+        let row = document.createElement('div');
+        for (let j = 0; j < 5; j += 1) { // quantidade de pixel
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            row.appendChild(pixel);
+        }
+        squares.appendChild(row);
+    }
+
+}
+
+createSquares();
+
