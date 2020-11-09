9919f022ef69da013697ed9eb51a52ad95fdf8dd
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-09 17:33:58 -0300

    cria a funçao de pintar os pixels com a cor selecionada

--- script.js
+++ script.js
@@ -13,14 +13,22 @@ function createBoard(num) {
     }
   }
 }
-
 createBoard(5);
 
-
+function createPalette() {
+  let firstColor = document.getElementById('firstColor');
+  let secondColor = document.getElementById('secondColor');
+  let thirdColor = document.getElementById('thirdColor');
+  let fourthColor = document.getElementById('fourthColor');
+  firstColor.style.backgroundColor = 'black';
+  secondColor.style.backgroundColor = 'green';
+  thirdColor.style.backgroundColor = 'orange';
+  fourthColor.style.backgroundColor = 'red';
+}
+createPalette();
 
 function selectedColor() {
   let colorPalette = document.querySelector('#color-palette');
-
 
   function changeStatus(event) {
     let selectedPalette = document.querySelector(".color.selected");
@@ -32,4 +40,17 @@ function selectedColor() {
   
 }
 selectedColor();
+
+function fillPixelColor() {
+  let pixelUnit = document.querySelector('#pixel-board');
+  
+
+  function changeColor(event) {
+    let selectedColor = document.querySelector('.selected');
+    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  };
+
+  pixelUnit.addEventListener('click', changeColor);
+}
+fillPixelColor();
 
