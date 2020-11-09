ae5bf86452437851d498e0a17d0809652f288177
Leandro Lachini <leandrolachini@gmail.com>
2020-11-09 20:30:53 -0300

    automatizacao e exe 9

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
+// Parametros de carregamento da pagina
 window.onload = function () {
  let classColorSelect = document.querySelector('.color');
  classColorSelect.className = 'color selected';   
     
 }
-    
 
+// Criando as cores da paleta
+function createColorPalette() {
+    
+    const containerColorPalette = document.getElementById('color-palette');
     let colorAvaliable = ['black', 'red', 'green', 'blue'];
     
- 
-    function setColors() {
-        let colorPallete = document.querySelectorAll('.color');
-        for (let index = 0; index < colorPallete.length; index+= 1) {
+    for (let index = 0; index < colorAvaliable.length; index+= 1) {
+        const divEachColor = document.createElement('div');
+        divEachColor.className = 'color';
+        divEachColor.style.backgroundColor = colorAvaliable[index];
+        containerColorPalette.appendChild(divEachColor);
+    }
+}
+createColorPalette();
+
+// Criando o quadro de pixels
+function squarePixels() {
+    let sizeSquarePixels = 5;
+    const containerSquarePixels = document.getElementById('pixel-board');
+   
+    // criando as colunas do quadrado
+    for (let iColumn = 0; iColumn < sizeSquarePixels; iColumn+= 1) {
+        const divColumnSquare = document.createElement('div');
+        divColumnSquare.className = 'pixels-set';
+        containerSquarePixels.appendChild(divColumnSquare);
+       
+        // criando as linhas do quadrado
+        const divColumn = document.getElementsByClassName('pixels-set');
+        for (let iLine = 0; iLine < sizeSquarePixels; iLine+= 1) {
+            const lineToColumn = divColumn[iColumn];
+            const divLineSquare = document.createElement('div');
+            divLineSquare.className = 'pixel';
+            lineToColumn.appendChild(divLineSquare);
             
-            colorPallete[index].style.backgroundColor = colorAvaliable[index];
         }
     }
-    setColors();
+}
+squarePixels();
+  
+// Definindo as cores; 
+    // function setColors() {
+    //     let colorPallete = document.querySelectorAll('.color');
+    //     for (let index = 0; index < colorPallete.length; index+= 1) {
+            
+    //         colorPallete[index].style.backgroundColor = colorAvaliable[index];
+    //     }
+    // }
+    // setColors();
 
+
+    // Definindo classe SELECTED;
     function setClassColor() {
         let colorPallete = document.querySelectorAll('.color');
         for (let index = 0; index < colorPallete.length; index+= 1) {
