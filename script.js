651e54a5646048838cf06de55a449d6309738965
Felipe Franca <felipefrancafj@gmail.com>
2020-11-09 16:47:30 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const paletaDeCores = document.getElementById("color-palette");
-const tabelaDeCores = ["black", "pink", "green", "red"];
+const tabelaDeCores = ["black", "blue", "green", "red"];
 for (let itens = 0; itens < tabelaDeCores.length; itens += 1) {
   let color = document.createElement("div");
   color.className = "color";
   color.style.background = tabelaDeCores[itens];
   color.style.display = "inline-block";
-  color.style.width = '100px'
-  color.style.border = 'solid 1px'
+  color.style.width = "100px";
+  color.style.border = "solid 1px";
   paletaDeCores.appendChild(color);
   let text = document.createElement("p");
   text.innerHTML = tabelaDeCores[itens];
-  text.style.textAlign = 'center'
-  text.style.color = '#fff'
+  text.style.textAlign = "center";
+  text.style.color = "#fff";
   color.appendChild(text);
+}
+const quadroDePixels = document.getElementById("pixel-board");
+quadroDePixels.style.width = "400px";
+quadroDePixels.style.height = "400px";
+quadroDePixels.style.textAlign = "center";
+for (let altura = 0; altura < 5; altura += 1) {
+  let quebraDeLinha = document.createElement("br");
+  for (let comprimento = 0; comprimento < 5; comprimento += 1) {
+    var pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.background = "white";
+    pixel.style.border = "solid black 1px";
+    pixel.style.display = "inline-block";
+    pixel.style.width = "40px";
+    pixel.style.height = "40px";
+    quadroDePixels.appendChild(pixel);
+  }
+  quadroDePixels.appendChild(quebraDeLinha);
 }
