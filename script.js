31fa3753c95d6c99631d816c0c0bf8399cf509a7
Bidfca <bruninhakjurugat11@gmail.com>
2020-11-09 19:01:32 -0300

    Let's see the progress again

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.onload = function () {
   let black = document.getElementsByClassName("color")[0];
   black.classList.add("selected");
+  fillWhite();
 }
 
 function createDiv(num, parent, div_class) {
   let parent_node = document.getElementById(parent);
 
-  while(num > 0) {
+  while (num > 0) {
     let div = document.createElement("div");
     div.className = div_class;
     parent_node.appendChild(div);
@@ -19,11 +20,16 @@ createDiv(25, "pixel-board", "pixel");
 
 let pallet = ["black", "green", "blue", "yellow"];
 let colors = document.getElementsByClassName("color");
-for(let index = 0; index < colors.length; index++) {
+for (let index = 0; index < colors.length; index++) {
   colors[index].style.backgroundColor = pallet[index];
 }
 
-let pixels = document.getElementsByClassName("pixels");
-for(pixel of pixels) {
-  pixel.style.backgroundColor = "rgb(255, 255, 255)";
+function fillWhite() {
+  let pixels = document.getElementsByClassName("pixel");
+  for (pixel of pixels) {
+    pixel.style.backgroundColor = "rgb(255, 255, 255)";
+  }
 }
+
+let button = document.getElementById("clear-board");
+button.addEventListener("click", fillWhite);
