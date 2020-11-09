debb7791c1775ee26230fca2e4465fff09e0e438
Pedro Amaral <amaralph1995@gmail.com>
2020-11-09 17:44:30 -0300

    Req 6, 7, 8 - cria funções pra mudar pixels de cor

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 window.onload = function () {
     let paletaCores = document.querySelectorAll('.color');
+
     let corPreta = paletaCores[0];
+    let corVermelha = paletaCores[1];
+    let corVerde = paletaCores[2];
+    let corAzul = paletaCores[3];
+
     corPreta.className += ' selected';
-    console.log(corPreta)
+    let selecionado = document.querySelector('.selected');
+    console.log(selecionado.style.backgroundColor)
+    
+    addEventListener('click', function(event){
+        let pixelClicado = event.target;
+        let cor = window.getComputedStyle(selecionado).backgroundColor;
+        if (pixelClicado.className === 'pixel') {
+            pixelClicado.style.backgroundColor = cor;
+        }
+    })
+
+    addEventListener('click', function(event){
+        if (event.target.className.split(' ')[0] == 'color') {
+            let corSelecionada = event.target;
+            let classe = selecionado.className.split(' ');
+            selecionado.className = classe[0] + " " + classe[1]
+            selecionado = corSelecionada;
+            selecionado.className += ' selected'
+        }
+    })
 }
