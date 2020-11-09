bc29b54e5c8aae772b2c6a56acb4915a9222e0db
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-09 15:22:47 -0300

    Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-for (let i=0; i<25; i+=1){
-    let pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    document.getElementById("pixel-board").appendChild(pixel);
+function criaPixels () {    
+    for (let i=0; i<25; i+=1){
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        document.getElementById("pixel-board").appendChild(pixel);
+    }
 }
+criaPixels();
+
+function selectColor (event) {
+    let selected = document.getElementsByClassName("selected");
+    selected[0].classList.remove("selected");
+    event.target.classList.add("selected");
+    console.log("Selecionou");
+}
+document.getElementById("color-palette").addEventListener("click",selectColor);
