dab96c564c8bb84545e03096c36da52858858ac6
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-09 14:30:45 -0300

    add button clear e primeiras funções js

---
+++ script.js
@@ -0,0 +1,24 @@
+function inputTinta1(){
+  console.log("botão preto clicado")
+  document.getElementById("pinta-pixel").style.backgroundColor = "black";
+}
+
+function inputTinta2(){
+  console.log("botão azul clicado")
+  document.getElementById("pinta-pixel").style.backgroundColor = "blue";
+}
+
+function inputTinta3(){
+  console.log("botão vermelho clicado")
+  document.getElementById("pinta-pixel").style.backgroundColor = "red";
+}
+
+function inputTinta4(){
+  console.log("botão amarelo clicado")
+  document.getElementById("pinta-pixel").style.backgroundColor = "yellow";
+}
+
+function limpar(){
+  console.log("botão limpar clicado")
+  document.getElementById("pinta-pixel").style.backgroundColor = "white";
+}
