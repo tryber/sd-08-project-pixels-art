dd127052f87d57060cb38b4638f6f0a64a88af99
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-09 18:32:41 -0300

    req-7 selected funcionando

--- script.js
+++ script.js
@@ -8,21 +8,6 @@ function nameColor(colored){
   return colorBox;
 }
 
-// const colored = document.querySelector('.color');
-// function toggleColor(target){
-//         let colorOfEvent = nameColor();
-//     let colorOfSelector = nameColor(document.querySelector('.selected'));
-//     console.log(colorOfEvent);
-//     console.log(colorOfSelector);
-//     if (document.querySelector('.selected') != event.target){
-//       document.querySelector('.selected').className = "color box "+ colorOfSelector;
-//       event.target.className = "color box selected "+colorOfEvent ;
-//     } else{
-//       console.log("cor já selecionada");
-//     }
-// }
-// colored.addEventListener.children('click',toggleColor(target))
-
 
   const colored = document.querySelectorAll('.color');
   for (let i = 0; i < colored.length ; i++) {
@@ -37,4 +22,17 @@ function nameColor(colored){
       console.log("cor já selecionada");
     }
   });
+  }
+
+  const matrix = document.querySelectorAll('.pixel');
+  for (let i = 0; i <= matrix.length; i++) {
+  matrix[i].addEventListener('click',function(event){
+    let colorOfSelector = nameColor(document.querySelector('.selected'));
+    console.log(matrix[i].classList[2]+" =?= "+colorOfSelector)
+    if(matrix[i].classList[2] != colorOfSelector){
+      console.log(matrix[i].className)
+      matrix[i].className = "box pixel " + colorOfSelector ;
+      console.log(matrix[i].className)
+    }
+  })
   }
