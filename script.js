281f842ffd432ec956f30b2e662fc8237fa7fb92
Ruben Albertassi <albertassi88@gmail.com>
2020-11-09 15:26:25 -0300

    Criacao da paleta de cores

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let body = document.querySelector("body");
+let div = document.createElement("div");
 
+body.appendChild(div);
 let h1 = document.createElement("h1");
 h1.id = "title";
 h1.innerHTML = "Paleta de Cores";
-body.appendChild(h1);
+div.appendChild(h1);
+
+let ul = document.createElement("ul");
+ul.id = "color-palette";
+div.appendChild(ul);
+
+for (let index=0; index<4; index++){
+    let color = ["black", "green", "blue", "red"];
+    let li = document.createElement("li");
+    li.className = "classe color";
+    ul.appendChild(li);
+
+    let colorIndex = document.querySelectorAll(".classe");
+    colorIndex[index].style.backgroundColor = color[index];
+}
+
+
+
