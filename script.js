52b8452129e4eab50fd42cc5d716480b3d1dceee
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-11-09 16:19:38 -0300

    Adicionado o requisito 6

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let pixel_board = document.querySelector('#pixel-board');
-let black = document.querySelector(".color .black");
-let red = document.querySelector("color .red");
-let green = document.querySelector("color .green");
-let yellow = document.querySelector("color .yellow");
+window.onload = function () {
+    let pixel_board = document.querySelector('#pixel-board');
+    let black = document.querySelector(".black");
+    black.className += ' selected';
 
-for (let index = 1; index <= 5; index++) {
+    let red = document.querySelector(".red");
+    let green = document.querySelector(".green");
+    let yellow = document.querySelector(".yellow");
+
     for (let index = 1; index <= 5; index++) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
+        for (let index = 1; index <= 5; index++) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
 
-        pixel_board.appendChild(pixel);
+            pixel_board.appendChild(pixel);
+        }
     }
+
+    black.addEventListener('click', selectColor);
+    red.addEventListener('click', selectColor);
+    green.addEventListener('click', selectColor);
+    yellow.addEventListener('click', selectColor);
 }
 
-function selectColor() {
+function selectColor(event) {
+    let currentSelected = document.querySelector('.selected');
+    currentSelected.className = currentSelected.className.replace(" selected", "");
 
+    event.target.className += ' selected';
 }
