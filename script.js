343ddc5fe396baff36499a6f17e99ee20765ff00
Patrícia <silva.patricia.v@gmail.com>
2020-11-09 18:49:09 -0300

    verificando requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,12 @@
+function createDivs() {
+  const arrayColor = ["black", "purple", "red", "green"];
+  const palletDiv = document.getElementById("color-palette");
+  for (let index = 0; index < arrayColor.length; index += 1) {
+    let divColor = arrayColor[index];
+    const myDivs = document.createElement("div");
+    palletDiv.appendChild(myDivs);
+    myDivs.className = "color";
+    myDivs.style.backgroundColor = divColor;
+  }
+}
+createDivs();
