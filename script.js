66c9ef29f19f827682b1cd8024cc20fc3af8a645
Douglas Ferreira <dodofp.df@gmail.com>
2020-11-09 15:08:50 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+
+    
+
+    function createDiv(number){
+        for(let i = 0; i < (number*number); i+=1){
+            const pixelQuadro = document.querySelector('#pixel-board');
+            const input = ''
+            const divCaixas = document.createElement('div');
+            divCaixas.className ="pixel";
+            divCaixas.innerHTML = input;
+            pixelQuadro.appendChild(divCaixas);
+            divCaixas.style.backgroundColor = 'white';
+
+
+        }
+
+    }
+    createDiv(5);
+
+
+
+
+
+
+
+}
