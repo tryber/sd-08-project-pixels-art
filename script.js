cd784c6ae8e0af4946ef5eda09a787cfbca8fb07
Erick Santos <erickgaves@gmail.com>
2020-11-09 18:51:36 -0300

    entregando requisitos 2

---
+++ script.js
@@ -0,0 +1,29 @@
+// Alterando a cor da paleta 01
+function cor01 () {
+    const cor01 = document.getElementsByClassName('color')[0].style;
+    cor01['backgroundColor'] = 'black';
+}
+
+cor01();
+// Alterando a cor da paleta 02
+function cor02 () {
+    const cor02 = document.getElementsByClassName('color')[1].style;
+    cor02['backgroundColor'] = '#735236'
+}
+
+cor02();
+// Alterando a cor da paleta 03
+function cor03 () {
+    const cor03 = document.getElementsByClassName('color')[2].style;
+    cor03['backgroundColor'] = '#D9A384'
+}
+
+cor03();
+
+// Alterando a cor da paleta 04
+function cor04 () {
+    const cor04 = document.getElementsByClassName('color')[3].style;
+    cor04['backgroundColor'] = '#D94A4A'
+}
+
+cor04();
