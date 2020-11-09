ab79a53dff905d9a2ebb6d74e0bfa5c953591300
antonioarieiro <sfgfffs@gmail.com>
2020-11-09 16:19:11 -0300

    requisito 2

--- script.js
+++
@@ -1,8 +0,0 @@
-function colorPaletBackground (){
-    let colorBkgd = document.querySelectorAll('.color');
-    colorBkgd.style.border = "thick solid #0000FF";
-    
-    
-    
-}
-colorPaletBackground()
