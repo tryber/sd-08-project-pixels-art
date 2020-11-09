694577aea7ca9b9dab178629e8ec70cf33ce8e68
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-09 19:17:16 -0300

    Requisito 07 finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Requisito 06
 //Função que adiciona a classe selected na cor inicial 'preta'
 //Raciocinio encontrado em uma discussão no StackoverFlow (https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro)
-function addSelect (){
+function addSelect() {
     document.querySelector('#color1').classList.add("selected");
 }
 addSelect();
@@ -11,19 +11,10 @@ addSelect();
 //Recuperando cores no html
 let classColors = document.querySelectorAll('.color');
 //Percorrendo cada cor, e adicionando o evento de click
-for (let index = 0; index < classColors.length; index +=1){
-    classColors[index].addEventListener('click', function(){
-        //classColors[index].classList.add("selected");
-        let element = classColors[index];
-        let tagClass = element.getAttribute('class');
-        if (tagClass == 'color'){
-            element.setAttribute('class', 'color selected')
-           
-        }  
-        if (tagClass === 'color selected'){
-            element.setAttribute('class', 'color')
-        }
-       
+for (let index = 0; index < classColors.length; index += 1) {
+    let tagClass = classColors[index];
+    tagClass.addEventListener('click', function (event) {
+        event.target.classList.toggle('selected')
     });
 }
 
