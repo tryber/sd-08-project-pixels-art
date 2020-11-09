23a96e364a11e6ae0d0df4794facf1ced20955b6
liisaambrosen <liisa.ambrosen@gmail.com>
2020-11-09 15:22:30 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,10 @@
+function coresPaleta () {
+const elementosPaleta = document.getElementsByClassName("color");
+
+elementosPaleta[0].style.backgroundColor = "black";
+elementosPaleta[1].style.backgroundColor = "orange";
+elementosPaleta[2].style.backgroundColor = "yellow";
+elementosPaleta[3].style.backgroundColor = "cyan";
+}
+
+coresPaleta ();
