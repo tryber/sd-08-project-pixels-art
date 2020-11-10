f47d964b0a16bc926614bf4c0bc78ba4487db31d
UmberBn <humberto-bandeira@hotmail.com>
2020-11-09 23:32:19 -0400

    Seperando funções corretamente, criação da função para requisito 6

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-window.onload = function(){
-        function createColorPallete() {
-            let getFrames = document.querySelectorAll('.color')
-            let colorClass = ['black', 'red', 'green', 'blue']
-            for (let index = 0; index < getFrames.length; index += 1)
-            {   let color = colorClass[index]
-                getFrames[index].classList.add(color);
-            }
-        }
+window.onload = function () {
 
-        createColorPallete()
+    function firstColorSelected() {
+        let initialColorFrame = document.querySelector('.color')
+        initialColorFrame.classList.add('selected')
     }
+    firstColorSelected()
+}
+
+function createColorPallete() {
+    let getFrames = document.querySelectorAll('.color')
+    let colorClass = ['black', 'red', 'green', 'blue']
+    for (let index = 0; index < getFrames.length; index += 1) {
+        let color = colorClass[index]
+        getFrames[index].classList.add(color);
+    }
+}
+
+createColorPallete()
