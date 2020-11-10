5bdab4cb7383568ad6b8c641dc3aac5dfc874f1b
jnpefo <jnpefo@gmail.com>
2020-11-09 22:26:05 -0300

    Req 6 tentativa 1

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let selecionado = document.querySelectorAll(".color");
 
-for (let i = 0; i <selecionado.length; i += 1){
-    let  atual = selecionado[i];
-    if(atual.className("selecte")){
-
-    }
+for (let i = 0; i < selecionado.length; i += 1) {
+    let atual = selecionado[i];
+    atual.addEventListener('click', function (event) {
+        for (let j = 0; j < selecionado.length; j += 1) {
+            let marcado = selecionado[j];
+            if (marcado.classList.contains('selected')) {
+                marcado.classList.remove('selected');
+            }
+        }
+        event.target.classList.add('selected');
+    })
 }
+
