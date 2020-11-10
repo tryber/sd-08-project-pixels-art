671e5935da1630e9d4c4ac89fdfd7ab5710c7f94
Giovanna Betti <giovannabetti@gmail.com>
2020-11-10 12:05:09 -0300

    Criando condicional para selecao de cores

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ const preto = document.getElementsByClassName('preto');
 const amarelo = document.getElementsByClassName('amarelo');
 const laranja = document.getElementsByClassName('laranja');
 const vermelho = document.getElementsByClassName('vermelho');
+
+const cores = document.querySelectorAll('.color');
+
+function corDeFundo() {
+    if (cores.className === 'preto') {
+        cores.style.backgroundColor = 'black'
+    } else if (cores.className === 'amarelo') {
+        cores.style.backgroundColor = 'yellow'
+    } else if (cores.className === 'laranja') {
+        cores.style.backgroundColor = 'orange'
+    } else {
+        cores.style.backgroundColor = 'red'
+    }
+};
