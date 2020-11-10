bb2c096e66735a53f1d4f918ba3bc97f8e4799e2
wagnerberna <cabelo29007@gmail.com>
2020-11-10 09:46:18 -0300

    Req. 04 e 05

---
+++ script.js
@@ -0,0 +1,10 @@
+function createBtn(nome, id) {
+  let newBtn = document.createElement('button');
+  newBtn.innerText = nome;
+  newBtn.id = id;
+  newBtn.className = btn;
+  document.getElementById('button-container').appendChild(newBtn);
+}
+createBtn('limpar', 'clear-board');
+
+createBtn('VQV', 'generate-board');
