c524135e25f5dff67986945db8d3b4bd11aef016
wagnerberna <cabelo29007@gmail.com>
2020-11-10 10:09:07 -0300

    javaScript inicio

---
+++ script.js
@@ -0,0 +1,11 @@
+function createBtn(nome, id) {
+  const btn = document.createElement('button');
+  btn.innerText = nome;
+  btn.id = id;
+  btn.className = btnClass;
+  document.getElementById('button-container').appendChild(btn);
+}
+
+createBtn("limpar", "clear-board");
+
+createBtn("VQV", "generate-board");
