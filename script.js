b554f589b72a26cfd761e834928c91b4e8204797
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-10 01:16:38 -0300

    Resolving problems codeclimate

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ window.onload = function () {
     pixel[0].style.backgroundColor = 'black';
     const index = [];
     while (index.length <= 3) {
-      const aux = Math.floor(Math.random() * (7 - 1 + 1));
+      const aux = Math.floor((Math.random()) * ((7 - 1 + 1)));
       if (index.indexOf(aux) === -1) index.push(aux);
     }
     for (let i = 1; i < pixel.length; i += 1) {
