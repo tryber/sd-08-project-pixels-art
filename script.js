1408d9ae31ea61120f58a0c79d72bb41c0326fdc
Uilton Rocha <itoclick@gmail.com>
2020-11-09 23:25:22 -0300

    inicio do projeto com os arquivos em branco

--- script.js
+++ script.js
@@ -10,16 +10,32 @@ window.onload =criaBox();
   }  
 };*/
 function criaBox() {
-  for (let index =0;index < 4; index +=1){
+  //for (let index =0;index < 4; index +=1){
     let para = document.createElement("div");
     let element = document.getElementById("hdr");
-  para.innerHTML = "cor";
+  para.innerHTML = "preto";
   para.className = "color";
-    if(index=0);{ para.id ="vpreto"
-    elseif (index =1); {para.id="vermelho"}
-    elseif (index =2); {para.id="azul"}
-    para.id = "verde"
-  }
+  para.id = "preto";  
   element.appendChild(para);
-  }
+  
+  let parav = document.createElement("div");
+  let elementv = document.getElementById("hdr");
+  parav.innerHTML = "vermel";
+  parav.className = "color";
+  parav.id = "vermelho";  
+  elementv.appendChild(parav);
+
+  let parad = document.createElement("div");
+  let elementd = document.getElementById("hdr");
+  parad.innerHTML = "verde";
+  parad.className = "color";
+  parad.id = "verde";  
+  elementd.appendChild(parad);
+
+  let paraz = document.createElement("div");
+  let elementz = document.getElementById("hdr");
+  paraz.innerHTML = "azul";
+  paraz.className = "color";
+  paraz.id = "azul";  
+  elementz.appendChild(paraz);
 };
