d4cc47a0f258b39a511fcd57b35871dc0c48c463
enicio <jessandro42@gmail.com>
2020-11-10 12:10:31 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-document.querySelector('#black').className= "color selected"
+document.querySelector('#black').className = "color selected"
+
+
+let RGB =  [];
+
+
+function generateRandomicRGBColors() {
+ for (let index = 0; index < 3; index += 1 ) {
+	let numberOfRgbColor = Math.random() * 255
+	RGB[index] = parseInt(numberOfRgbColor)
+ }
+ return RGB
+}
+
+
+function setRandomColor () {
+	for (let index = 1; index < 4; index += 1) {
+		let RGB = generateRandomicRGBColors()
+		document.querySelectorAll('.color')[index].style.background = `rgb(${RGB[0]}, ${RGB[1]}, ${RGB[2]})`
+	}
+}
+setRandomColor ()
 
 
 const colorPalette = document.getElementById('color-palette')
@@ -7,10 +28,11 @@ colorPalette.addEventListener('click', selectColor)
 
 function selectColor(e) {
   const color = e.target.style.backgroundColor;
-  let colorSelected = document.getElementById(e.target.id)
-  colorSelected.className = "color selected"
+	let colorSelected = document.getElementById(e.target.id)
+	colorSelected.className = "color selected"
 
-switch (color) {
+//switch (color) {
+	switch (colorSelected.id) {
   case 'red' :
     colorSelected.className = "color selected"
     document.querySelector('#black').className= "color"
@@ -38,7 +60,7 @@ switch (color) {
     default:
 			break;
             //console.log("ops")
-   }
+    }
 }
 
 const pixelToPaint = document.querySelector('#pixel-board')
@@ -60,5 +82,30 @@ function colorPixel(e) {
 	for (let index = 0; index < lengthOFTable; index += 1) {
 		document.getElementsByClassName('pixel')[index].style.background = "white"
 	}
-
  }
+
+
+
+//  Cria a tabela de forma dinamica
+
+ let newRow = document.createElement('TR')
+
+ let newColumn = document.createElement('TD')
+ let matrix = 5;
+
+
+ for (let index = 0; index < matrix; index += 1) {
+	newRow = document.createElement('TR')
+	document.querySelector('#table-of-pixel').appendChild(newRow)
+	createLineOfTable(matrix)
+ }
+
+ function createLineOfTable (matrix) {
+	let ops = document.querySelector('#table-of-pixel').lastChild
+	for (let index = 0; index < matrix; index += 1){
+		newColumn = document.createElement('TD')
+		ops.appendChild(newColumn).className = 'pixel'
+	 	}
+ }
+
+
