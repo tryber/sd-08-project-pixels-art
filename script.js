763141243ab54525ba34d8be8869d2cce640fdc2
jnpefo <jnpefo@gmail.com>
2020-11-10 00:26:05 -0300

    Req 8 tentativa 1

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let selecionado = document.querySelectorAll(".color");
+// Req 7 
+const corSelecionada = document.querySelectorAll('.color');
 
-for (let i = 0; i < selecionado.length; i += 1) {
-    let atual = selecionado[i];
+for (let i = 0; i < corSelecionada.length; i += 1) {
+    const atual = corSelecionada[i];
     atual.addEventListener('click', function (event) {
-        for (let j = 0; j < selecionado.length; j += 1) {
-            let marcado = selecionado[j];
+        for (let j = 0; j < corSelecionada.length; j += 1) {
+            const marcado = corSelecionada[j];
             if (marcado.classList.contains('selected')) {
                 marcado.classList.remove('selected');
             }
         }
         event.target.classList.add('selected');
-    })
+    });
 }
+const allPixels = document.querySelectorAll('.pixel');
 
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('pixel')) {
+        let cor = document.querySelector('.selected');
+        let pickedColor = window.getComputedStyle(cor, null).getPropertyValue("background-color");
+        event.target.style.backgroundColor = pickedColor;
+    }
+});
