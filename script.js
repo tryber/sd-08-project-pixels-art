91096c81bfba5130a66ad7ccda033fe0b20b5d24
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-09 21:10:32 -0300

    Requisito 07 finalizado

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ let classColors = document.querySelectorAll('.color');
 //Percorrendo cada cor, e adicionando o evento de click
 for (let index = 0; index < classColors.length; index += 1) {
     let tagClass = classColors[index];
+
     tagClass.addEventListener('click', function (event) {
-        event.target.classList.toggle('selected')
+        for (let i =0; i < classColors.length; i+=1){
+            if (classColors[i].classList.contains('selected')){
+                classColors[i].classList.remove('selected');
+            }
+        }
+        event.target.classList.add('selected');
+        
     });
 }
 
-//classColors[index].classList.add("selected");
+
+
