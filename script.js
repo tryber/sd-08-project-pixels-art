886f284f04d7a3070466541baf470569bf1d1245
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-10 17:13:43 -0300

    Até requisito 5

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // criar 5 tr's e 5 td's em cada TR
 
-function creatingLinesandCells() {
+function creatingLinesAndCells() {
     const table = document.getElementById('pixel-board');
     for (let i = 0; i < 5; i += 1) {
-        const line = document.createElement('tr');
+        const line = document.createElement('div');
+        line.className = "linha";
+        table.appendChild(line);
+
+        for (let j = 0; j < 5; j += 1) {
+            const cell = document.createElement('div');
+            line.appendChild(cell);
+            cell.className = "pixel";
+        }
 
     }
 }
+
+creatingLinesAndCells();
