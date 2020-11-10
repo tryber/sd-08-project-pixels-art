b4a03b26953899b829f968b4a4cb05ddde46973b
Tandy <tandyfw@gmail.com>
2020-11-10 09:09:15 -0300

    Refatoraçao requisito 7 e requisito bonus 12

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ window.onload = function () {
 }
 
 // Requisito 10.
-
+function hexaRandom (num){
+  let random = Math.floor(Math.random() * num);
+  return random;
+}
+function randomColor() {
+  let hexadecimal = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  for(let index = 0; index < 6; index += 1) {
+    let randomNum = hexaRandom(16);
+    cor += hexadecimal[randomNum];
+    if(cor === "#FFFFFF"){
+      index = 0;
+    }
+  }
+  return cor;
+}
 
 //Requisito 02 - A página deve possuir uma paleta de quatro cores distintas.
 function initialColors () {
   let colors = document.querySelectorAll('.color');
   // Requisito 03 - A cor preta deve ser a primeira na paleta de cores.
   colors[0].style.backgroundColor = 'black';
-  colors[1].style.backgroundColor = 'blue';
-  colors[2].style.backgroundColor = 'yellow';
-  colors[3].style.backgroundColor = 'green';
+  colors[1].style.backgroundColor = randomColor();
+  colors[2].style.backgroundColor = randomColor();
+  colors[3].style.backgroundColor = randomColor();
 }
 initialColors();
 
@@ -31,34 +46,42 @@ pixelBoard();
 
 // Requisito 07 - Ao clicar em uma das cores da paleta, a cor selecionada é que vai ser usada para preencher os pixels no quadro.
 function selectColor () {
-  let element1 = document.querySelectorAll('.color')[0];
-  let element2 = document.querySelectorAll('.color')[1];
-  let element3 = document.querySelectorAll('.color')[2];
-  let element4 = document.querySelectorAll('.color')[3];
+  let filhos = document.getElementById('color-palette').children;
+  // let element1 = document.querySelectorAll('.color')[0];
+  // let element2 = document.querySelectorAll('.color')[1];
+  // let element3 = document.querySelectorAll('.color')[2];
+  // let element4 = document.querySelectorAll('.color')[3];
   let palette = document.querySelector('#color-palette');
   
   palette.addEventListener('click', function(event) {
-    if (event.target.style.backgroundColor === 'black' && event.target.className === 'color') {
-      event.target.className = 'color selected';
-      element2.className = 'color';
-      element3.className = 'color';
-      element4.className = 'color';
-    } else if (event.target.style.backgroundColor === 'blue' && event.target.className === 'color') {
-      event.target.className = 'color selected';
-      element1.className = 'color';
-      element3.className = 'color';
-      element4.className = 'color';
-    } else if (event.target.style.backgroundColor === 'yellow' && event.target.className === 'color') {
-      event.target.className = 'color selected';
-      element1.className = 'color';
-      element2.className = 'color';
-      element4.className = 'color';
-    } else if (event.target.style.backgroundColor === 'green' && event.target.className === 'color') {
-      event.target.className = 'color selected';
-      element1.className = 'color';
-      element2.className = 'color';
-      element3.className = 'color';
+    for(let index = 0; index < filhos.length; index += 1) {
+      if(filhos[index] === event.target) {
+        event.target.className = 'color selected';
+      } else {
+        filhos[index].className = 'color';
+      }
     }
+    // if (event.target.style.backgroundColor === 'black' && event.target.className === 'color') {
+    //   event.target.className = 'color selected';
+    //   element2.className = 'color';
+    //   element3.className = 'color';
+    //   element4.className = 'color';
+    // } else if (event.target.style.backgroundColor === 'blue' && event.target.className === 'color') {
+    //   event.target.className = 'color selected';
+    //   element1.className = 'color';
+    //   element3.className = 'color';
+    //   element4.className = 'color';
+    // } else if (event.target.style.backgroundColor === 'yellow' && event.target.className === 'color') {
+    //   event.target.className = 'color selected';
+    //   element1.className = 'color';
+    //   element2.className = 'color';
+    //   element4.className = 'color';
+    // } else if (event.target.style.backgroundColor === 'green' && event.target.className === 'color') {
+    //   event.target.className = 'color selected';
+    //   element1.className = 'color';
+    //   element2.className = 'color';
+    //   element3.className = 'color';
+    // }
   });
 
   // let colorselection = document.querySelectorAll('.color').forEach (function (item) {
