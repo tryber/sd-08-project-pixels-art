ad0831c901ae55d53a4f24182dd28f6caa684404
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2020-11-10 16:37:08 -0300

    Color palette by function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
 const repeat = (what, times) => {
     for (let index = 0; index < times; index++) what();
+}
+
+let color_palette = document.querySelector('div#color-palette');
+const generatePalette = (colors) => {
+    for (let color of colors) {
+        let color_pixel = document.createElement('div');
+        color_pixel.className = 'color';
+        color_pixel.id = color;
+        color_pixel.style.backgroundColor = color;
+        color_palette.appendChild(color_pixel);
+    }
+    let first_element = document.querySelector('div#color-palette').firstElementChild;
+    first_element.classList.add('selected');
 }
 
 const createPixel = () => {
@@ -26,4 +40,6 @@ const createPixelGrid = (height, width) => {
     }, height);
 }
 
+let custom_colors = ['red', 'lime', 'blue'];
+generatePalette(['black'].concat(custom_colors));
 createPixelGrid(5, 5);
