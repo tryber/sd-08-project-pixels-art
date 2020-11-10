1e72c9f8c0104744bc6232327da1ace847597ce3
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-11-10 16:01:54 -0300

    Primeiro commit com index script e style criado

---
+++ script.js
@@ -0,0 +1,9 @@
+// criar 5 tr's e 5 td's em cada TR
+
+function creatingLinesandCells() {
+    const table = document.getElementById('pixel-board');
+    for (let i = 0; i < 5; i += 1) {
+        const line = document.createElement('tr');
+
+    }
+}
