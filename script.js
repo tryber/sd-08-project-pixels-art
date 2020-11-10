5898e03cd55f729d98dfe920f216ec7fc0f612ac
Ruan Miguel <ruanmiguelps@gmail.com>
2020-11-10 15:09:28 -0300

    tentando arrumar

--- script.js
+++ script.js
@@ -117,21 +117,3 @@ yellowCC.addEventListener('click', function (event) {
 toogleColors4()
 
 
-function addColor (){
-    
-    const board = querySelector('#pixel-board')
-
-    board.addEventListener('click', function(event){
-
-        const selected = document.querySelector('.color selected')
-
-        if( selected.style.backgroundColor !== null)
-         {
-
-        event.target.style.backgroundColor = selected.style.backgroundColor
-        }
-    })}
-
- 
-
-addColor()
