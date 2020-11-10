ad2d517c06659d1e8fe4b5e21bbb783113ee7cb9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-10 15:38:33 -0300

    req1: Cria h1

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body');
+
+function createElements(){
+    let h1 = document.createElement('h1');
+    h1.id = 'title';
+    h1.innerHTML = 'Paleta de Cores';
+    body.appendChild(h1);
+}
+
+createElements();
