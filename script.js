c9dab120d009454cd3e95c266b98be148f696dc3
Pedro Alves <ped.alm2709@gmail.com>
2020-11-09 21:07:14 -0300

    incluindo paleta de cores

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function createDiv() {
   let pallete = document.getElementById('color-palette');
-
   for (let i = 1; i < 5; i+=1) {
     let colorDiv = document.createElement('div');
     colorDiv.className = 'color';
@@ -8,6 +7,15 @@ function createDiv() {
   }
 }
 createDiv();
+
+function fillColor() {
+  let rgb = ['black', 'red', 'green', 'blue'];
+  for (let j = 0; j < rgb.length; j++) {
+    let someColor = document.getElementsByClassName('color')[j];
+    someColor.style.backgroundColor = rgb[j];
+  }
+}
+fillColor();
 
 
 
