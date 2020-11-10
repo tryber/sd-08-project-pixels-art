5506e121a462e5dffc41a01c5387f3e0c113e23f
Rafael <rafaelmaltez89@gmail.com>
2020-11-10 12:10:52 -0300

    correção de erros do codeclimate

--- script.js
+++ script.js
@@ -1,86 +1,84 @@
-function criaCores(){
+function criaCores() {
   const paleta = document.getElementById('color-palette');
-  for (index = 1; index <=4; index +=1){
-    const cor = document.createElement('div');    
+  for (index = 1; index <= 4; index += 1){
+    const cor = document.createElement('div');
     paleta.appendChild(cor);
     cor.id = `color${index}`;
-    cor.className = 'color'; 
-    }
-    let cores = document.getElementsByClassName('color');
-    cores[0].className = 'color selected';
-    for (let index =1; index <4; index +=1){
-      cores[index].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
-      // copiei a função de numero aleatorio do site https://www.w3schools.com/js/js_random.asp
-    };
-  }  
+    cor.className = 'color';
+  }
+  let cores = document.getElementsByClassName('color');
+  cores[0].className = 'color selected';
+  for (let index = 1; index < 4; index += 1){
+    cores[index].style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+    // copiei a função de numero aleatorio do site https://www.w3schools.com/js/js_random.asp
+  }
+  }
 
 criaCores();
 
 const board = document.getElementById('pixel-board');
 
-function criaPixels(numeroDePixels){  
-  for (let index = 0; index < Math.pow(numeroDePixels, 2); index += 1){ 
+function criaPixels(numeroDePixels) {
+  for (let index = 0; index < (numeroDePixels ** 2); index += 1) {
     const pixel = document.createElement('div');
-    pixel.className = 'pixel';          
+    pixel.className = 'pixel';
     board.appendChild(pixel);
   }
   board.style.maxWidth = (numeroDePixels * 40) + 'px';
 }
 criaPixels(5);
-//função Math.pow retirada de https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
 
 function selecionaCor(){
-  const cores = document.querySelectorAll('.color');  
-    for (index = 0; index < cores.length; index +=1){
-      cores[index].addEventListener('click', mudaClasse)
-    }
-    function mudaClasse(event){
-      for (index = 0; index<cores.length; index+=1){
-        cores[index].className = 'color';
-      }
+  const cores = document.querySelectorAll('.color');
+  for (let index = 0; index < cores.length; index += 1) {
+    cores[index].addEventListener('click', mudaClasse)
+  }
+  function mudaClasse(event){
+    for (let index = 0; index < cores.length; index += 1) {
+      cores[index].className = 'color';
+  }
     event.target.className = 'color selected';
-    }
+  }
 }
 selecionaCor();
 
-let pixels = document.getElementsByClassName('pixel');
+const pixels = document.getElementsByClassName('pixel');
 
-function colorePixel(){  
-  for (index = 0; index < pixels.length; index +=1){
-  pixels[index].addEventListener('click', mudaCor); 
+function colorePixel() {  
+  for (let index = 0; index < pixels.length; index +=1) {
+  pixels[index].addEventListener('click', mudaCor);
   }
-  function mudaCor(event){
-    let selecionada = document.querySelector('.selected');
-    let cor = window.getComputedStyle(selecionada, null).getPropertyValue("background-color");
+  function mudaCor(event) {
+    const selecionada = document.querySelector('.selected');
+    const cor = window.getComputedStyle(selecionada, null).getPropertyValue("background-color");
     event.target.style.backgroundColor = cor;
   }
 }
 colorePixel();
 
-function limpaQuadro(){
-  let botao = document.querySelector('button');
+function limpaQuadro() {
+  const botao = document.querySelector('button');
   botao.addEventListener('click', fundoBranco)
   function fundoBranco(){
-    for (index = 0; index < pixels.length; index +=1){
+    for (let index = 0; index < pixels.length; index +=1) {
     pixels[index].style.backgroundColor = 'white';
     }
   }
 }
 limpaQuadro();
 
-
-function quadroUsuario(){  
-let vqv = document.getElementById('generate-board');
+function quadroUsuario() {
+const vqv = document.getElementById('generate-board');
 vqv.addEventListener('click', criaQuadro);
-function criaQuadro(){
+function criaQuadro() {
   removePixels();
   let input = document.getElementById('board-size');
   let value = parseInt(input.value);
-  if(!value){
-    alert("Board inválido!")   
-  }else if (value > 50){
+  if(!value) {
+    alert("Board inválido!");
+  } else if (value > 50) {
     value = 50;
-  }else if (value < 5){
+  } else if (value < 5) {
     value = 5;
   }
   criaPixels(value);
@@ -89,7 +87,7 @@ function criaQuadro(){
 }
 quadroUsuario();
 
-function removePixels(){
+function removePixels() {
   board.innerHTML = '';
 }
 // Essa função foi baseada nessa respostas do Stack Overflow: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
