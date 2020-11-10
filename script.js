52ffd09d7860dbc892bc22eaa1b006ed4ebef577
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-10 17:05:45 -0300

    Red 8 e adiciona referencias

--- script.js
+++ script.js
@@ -2,20 +2,32 @@ window.onload = () => {
     document.getElementsByClassName('color')[0].classList.add('selected');
 };
 
-
-
-
 let selectedColor = document.getElementsByClassName('color');
-
 for(let x of selectedColor){
     x.addEventListener('click', (event) => {
         let selectColor = document.querySelector('.selected');
-        if(event.target.classList.contains('selected') == false){
+        if(x.classList.contains('selected') == false){
+            // Ref element.classList -> https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
             selectColor.classList.remove('selected');
             x.classList.add('selected');
         }
     })
 }
+
+
+let pixels = document.getElementsByClassName('pixel');
+
+for(let x of pixels){
+    x.addEventListener('click', (pixelSelected) => {
+        let color = document.querySelector('.selected');
+        color = window.getComputedStyle(color,null);
+        // Ref window.getComputedStyle() -> https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+        pixelSelected.target.style.backgroundColor = color.backgroundColor;
+        console.log(pixelSelected.target);
+    })
+}
+
+
 
 // sColor.addEventListener('click', function() {
 //     let selectedColor = document.querySelector('.selected');
