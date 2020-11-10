7b35e02402a5b00cf48653ef33a92aaf85910a66
Maurice Alves <maurice.estevam@gmail.com>
2020-11-10 12:48:53 -0300

    Paleta de quatro cores distintas

---
+++ script.js
@@ -0,0 +1,4 @@
+let corPreto = document.getElementsByClassName('color')[0]; corPreto.style.background = 'black';
+let corVermelho = document.getElementsByClassName('color')[1];corVermelho.style.background = 'red';
+let corAzul = document.getElementsByClassName('color')[2]; corAzul.style.background = 'blue';
+let corVerde = document.getElementsByClassName('color')[3]; corVerde.style.background = 'green';
