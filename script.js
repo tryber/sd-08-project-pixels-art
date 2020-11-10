7176b00f18f66c45731414a10a7571b5a4ebc430
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2020-11-10 00:10:59 -0300

    req2: Reescrevi esse requisito ancorando tags no html que servirao de base para requisitos futuros

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-/*
-//requisito 1 - feito em JS, pena que o evaluator não aceitou :/
-const titlePaletadeCores = document.createElement('h1');
-const captureBody = document.querySelector('body');
-//dando o id e nome para o h1 criado
-titlePaletadeCores.id = 'title';
-titlePaletadeCores.innerHTML = 'Paleta de Cores';
-//posicionei title como filho de body
-captureBody.appendChild (titlePaletadeCores);
-*/
-
-//requisito 2
-const captureBody = document.querySelector('body');
-//criação da div 'color-palette'
-const idPaletaDeCores = document.createElement('div');
-idPaletaDeCores.id = `color-palette`;
-//color-palete esta como filha de 'body'
-captureBody.appendChild(idPaletaDeCores);
-
-//esse laço vai criar 4 divs e dar o nome 'color' a elas
+//requisito 2 - criando as 4 divs 'color'
+const captureDivColorPalette = document.getElementById('color-palette')
 for (let index = 0; index <= 3; index += 1) {
     let colorClass = document.createElement('div');
     colorClass.className = 'color';
-    idPaletaDeCores.appendChild(colorClass);
+    captureDivColorPalette.appendChild(colorClass);
 }
 
-//capturar a div 'color' para poder usar o laço for - getElementsByClassName gera um array, o que dispensa o laço for
-const captureDivColor = document.getElementsByClassName('color');
-    //requisito 3
-   captureDivColor[0].style.backgroundColor = 'black';
-   captureDivColor[1].style.backgroundColor = 'blue';
-   captureDivColor[2].style.backgroundColor = 'green';
-   captureDivColor[3].style.backgroundColor = 'orange';
+//requisito 2 - função responsável por colorir
+function toColorDiv (color) {
+    const captureDivColor = document.getElementsByClassName('color');
+    //laço responsável por distribuir as cores do array recebido e a 'div - color' criada na primeira parte do requisito 2
+    for (let index = 0; index < captureDivColor.length; index += 1) {
+        let toColor = captureDivColor[index]
+        toColor.style.backgroundColor = color[index];
+    }
+}
+toColorDiv(['orange', 'green', 'blue', 'pink']);
+
+    
+
+
+
+
 
 
 
