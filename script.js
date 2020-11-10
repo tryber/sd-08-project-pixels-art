f65b093296eebc9ee568f8e007315685fc759b57
jnpefo <jnpefo@gmail.com>
2020-11-10 13:35:16 -0300

    Req 8 tentativa 4

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 const corSelecionada = document.querySelectorAll('.color');
 
 for (let i = 0; i < corSelecionada.length; i += 1) {
-    const atual = corSelecionada[i];
-    atual.addEventListener('click', function (event) {
-        for (let j = 0; j < corSelecionada.length; j += 1) {
-            const marcado = corSelecionada[j];
-            if (marcado.classList.contains('selected')) {
-                marcado.classList.remove('selected');
-            }
-        }
-        event.target.classList.add('selected');
-    });
+  const atual = corSelecionada[i];
+  atual.addEventListener('click', function (event) {
+    for (let j = 0; j < corSelecionada.length; j += 1) {
+      const marcado = corSelecionada[j];
+      if (marcado.classList.contains('selected')) {
+        marcado.classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  });
 }
 const allPixels = document.querySelectorAll('.pixel');
 
 // Req 8
 document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('pixel')) {
-        let cor = document.querySelector('.selected');
-        let pickedColor = window.getComputedStyle(cor, null).getPropertyValue("background-color");
-        event.target.style.backgroundColor = pickedColor;
-    }
+  if (event.target.classList.contains('pixel')) {
+    let cor = document.querySelector('.selected');
+    let pickedColor = window.getComputedStyle(cor, null).getPropertyValue("background-color");
+    event.target.style.backgroundColor = pickedColor;
+  }
 });
