e4c9cacee3c999364e0beaf770e46c720e583300
enicio <jessandro42@gmail.com>
2020-11-09 21:15:52 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-document.querySelector('#color-1').className= "color selected"
+document.querySelector('#black').className= "color selected"
+
+
+const colorPalette = document.getElementById('color-palette')
+colorPalette.addEventListener('click', selectColor)
+
+
+function selectColor(e) {
+   //console.log(e.target.style.backgroundColor)
+   const color = e.target.style.backgroundColor;
+  // console.log(e.target.id)
+   let colorSelected = document.getElementById(e.target.id)
+  // console.log(colorSelected)
+   colorSelected.className = "color selected"
+
+   switch (color) {
+    case 'red' :
+      colorSelected.className = "color selected"
+      document.querySelector('#black').className= "color"
+      document.querySelector('#blue').className= "color"
+      document.querySelector('#green').className= "color"
+        break;
+    case 'blue' :
+      colorSelected.className = "color selected"
+      document.querySelector('#black').className= "color"
+      document.querySelector('#red').className= "color"
+      document.querySelector('#green').className= "color"
+				break;
+		case 'green' :
+			colorSelected.className = "color selected"
+		  document.querySelector('#black').className= "color"
+			document.querySelector('#red').className= "color"
+			document.querySelector('#blue').className= "color"
+				break;
+		case 'black' :
+			colorSelected.className = "color selected"
+			document.querySelector('#blue').className= "color"
+			document.querySelector('#red').className= "color"
+			document.querySelector('#green').className= "color"
+				break;
+    default:
+            console.log("ops")
+   }
+}
+
