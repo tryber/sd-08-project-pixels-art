c9fe9f511c7f72e380674b623b029fbc034f8812
Giovanna Betti <giovannabetti@gmail.com>
2020-11-10 14:00:19 -0300

    Codeclimate erros

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const cores = document.querySelectorAll('.color');
 
 // function createDiv() {
 //     let palette = document.getElementById('color-palette');
-    
+
 //     for (let i = 1; i < 5; i+=1) {
 //         let colorDiv = document.createElement('div');
 //         colorDiv.className = ('color');
@@ -20,19 +20,17 @@ const cores = document.querySelectorAll('.color');
 
 // createDiv();
 
-window.onload = function() {
-    preto.style.backgroundColor = 'black';
-    preto.classList.add('selected');
+window.onload = function () {
+  preto.style.backgroundColor = 'black';
+  preto.classList.add('selected');
 };
-
 function corDeFundo() {
-    if (cores.className === 'amarelo') {
-        cores.style.backgroundColor = 'yellow'
-    } else if (cores.className === 'laranja') {
-        cores.style.backgroundColor = 'orange'
-    } else {
-        cores.style.backgroundColor = 'red'
-    }
-};
-
+  if (cores.className === amarelo) {
+    cores.style.backgroundColor = 'yellow';
+  } else if (cores.className === laranja) {
+    cores.style.backgroundColor = 'orange';
+  } else {
+    cores.style.backgroundColor = 'red';
+  }
+}
 corDeFundo();
