e07459198fe6e85056ab7a7251819534c2e10d3d
Blucilha <blucilha.nascimento@gmail.com>
2020-11-10 16:09:35 -0300

    Cria tabela

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.getElementById("preta").style.backgroundColor = "black";
 document.getElementById("amarelo").style.backgroundColor = "yellow";
 document.getElementById("cinza").style.backgroundColor = "grey";
 document.getElementById("vermelho").style.backgroundColor = "red";
+
+   
