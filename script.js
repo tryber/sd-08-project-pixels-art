8504aff6e71caa8920122802a334e6dcb7411868
wagnerberna <cabelo29007@gmail.com>
2020-11-10 10:37:32 -0300

    teste botão

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('teste');
+function createBtn(nome, id) {
+  const btn = document.createElement('button');
+  btn.innerHTML = nome;
+  btn.id = id;
+  
+  document.getElementById('button-container').appendChild(btn);
+}
+
+createBtn('limpar', 'clear-board');
+
+createBtn('VQV', 'generate-board');
