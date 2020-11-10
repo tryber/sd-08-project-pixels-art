9833879671df756005385dec3e524ee83cc3e17a
MuriloBatista <git@github.com:MuriloBatista>
2020-11-09 21:32:53 -0300

    Adiciona 8º requisito

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-//Pagina inicia com a cor black selecionada
-const corIni = document.querySelectorAll('.color');
-corIni[0].classList.add('selected');
+window.onload = function (){
 
-//Add listener para paleta de cores
-const paleta = document.getElementById('color-palette');
-paleta.addEventListener('click', setColor);
+    //Pagina inicia com a cor black selecionada
+    const corIni = document.querySelectorAll('.color');
+    corIni[0].classList.add('selected');
 
-//Remove seletor
-function selectionRem() {
-    for (let index = 0; index < corIni.length; index += 1) {
-      corIni[index].className = "color";
+    //Add listener para paleta de cores
+    const paleta = document.getElementById('color-palette');
+    paleta.addEventListener('click', setColor);
+
+    //Remove seletor paleta
+    function selectionRem() {
+        for (let index = 0; index < corIni.length; index += 1) {
+        corIni[index].className = "color";
+        }
+    }
+    //Adiciona seletor paleta
+    function setColor(event) {
+        selectionRem();
+        event.target.className = "color selected";
     }
 }
-
-//Adiciona seletor
-function setColor(event) {
-    selectionRem();
-    event.target.className = "color selected";
-}
+    //Add listener para o pixel board
+    const pxBoard = document.getElementById('pixel-board');
+    pxBoard.addEventListener('click', paint);
+    
+    function paint(event) {
+        let teste = document.querySelector('.color.selected').id;
+        event.target.style.backgroundColor = teste;
+          
+    }
+    
