a5cb7f1df01e093beebc7c02b58f31a7f2c89027
JRGCast <joao.ralph1@gmail.com>
2020-11-10 13:14:08 -0300

    apos longa luta pra mudar de table para divs, consegui deixar o quadro estavel; window.onload refeito; adicionadas funcoes para tornar as cores randomizadas (ainda nao implementado ao color palette)

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// Requisito 06 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels:
 window.onload = function () {
-  let blkSqrColor = document.getElementById('color-palette')
-    .firstElementChild.style.backgroundColor;
-  blkSqrColor.className = 'color selected';
-};
+  document.getElementsByClassName('color')[0].className = 'color selected';
+  
+}
+
+function randomNumber (number){
+  let randomized = Math.floor(Math.random() * number);
+  return randomized;
+}
+function randomColor() {
+  let colorHex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let colorHash = '#';
+  for(let i = 0; i < 6; index += 1) {
+    let randomic = randomNumber(hexadecimal.length);
+    colorHash += colorHex[randomic];
+    if(colorHash === "#FFFFFF"){
+      i = 0;
+    }
+  }
+  return colorHash;
+}
 
 function selecionaCor() {
     let blkSqr = document.getElementById('color-palette')
@@ -31,21 +48,21 @@ paletaCores();
 function quadroPixels() {
   // let tamanhoDefinicao = prompt('quantos quadrados você quer?');
   // let tamanhoDefinido = Number(tamanhoDefinicao);
-  const linhaQuadrados = 5;
+  const linhaQuadrados = 25;
   const colunaQuadrados = 5;
   const squareContainer = document.getElementById('pixel-board');
 
   for (let i = 0; i < linhaQuadrados; i += 1) {
-    let linhaSquares = document.createElement('tr');
-    linhaSquares.className = 'pixel-row';
+    let linhaSquares = document.createElement('div');
+    linhaSquares.className = 'pixel';
     squareContainer.appendChild(linhaSquares);
 
-    for (let i = 0; i < colunaQuadrados; i += 1) {
-      let colunaSquares = document.createElement('td');
-      colunaSquares.className = 'pixel';
-      squareContainer.appendChild(colunaSquares);
-    }
-  }
+    // for (let i = 0; i < colunaQuadrados; i += 1) {
+    //   let colunaSquares = document.createElement('div');
+    //   colunaSquares.className = 'pixel';
+    //   squareContainer.appendChild(colunaSquares);
+    // }
+   }
 }
 quadroPixels();
 
