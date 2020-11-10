45ae7e4c39f3dea6a412081d5b0e72092cdf9a1e
Edson Araujo <araujo.barros@matematica.ufrj.br>
2020-11-09 21:41:31 -0300

    Requisitos bônus 10 e 11 Criando quadro de pixel do capeta

--- script.js
+++ script.js
@@ -1,30 +1,93 @@
-let paleta = document.querySelector('#color-palette');
-paleta.addEventListener('click', function (event){
+const paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', function (event) {
   if (event.target.className !== 'color selected') {
-    const paletaSelecionada = document.querySelector('.selected')
+    const paletaSelecionada = document.querySelector('.selected');
     event.target.className = 'color selected';
     paletaSelecionada.className = 'color';
   }
-} )
+});
 
 let paletas = document.querySelectorAll('.color');
 let pixels = document.querySelector('#pixel-board');
-let teste = document.querySelector('#cor2');
-pixels.addEventListener('click', function (event){
-  for (i = 0; i< paletas.length; i++) {
-    console.log(paletas[i].style.backgroundColor)
+pixels.addEventListener('click', function (event) {
+  for (let i = 0; i < paletas.length; i++) {
     if (paletas[i].className === 'color selected') {
-      event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue("background-color")
+      event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue('background-color')
     }
   }
 })
 
 let pixelsList = document.querySelectorAll('.pixel');
-console.log(pixelsList);
 let botaoLimpeza = document.querySelector('#clear-board');
-
 botaoLimpeza.addEventListener('click', function () {
-  for (i=0; i<pixelsList.length; i++) {
+  for (let i = 0; i < pixelsList.length; i++) {
     pixelsList[i].style.backgroundColor = 'white'
   }
 })
+
+
+let botaoVQV = document.querySelector("#generate-board")
+botaoVQV.addEventListener('click', function() {
+  
+  let quant = document.querySelector("#board-size");
+  let receptora = document.querySelector("body > div:nth-child(5)")
+  receptora.id = 'receptora'
+  let newPixels = document.createElement('div')
+  receptora.appendChild(newPixels);
+  newPixels.id = 'pixel-board';
+  
+  
+  if (quant.value !== ""){
+    document.querySelector('#pixel-board').parentNode.removeChild(document.querySelector('#pixel-board'));
+    if (quant.value < 50 && quant.value > 5){
+          for (let i = 1; i<= quant.value; i++){
+            let linhas = document.createElement('div');
+            newPixels.appendChild(linhas)
+            linhas.className = 'new-divTableRow'
+            for (let j = 1; j <= quant.value; j++){
+              let colunas = document.createElement('div');
+              linhas.appendChild(colunas);
+              colunas.className = 'new-pixel';
+            }
+          }
+    }else{
+      if (quant.value <= 5){
+        for (let i = 1; i<= 5; i++){
+          let linhas = document.createElement('div');
+          newPixels.appendChild(linhas)
+          linhas.className = 'new-divTableRow'
+          for (let j = 1; j <= 5; j++){
+            let colunas = document.createElement('div');
+            linhas.appendChild(colunas);
+            colunas.className = 'new-pixel';
+          }
+        }
+      }else{
+        for (let i = 1; i<= 50; i++){
+          let linhas = document.createElement('div');
+          newPixels.appendChild(linhas)
+          linhas.className = 'new-divTableRow'
+          for (let j = 1; j <= 50; j++){
+            let colunas = document.createElement('div');
+            linhas.appendChild(colunas);
+            colunas.className = 'new-pixel';
+          }
+        }
+      }
+    }
+  }else{
+    alert("Board inválido!");
+  }
+    
+  newPixels.addEventListener('click', function (event) {
+    for (let i = 0; i < paletas.length; i++) {
+      if (paletas[i].className === 'color selected') {
+        event.target.style.backgroundColor = window.getComputedStyle(paletas[i], null).getPropertyValue('background-color')
+      }
+    }
+  })
+})
+
+
+
+
