1cfe868b5c8c8effeef20ff2774a16f7fb93d215
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2020-11-10 00:06:16 -0300

    Requisitos 10 e 11

--- script.js
+++ script.js
@@ -19,34 +19,62 @@ document.querySelectorAll('.color').forEach((color, index, arr) => {
     });
 });
 
-const pixelsLineAndColumn = [5, 5];
+const boardSize = document.querySelector('#board-size');
+const pixelBoard = document.querySelector('#pixel-board');
 
-for (let lineNumber = 1; lineNumber <= pixelsLineAndColumn[0]; lineNumber += 1) {
-    const line = document.createElement('div');
-
-    line.className = 'line';
-
-    for (let columnNumber = 1; columnNumber <= pixelsLineAndColumn[1]; columnNumber += 1) {
-        const column = document.createElement('div');
-
-        column.className = 'pixel';
-        column.style.backgroundColor = 'white';
-
-        line.appendChild(column);
+function generateBoard(size) {
+    for (let lineNumber = 1; lineNumber <= size; lineNumber += 1) {
+        const line = document.createElement('div');
+    
+        line.className = 'line';
+    
+        for (let columnNumber = 1; columnNumber <= size; columnNumber += 1) {
+            const column = document.createElement('div');
+    
+            column.className = 'pixel';
+            column.style.backgroundColor = 'white';
+    
+            line.appendChild(column);
+        }
+    
+        pixelBoard.appendChild(line);
     }
+};
 
-    document.querySelector('#pixel-board').appendChild(line);
-}
+generateBoard(5);
 
-const pixels = document.querySelectorAll('.pixel')
+const pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('click', () => {
-        const colorToApply = document.querySelector('.selected').style.backgroundColor;
-
-        pixel.style.backgroundColor = colorToApply;
+function addColorListener(pixelsArr) {
+    pixelsArr.forEach((pixel) => {
+        pixel.addEventListener('click', () => {
+            const colorToApply = document.querySelector('.selected').style.backgroundColor;
+    
+            pixel.style.backgroundColor = colorToApply;
+        });
     });
+};
+
+addColorListener(pixels);
+
+document.querySelector('#generate-board').addEventListener('click', () => {
+    if (boardSize.value != '') {
+        for (let i = pixelBoard.children.length - 1; i >= 0; i -= 1) {
+            pixelBoard.children[i].remove();
+        };
+        if (boardSize.value < 5) {
+            generateBoard(5);
+        } else if (boardSize.value > 50) {
+            generateBoard(50);
+        } else {
+            generateBoard(boardSize.value);
+        }
+        addColorListener(document.querySelectorAll('.pixel'));
+    } else {
+        alert('Board inválido!');
+    }
 });
+
 
 document.querySelector('#clear-board').addEventListener('click', () => {
     pixels.forEach(pixel => pixel.style.backgroundColor = 'white');
