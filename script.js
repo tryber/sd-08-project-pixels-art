a848b9e5b204f45c9468fda57f51ed0adcf56353
Giovanna Betti <giovannabetti@gmail.com>
2020-11-10 13:32:09 -0300

    Inserindo quadro de pixels

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// function createDiv() {
-//     let palette = document.getElementById('color-palette');
-    
-//     for (let i = 1; i < 5; i+=1) {
-//         let colorDiv = document.createElement('div');
-//         colorDiv.className = ('color');
-//     }
-// }
-
-// createDiv();
-
-
+// Declaracao de variaveis globais
 const palette = document.getElementById('color-palette');
 
 const preto = document.getElementsByClassName('preto');
@@ -19,10 +8,25 @@ const vermelho = document.getElementsByClassName('vermelho');
 
 const cores = document.querySelectorAll('.color');
 
+
+// function createDiv() {
+//     let palette = document.getElementById('color-palette');
+    
+//     for (let i = 1; i < 5; i+=1) {
+//         let colorDiv = document.createElement('div');
+//         colorDiv.className = ('color');
+//     }
+// };
+
+// createDiv();
+
+window.onload = function() {
+    preto.style.backgroundColor = 'black';
+    preto.classList.add('selected');
+}
+
 function corDeFundo() {
-    if (cores.className === 'preto') {
-        cores.style.backgroundColor = 'black'
-    } else if (cores.className === 'amarelo') {
+    if (cores.className === 'amarelo') {
         cores.style.backgroundColor = 'yellow'
     } else if (cores.className === 'laranja') {
         cores.style.backgroundColor = 'orange'
@@ -30,3 +34,5 @@ function corDeFundo() {
         cores.style.backgroundColor = 'red'
     }
 };
+
+corDeFundo();
