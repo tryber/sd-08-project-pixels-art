db71eb00b65da94c35fbc8556cd03baaaf8d798b
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-10 04:12:23 -0300

    primeiro requisito

---
+++ script.js
@@ -0,0 +1,12 @@
+//Paleta de cores
+let colorList = ['black', 'purple', 'gold', 'navy'];
+let colorPalette = document.getElementById('color-palette')
+
+for (let i = 0; i < colorList.length; i += 1){
+    let div = document.createElement('div')
+    div.className = 'color';
+    div.style.backgroundColor = colorList[i];
+    colorPalette.appendChild(div);
+
+}
+    console.log(div)
