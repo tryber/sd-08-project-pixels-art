f1a9b61afbdfb63306b4484093afa42d22ee346c
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-10 04:38:03 -0300

    ajustes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let colorList = ['black', 'purple', 'gold', 'navy'];
 let colorPalette = document.getElementById('color-palette')
 
 for (let i = 0; i < colorList.length; i += 1){
-    let div = document.createElement('div')
+    const div = document.createElement('div')
     div.className = 'color';
     div.style.backgroundColor = colorList[i];
     colorPalette.appendChild(div);
