0cd722d04d808f8e09c905086e3b4ba3371bc608
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-11-10 11:06:23 -0300

    add requisito 6,7 e 8

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-window.onload = function(){
-  document.querySelectorAll(".color")[0].classList.add("selected");
-}
+let color = document.querySelectorAll('.color');
 
-
-function createPalletColors() {
-  let divColorPallet = document.getElementById("color-palette");
-  for (let i = 0; i < 4; i += 1) {
-    let divChild = document.createElement("button");
-    divChild.className = "color";
-    let colors = ["black", "blue", "orange", "green"];
-    divChild.style.backgroundColor = colors[i];
-    divColorPallet.appendChild(divChild);
-  }
-}
-createPalletColors();
 function createSquares() {
   let squares = document.getElementById("pixel-board");
   for (let i = 0; i < 5; i += 1) {
     // quantidade de linhas
-    let row = document.createElement("div");
+    let coluna = document.createElement("div");
     for (let j = 0; j < 5; j += 1) {
       // quantidade de pixel
       let pixel = document.createElement("div");
       pixel.className = "pixel";
-      row.appendChild(pixel);
+      coluna.appendChild(pixel);
     }
-    squares.appendChild(row);
+    squares.appendChild(coluna);
   }
 }
 createSquares();
 
-function limpar() {
-  console.log("botão limpar clicado");
-  document.querySelectorAll(".pixel")[0].style.backgroundColor = "white"
-}
+addEventListener('click', function (evt){
+  
+  if (evt.target.className == 'color') {
+    for (let i = 0; i < color.length; i+=1) {
+      color[i].className = 'color';
+    }
+    evt.target.className = 'color selected';
+  }
+});
 
-limpar();
+addEventListener('click', function(evt) {
+  for (let i = 0; i < color.length; i+=1){
+    if(color[i].className == 'color selected' && evt.target.className == 'pixel'){
+      evt.target.style.backgroundColor = getComputedStyle(color[i]).backgroundColor;
+    }
+  }
+});
+
+
+
+
