2dbd917e4f4f8edd53fe2464180a1c40d4658dd4
Giovanna Betti <giovannabetti@gmail.com>
2020-11-10 11:52:08 -0300

    Estruturando JS com variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+// function createDiv() {
+//     let palette = document.getElementById('color-palette');
+    
+//     for (let i = 1; i < 5; i+=1) {
+//         let colorDiv = document.createElement('div');
+//         colorDiv.className = ('color');
+//     }
+// }
+
+// createDiv();
+
+
+const palette = document.getElementById('color-palette');
+
+const preto = document.getElementsByClassName('preto');
+const amarelo = document.getElementsByClassName('amarelo');
+const laranja = document.getElementsByClassName('laranja');
+const vermelho = document.getElementsByClassName('vermelho');
