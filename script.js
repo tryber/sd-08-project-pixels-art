fc9ad742559f688ae5104712cd0c7cb13e62da4a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2020-11-10 15:50:40 -0300

    Req2: adiciona paleta de cores

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function createElements(){
 }
 
 createElements();
+
+function createColorPallet(){
+    let colors = ["blue", "red", "pink", "green"];
+    let colorPallet = document.createElement('div');
+    colorPallet.id = 'color-palette';
+    colorPallet.className = 'color';
+    colorPallet.style.display = 'inline-block'
+    body.appendChild(colorPallet);
+
+    for(let i = 0; i < colors.length; i += 1){
+        let block = document.createElement('div');
+        block.style.height = '30px';
+        block.style.width = '30px';
+        block.style.display = 'inline-block';
+        block.style.border = '1px solid black';
+        block.style.backgroundColor = colors[i];
+        colorPallet.appendChild(block);
+    }
+}
+
+createColorPallet();
