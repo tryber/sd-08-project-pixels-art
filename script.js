e261b38939bba37048a7d74e9ead798dbde7b663
Hugo Andrade Braga <hugobrr@gmail.com>
2020-11-10 14:14:44 -0300

    re9 ajustando para o codeclim pela terceira vez com 23 erros

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function nameColor(colored) {
   let colorBox = 'black';
   if (colored.classList[3] != null) {
-        colorBox = colored.classList[3];
+    colorBox = colored.classList[3];
   } else {
-        colorBox = colored.classList[2];
+    colorBox = colored.classList[2];
   }
   return colorBox;
 }
@@ -13,17 +13,17 @@ colored.addEventListener('click', function () {
   // console.log(event.target);
   const colorOfEvent = nameColor(event.target);
   const colorOfSelector = nameColor(document.querySelector('.selected'));
-  console.log(colorOfEvent);
+  // console.log(colorOfEvent);
   if (document.querySelector('.selected') !== event.target) {
     document.querySelector('.selected').className = 'color box ' + colorOfSelector;
-    event.target.className = 'color box selected ' + colorOfEvent ;
+    event.target.className = 'color box selected ' + colorOfEvent;
   } else {
-    console.log('cor já selecionada');
+    // console.log('cor já selecionada');
   }
 });
 
 const button = document.getElementById('clear-board');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   document.getElementsByClassName('pixel')[0].className = 'box pixel white';
   document.getElementsByClassName('pixel')[1].className = 'box pixel white';
   document.getElementsByClassName('pixel')[2].className = 'box pixel white';
@@ -53,13 +53,13 @@ button.addEventListener('click', function() {
 
 
 const mapa = document.getElementById('pixel-board');
-mapa.addEventListener('click', function() {
+mapa.addEventListener('click', function () {
   const colorOfSelector = nameColor(document.querySelector('.selected'));
-  console.log(event.target.classList[2] + " =?= "+colorOfSelector)
+  console.log(event.target.classList[2] + " =?= " + colorOfSelector);
   if (event.target.classList[0] === 'box') {
     if (event.target.classList[2] !== colorOfSelector) {
     // console.log(matrix[i].className)
-    event.target.className = ('box pixel ' + colorOfSelector);
+      event.target.className = ('box pixel ' + colorOfSelector);
     // console.log(matrix[i].className)
-  } }
+    } }
 });
