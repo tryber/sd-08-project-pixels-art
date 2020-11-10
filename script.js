44edc5f17be4cb06bbe63f09bca38ef711a47511
Patrícia <silva.patricia.v@gmail.com>
2020-11-09 22:33:27 -0300

    requisitos 4 e 5 completos

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function createDivs() {
   const arrayColor = ["black", "purple", "red", "green"];
   const palletDiv = document.getElementById("color-palette");
   for (let index = 0; index < arrayColor.length; index += 1) {
-    let divColor = arrayColor[index];
     const myDivs = document.createElement("div");
+    myDivs.style.backgroundColor = arrayColor[index];
     palletDiv.appendChild(myDivs);
     myDivs.className = "color";
-    myDivs.style.backgroundColor = divColor;
   }
 }
 createDivs();
