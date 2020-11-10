b481054eee21d1db328da9323cc40618bfab6bee
josebejohann <josebernardojohann@gmail.com>
2020-11-10 08:24:54 -0300

    Sets black color as primary and configures selected class name

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let createPixels = () => {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i += 1) {
     let board = document.querySelector("#pixel-board");
     let pixel = document.createElement("div");
     pixel.className = "pixel";
@@ -9,3 +9,46 @@ let createPixels = () => {
 };
 
 createPixels();
+
+const colorList = document.querySelectorAll(".color");
+
+const black = document.querySelector(".black");
+black.classList.add("selected");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const green = document.querySelector(".green");
+
+black.style.backgroundColor = "black";
+red.style.backgroundColor = "red";
+blue.style.backgroundColor = "blue";
+green.style.backgroundColor = "green";
+
+let removeSelectedClass = () => {
+  for (let i = 0; i < colorList.length; i += 1) {
+    colorList[i].classList.remove("selected");
+  }
+};
+
+let selectedColor = () => {
+  for (let i = 0; i < colorList.length; i += 1) {
+    if (colorList[i].classList.contains("selected")) {
+      return colorList[i].style.backgroundColor;
+    }
+  }
+
+  return "";
+};
+
+selectedColor();
+
+let palette = document.querySelector("#color-palette");
+
+let selectColor = (event) => {
+  if (event.target.className != "selected") {
+    removeSelectedClass();
+
+    event.target.classList.add("selected");
+  }
+};
+
+palette.addEventListener("click", selectColor);
