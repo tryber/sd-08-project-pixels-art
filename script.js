b3597efad1d12f55af8118a647b17a63039e00be
Luiz Reina <luizreeina@gmail.com>
2020-11-10 14:08:13 -0300

    corrigindo codeclimate e requisitos v13

--- script.js
+++ script.js
@@ -89,9 +89,9 @@ function geraCores() {
   cor[0].className = 'selected color';
 
   for (let index = 1; index < 4; index += 1) {
-    let newColor = '#'
-    let temp = (Math.floor(Math.random() * 16777215).toString(16)).toString();
-    newColor = newColor + temp
+    let newColor = '#';
+    const temp = (Math.floor(Math.random() * 16777215).toString(16)).toString();
+    newColor += temp;
     cor[index].style.backgroundColor = newColor;
     cores.push(cor[index].style.backgroundColor);
   }
