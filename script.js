9c196d03911954c60c4f335ad41118d6c4599c77
Rafael <rafaeljavaro750@gmail.com>
2020-11-10 14:11:00 -0300

    2nd Tentativa - Refatorando CodeClimate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function randomColor() {
-  let c1 = Math.floor(Math.random() * 255);
-  let c2 = Math.floor(Math.random() * 255);
-  let c3 = Math.floor(Math.random() * 255);
+  const c1 = Math.floor(Math.random() * 255);
+  const c2 = Math.floor(Math.random() * 255);
+  const c3 = Math.floor(Math.random() * 255);
   return `rgb(${c1},${c2},${c3})`;
 }
 
@@ -9,11 +9,11 @@ function colorsOptions() {
   const options = document.getElementById('color-palette');
   const colors = [];
   for (let i = 0; i < 4; i += 1) {
-      colors[i] = document.createElement('ul');
-      colors[i].classList.add('color');
-      colors[i].style.backgroundColor = randomColor();
-      options.appendChild(colors[i]);
-    }
+    colors[i] = document.createElement('ul');
+    colors[i].classList.add('color');
+    colors[i].style.backgroundColor = randomColor();
+    options.appendChild(colors[i]);
+  }
 }
 
 colorsOptions();
