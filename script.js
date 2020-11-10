99881a0438ba1302a545e7d4bfaad12737791a92
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-10 14:20:45 -0300

    adicionando o requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const pixel = document.querySelector('#pixel-board');
-for (let col = 0; col < 5; col +=1) {
-    for(let line = 0; line < 5; line +=1) {
-        let pix = document.createElement('div');
+for (let col = 0; col < 5; col += 1) {
+    for (let line = 0; line < 5; line += 1) {
+        const pix = document.createElement('div');
         pix.className = 'pixel';
-        pixel.appendChild(pix)
+        pixel.appendChild(pix);
     }
+}
+window.onload = function(){
+    document.getElementById('black').classList.add('selected');
 }
