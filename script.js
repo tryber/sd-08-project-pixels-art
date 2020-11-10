fcaab7c40115eec175ea9773b5af6f5711c80d53
Nícolas Tozato <ntozato9@gmail.com>
2020-11-10 18:34:30 -0300

    requisito 4 nova tentativa

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = funcaoInicial;
-
-function funcaoInicial() {
-    let pixels = document.querySelectorAll('.pixel');
-
-    for (let i = 0; i < pixels.length; i++) {
-    pixels[i].style.backgroundColor = 'white';
-    }
-}
