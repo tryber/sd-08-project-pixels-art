baac2bc44ee25a2cfb2365567f520341b408440c
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-09 21:15:26 -0300

    verifica requisitos 2 e 3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (let index = 0; index < colors.length; index += 1) {
     let divColor = colors[index];
     let myDivs = document.createElement('div');
     colorPalette.appendChild(myDivs);
-    myDivs.className = 'color';
+    myDivs.className = 'color';    
     myDivs.style.backgroundColor = divColor;
 }
 }
