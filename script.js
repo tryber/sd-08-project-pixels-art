46bba9da403bd72dace67859dc8a318e8f072729
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-10 13:02:14 -0300

    acertos codeClimate

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-
 const pixelBoard = document.getElementById('pixel-board');
 
 function generateBoard(num) {
-    for (let index = 1; index <= num; index += 1) {
-    let divLine = document.createElement('div');
+  for (let index = 1; index <= num; index += 1) {
+    const divLine = document.createElement('div');
     pixelBoard.appendChild(divLine);
 
-    for (let index = 1; index <= num; index += 1) {
-      let pixelUnit = document.createElement('div');
+    for (let j = 1; j <= num; j += 1) {
+      const pixelUnit = document.createElement('div');
       pixelUnit.className = 'pixel';
-      divLine.appendChild(pixelUnit); 
+      divLine.appendChild(pixelUnit);
     }
   }
-};
+}
 generateBoard(5);
 
 function createPalette() {
-  let firstColor = document.getElementById('firstColor');
-  let secondColor = document.getElementById('secondColor');
-  let thirdColor = document.getElementById('thirdColor');
-  let fourthColor = document.getElementById('fourthColor');
+  const firstColor = document.getElementById('firstColor');
+  const secondColor = document.getElementById('secondColor');
+  const thirdColor = document.getElementById('thirdColor');
+  const fourthColor = document.getElementById('fourthColor');
 
-  let arrayColors = [];
+  const arrayColors = [];
   for (let i = 0; i < 3; i += 1) {
-    let r = Math.floor(Math.random()*255),
-        g = Math.floor(Math.random()*255),
-        b = Math.floor(Math.random()*255);
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
 
-    arrayColors.push('rgb('+r+','+g+','+b+')');
+    arrayColors.push('rgb(' + r + ',' + g + ',' + b + ')');
   }
+  
   firstColor.style.backgroundColor = 'black';
   secondColor.style.backgroundColor = arrayColors[0];
   thirdColor.style.backgroundColor = arrayColors[1];
@@ -37,35 +37,33 @@ function createPalette() {
 createPalette();
 
 function selectedColor() {
-  let colorPalette = document.querySelector('#color-palette');
+  const colorPalette = document.querySelector('#color-palette');
 
   function changeStatus(event) {
-    let selectedPalette = document.querySelector(".color.selected");
+    const selectedPalette = document.querySelector('.color.selected');
     selectedPalette.className = 'color';
     event.target.className = 'color selected';
   }
 
-  colorPalette.addEventListener('click' , changeStatus);
-  
+  colorPalette.addEventListener('click', changeStatus);
 }
 selectedColor();
 
 function fillPixelColor() {
-  let pixelUnit = document.querySelector('#pixel-board');
-  pixelUnit.addEventListener('click', changeColor);
+  const pixelUnit = document.querySelector('#pixel-board');
 
   function changeColor(event) {
-    let selectedColor = document.querySelector('.selected');
+    const selectedColor = document.querySelector('.selected');
     event.target.style.backgroundColor = selectedColor.style.backgroundColor;
-  };
+  }
 
-  
+  pixelUnit.addEventListener('click', changeColor);
 }
 fillPixelColor();
 
 function clearBoard() {
-  let clearPixels = document.getElementById("clear-board");
-  let pixelUnits = document.querySelectorAll(".pixel");
+  const clearPixels = document.getElementById('clear-board');
+  const pixelUnits = document.querySelectorAll('.pixel');
 
   function clearPixelsColor() {
     for (let i = 0; i < pixelUnits.length; i += 1) {
@@ -77,23 +75,22 @@ function clearBoard() {
 clearBoard();
 
 function generateNewBoard() {
-  let VQV = document.getElementById("generate-board");
+  const VQV = document.getElementById('generate-board');
 
-  VQV.addEventListener('click', function(){
-    let input = document.getElementById("board-size").value;
-    if (input == "") {
-      alert("Board inválido!");
+  VQV.addEventListener('click', function () {
+    let input = document.getElementById('board-size').value;
+    if (input === '') {
+      alert('Board inválido!');
     }
 
     if (input < 5) {
       input = 5;
-    }
-    else if (input > 50) {
+    } else if (input > 50) {
       input = 50;
     }
 
     if (input >= 5 || input <= 50) {
-      const myNode = document.getElementById("pixel-board");
+      const myNode = document.getElementById('pixel-board');
       while (myNode.firstChild) {
         myNode.removeChild(myNode.lastChild);
       }
