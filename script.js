b2a214b6c4c1b8b9d5850cd3882deb1394e8b86c
Tandy <tandyfw@gmail.com>
2020-11-10 06:05:27 -0300

    Comentando código

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Requisito 06 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.
 window.onload = function () {
   document.querySelector('.color').className = 'color selected';
 }
 
+// Requisito 10.
+
+
+//Requisito 02 - A página deve possuir uma paleta de quatro cores distintas.
 function initialColors () {
   let colors = document.querySelectorAll('.color');
+  // Requisito 03 - A cor preta deve ser a primeira na paleta de cores.
   colors[0].style.backgroundColor = 'black';
   colors[1].style.backgroundColor = 'blue';
   colors[2].style.backgroundColor = 'yellow';
@@ -11,6 +17,8 @@ function initialColors () {
 }
 initialColors();
 
+
+// Requisito 04 - A página deve possuir um quadro de pixels, com 25 pixels.
 function pixelBoard () {
   let divBoard = document.querySelector('#pixel-board');
   for (let index = 1; index < 26; index += 1) {
@@ -21,6 +29,7 @@ function pixelBoard () {
 }
 pixelBoard();
 
+// Requisito 07 - Ao clicar em uma das cores da paleta, a cor selecionada é que vai ser usada para preencher os pixels no quadro.
 function selectColor () {
   let element1 = document.querySelectorAll('.color')[0];
   let element2 = document.querySelectorAll('.color')[1];
@@ -61,6 +70,7 @@ function selectColor () {
 }
 selectColor();
 
+// Requisito 08 - Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.
 function paint () {
   let pixelBoad = document.querySelectorAll('.pixel').forEach(function(item) {
     item.addEventListener('click', function(event) {
@@ -71,6 +81,7 @@ function paint () {
 }
 paint();
 
+// Requisito 09 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
 function clear() {
   let btnClear = document.querySelector('#clear-board');
     btnClear.addEventListener('click', function() {
