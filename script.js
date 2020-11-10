64017b71d06bd0a2a2db1294909244d878cb0df1
Nícolas Godek <nicolasgodek@gmail.com>
2020-11-10 17:56:26 -0300

    requisito 10

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ window.onload = function () {
 }
 
 function color() {
-    let colors = ['black','blue','yellow','green']
-    for (let i in colors){
+    let colors = ['black','blue','yellow','green'];
+    //rgb((Math.random()*255),(Math.random()*255),(Math.random()*255))
+    for (let i in colors) {
         let spot = document.createElement('li');
-        spot.classList = 'color'
+        spot.classList = 'color';
         spot.style.backgroundColor = colors[i];
         document.querySelector('#color-palette').appendChild(spot);
         selecting(spot);
@@ -17,21 +18,25 @@ function color() {
 color();
 
 function createTable() {
-    for ( let i = 0; i < 25; i += 1) {
-        let spot = document.createElement('li');
-        spot.classList = 'pixel';
-        spot.style.backgroundColor = 'white';
+    for (let i = 0; i < 5; i += 1) {
+        let spot = document.createElement('div');
         document.querySelector('#pixel-board').appendChild(spot);
-        colorChange(spot);
-        clear(spot);
+        spot.style.margin = 0;
+        for (let i = 0; i < 5; i += 1) {
+            let spot = document.createElement('div');
+            spot.classList = 'pixel';
+            document.querySelector('#pixel-board').appendChild(spot);
+            colorChange(spot);
+            clear(spot);
+        }
     }
 }
 
 createTable();
 
 function selecting(element) {
-    element.addEventListener('click',function(event) {
-        let selected = document.querySelector('.selected')
+    element.addEventListener('click', function(event) {
+        let selected = document.querySelector('.selected');
         selected.className = 'color';
         event.target.className = 'color selected';
     })
@@ -46,7 +51,39 @@ function colorChange(element){
 
 function clear(element) {
     let button = document.querySelector('#clear-board');
-    button.addEventListener('click',function(){
+    button.addEventListener('click', function() {
         element.style.backgroundColor = 'white';
     })
 }
+
+function generateBoard() {
+    let input = document.querySelector('#board-size');
+    let button = document.querySelector('#generate-board');
+    button.addEventListener('click', function() {
+        if (input.value === ''){
+            alert('Board inválido!')
+        } else {
+            if (input.value > 50) {
+                input.value = 50;
+            } else if (input.value < 5) {
+                input.value = 5;
+            }
+            let board = document.querySelector('#pixel-board');
+            board.innerHTML = '';
+            for (let i = 0; i < input.value; i += 1) {
+                let spot = document.createElement('div');
+                board.appendChild(spot);
+                spot.style.margin = 0;
+                for (let i = 0; i < input.value; i += 1) {
+                    let spot = document.createElement('div');
+                    spot.classList = 'pixel';
+                    board.appendChild(spot);
+                    colorChange(spot);
+                    clear(spot);
+                }
+            }
+        }   
+    })
+}
+
+generateBoard();
