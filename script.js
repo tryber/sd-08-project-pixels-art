57eb513f7b2ec37c0ff545922ee4802acd379e6c
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-10 11:57:43 -0300

    Arrumando erros code climate

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-//Requisito 06
-//Função que adiciona a classe selected na cor inicial 'preta'
-//Raciocinio encontrado em uma discussão no StackoverFlow (https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro)
+// Requisito 06
+// Função que adiciona a classe selected na cor inicial 'preta'
+// Raciocinio encontrado em uma discussão no StackoverFlow (https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro)
 function addSelect() {
-    document.querySelector('#color1').classList.add("selected");
+    document.querySelector('#color1').classList.add('selected');
 }
 addSelect();
 
-//Requisito 07
+// Requisito 07
 function classSelected() {
-    //Recuperando cores no html
-    let classColors = document.querySelectorAll('.color');
-    //Percorrendo cada cor, e adicionando o evento de click
+    // Recuperando cores no html
+    const classColors = document.querySelectorAll('.color');
+    // Percorrendo cada cor, e adicionando o evento de click
     for (let index = 0; index < classColors.length; index += 1) {
         let tagClass = classColors[index];
 
@@ -28,7 +28,7 @@ function classSelected() {
 }
 classSelected();
 
-//Requisito 08
+// Requisito 08
 function changeColor() {
     let classPixel = document.querySelectorAll('.pixel');
 
@@ -41,7 +41,7 @@ function changeColor() {
 }
 changeColor();
 
-//Requisito 09
+// Requisito 09
 function clearPixels() {
     let recoverSection = document.querySelector('#location-button');
     let button = document.createElement('button');
