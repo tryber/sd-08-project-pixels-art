c21700259c850be6bf9f245db746000fc8e7ed64
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-11 00:44:01 -0300

    requiriment 6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Criação da paleta de 4 cores
+//  Criação da paleta de 4 cores
 
 const colorList = ['black', 'Brown', 'gold', 'turquoise'];
 const colorPalette = document.getElementById('color-palette');
@@ -6,11 +6,14 @@ const colorPalette = document.getElementById('color-palette');
 for (let i = 0; i < colorList.length; i += 1) {
     const div = document.createElement('div');
     div.className = 'color';
+    if (colorList[i] == 'black') {
+        div.className = 'color selected'
+    }
     div.style.backgroundColor = colorList[i];
     colorPalette.appendChild(div);
 }
 
-//Criação do quadro de pixel
+//  Criação do quadro de pixel
 
 const elementNum = 5;
 
