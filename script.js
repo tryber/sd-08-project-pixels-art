ad37c003223c6cbc3485632f5cbdf74edea15684
Nícolas Tozato <ntozato9@gmail.com>
2020-11-11 20:39:34 -0300

    testeee

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = iniciaPreto;
 
 function iniciaPreto() {
-    let preto = document.getElementsByClassName('color')[0];
+    let preto = document.getElementsByClassName('.color')[0];
     preto.classList.add('selected');
 }
 
@@ -16,11 +16,3 @@ function selector (event) {
 const palette = document.getElementById('color-palette');
 palette.addEventListener('click', selector);
 
-clearButton = document.getElementById("clear-board");
-let pixels = document.querySelectorAll('.pixel');
-
-clearButton.addEventListener("click", function () {
-    for (index = 0; index < pixels.length; index++) {
-        pixels[index].style.backgroundColor = "white";
-    }
-});
