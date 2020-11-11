dc8770a7f79d8eb29876c1fdb44907c5473360cd
Nícolas Tozato <ntozato9@gmail.com>
2020-11-11 20:48:45 -0300

    teste 2343

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = iniciaPreto;
 
 function iniciaPreto() {
-    let preto = document.getElementsByClassName('.color')[0];
+    let preto = document.getElementsByClassName('color')[0];
     preto.classList.add('selected');
 }
 
@@ -16,3 +16,13 @@ function selector (event) {
 const palette = document.getElementById('color-palette');
 palette.addEventListener('click', selector);
 
+function paintPixel() {
+    const pixelSelected = document.querySelectorAll(".pixel");
+    for (let index = 0; index < pixelSelected.length; index += 1) {
+      pixelSelected[index].addEventListener("click", function (event) {
+        let colorCurrent = document.querySelector(".selected").style.backgroundColor;
+        event.target.style.backgroundColor = colorCurrent;
+      });
+    }
+  }
+  paintPixel();
