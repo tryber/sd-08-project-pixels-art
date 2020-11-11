05062ebdf7d121fe6727f9ffb2675a72f4675c8f
JRGCast <joao.ralph1@gmail.com>
2020-11-11 08:53:39 -0300

    trocado let da linha 21 do JS para const

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function paletaCores() {
   const palette = document.getElementById('color-palette');
 
   for (let i = 0; i < coresFundo.length; i += 1) {
-    let quadradosCores = document.createElement('li');
+    const quadradosCores = document.createElement('li');
     quadradosCores.className = 'color';
     quadradosCores.style.backgroundColor = coresFundo[i];
     palette.appendChild(quadradosCores);
