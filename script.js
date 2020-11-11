f56c8842986a79429432b738fd454aafab7e925c
Digo-Castro <dygo.castro@gmail.com>
2020-11-11 17:22:20 -0300

    Adiciona funcionalidade ao botão 'Limpar'

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btnRed = document.getElementById('red');
 const btnOrange = document.getElementById('orange');
 const btnYellow = document.getElementById('yellow');
 const btnPixel = document.getElementById('pixel-board');
+const pixelList = document.getElementsByClassName('pixel');
+const btnClear = document.getElementById('clear-board');
 
 function saveColor(key, value) {
   sessionStorage.setItem(key, value);
@@ -27,8 +29,17 @@ btnRed.addEventListener('click', classChange);
 btnOrange.addEventListener('click', classChange);
 btnYellow.addEventListener('click', classChange);
 
-btnPixel.addEventListener('click', changeColor);
 function changeColor(evt) {
   const color = sessionStorage.getItem('selectedColor');
   evt.target.style.backgroundColor = color;
 }
+
+btnPixel.addEventListener('click', changeColor);
+
+function reset(){
+  for (let index = 0; index < pixelList.length; index += 1) {
+    pixelList[index].style.backgroundColor = 'white';
+  }
+}
+
+btnClear.addEventListener('click', reset);
