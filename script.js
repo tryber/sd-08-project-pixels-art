91d18cfa8d5fe735b0e8a6818bce56bc50657ca3
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-10 23:09:56 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTable() {
+    const table = document.querySelector('#pixel-board');
+  
+    for (let index = 0; index < 5; index += 1) {
+      for (let index2 = 0; index2 < 5; index2 += 1) {
+        const tableDiv = document.createElement('tr');
+        tableDiv.className = 'pixel';
+        table.appendChild(tableDiv);
+      }
+    }
+  }
+
+addTable();
