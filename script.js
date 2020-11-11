aa039cf5ca6e5b4a3246641123b7ef0716ac2452
Fernanda Marques <nandateixmarques@gmail.com>
2020-11-10 23:31:34 -0300

    corrigindo o requisito 7

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ for (let col = 0; col < 5; col += 1) {
         pixel.appendChild(pix);
     }
 }
-window.onload = function(){  //seleciona a cor black assim ue carrega a pagina
-    document.getElementById('black').classList.add('selected');
+window.onload = function () {
+    document.getElementById('black').classList.add('selected')
+    addEventListener("click", function (event) {
+        if (event.target.className == "color") {
+            for (let index = 0; index < color.length; index += 1) {
+            color[index].className = "color";
+            }
+            event.target.className = "color selected";
+        }
+    });
+    addEventListener('click', function (event) {
+        for (let index = 0; index < color.length; index += 1) {
+            if (color[index].className == 'color selected' && event.target.className == 'pixel') {
+                event.target.style.backgroundColor = getComputedStyle(color[index]).backgroundColor;
+            }
+        }
+    });
 }
 
-
 function limpar(){
-    document.getElementById('pixel-board').style.backgroundColor = 'white';
+    document.getElementById('pixel').style.backgroundColor = 'white';
 }
 limpar();
