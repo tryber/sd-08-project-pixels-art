16048890aff518a5da7d3744c792c1c54e82f463
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-11 00:09:30 -0300

    :rocket: script com seleção de cores pronto

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
 // Paleta de cores 
+    function createPallete() {
+    let sq = document.getElementById('color-pallete');
+        for(index = 0; index < 4; index++){
+            let pallete = document.createElement('div');
+            pallete.className = 'color';
+            sq.appendChild(pallete);
+        }
+    }
 
-// cores da paleta 
+createPallete();
 
+// Cores da paleta
 document.querySelector('#color-pallete').children[0].style.backgroundColor = 'black';
 document.querySelector('#color-pallete').children[1].style.backgroundColor = 'red';
 document.querySelector('#color-pallete').children[2].style.backgroundColor = 'blue';
 document.querySelector('#color-pallete').children[3].style.backgroundColor = 'yellow';
+
+
+
+
+// Pixel - Board 
+    function createSq() {
+
+    let sq = document.getElementById('pixel-board');
+
+        for (index = 0; index < 5; index++){
+
+            let column = document.createElement('div');
+
+            for (indexTwo = 0; indexTwo < 5; indexTwo++){
+
+                let pixel = document.createElement('div');
+                pixel.className = 'pixel';
+                column.appendChild(pixel);
+            }
+        sq.appendChild(column)
+        }
+    }
+
+createSq();
+
+
+// Variavel que armazena cor para pintar o quadro
+var masterColor = 'black';
+
+// Selecionando a cor na paleta
+addEventListener('click', function (event){
+    if (event.target.className === 'color'){
+       masterColor = event.target.style.backgroundColor;
+    }
+})
+
+// Inserindo cor no quadro 
+addEventListener('click', function (event){
+    if (event.target.className === 'pixel'){
+        event.target.style.backgroundColor = masterColor;
+    }
+})
