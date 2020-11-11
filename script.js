3b8fe7875741ee0fc4ae420cf83ac3c726135ad2
Blucilha <blucilha.nascimento@gmail.com>
2020-11-11 16:51:18 -0300

    Ajeita o tamanho dos pixels

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ for (let index = 0; index < cores.length; index += 1){
 
     paleta.appendChild(criaDiv);
 }
-
+/*tabela de pixel*/
 let tabela = document.querySelector("#pixel-board");
 for (let linha = 0; linha < 5; linha += 1){
-  let tabelaLinha = document.createElement("tr");
+  let tabelaLinha = document.createElement("div");
   tabela.appendChild(tabelaLinha);
-  let linhaLocal = document.querySelectorAll("tr")[linha];
+  tabelaLinha.className = "line";
+  let linhaLocal = document.querySelectorAll(".line")[linha];
 
     for (let coluna = 0; coluna < 5; coluna += 1){
-      let tabelaColuna = document.createElement("td");
+      let tabelaColuna = document.createElement("div");
       tabelaColuna.className = "pixel";
       linhaLocal.appendChild(tabelaColuna);
     }
