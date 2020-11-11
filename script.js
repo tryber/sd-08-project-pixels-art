1e5ccb61eabaa132843f535b977dc53b4ce99afa
Nícolas Tozato <ntozato9@gmail.com>
2020-11-11 13:43:00 -0300

    refazendo projeto/ requisitos 1, 2 e 3

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function eventsColor(event) {
-    const colorClass = document.querySelectorAll('.selected');
-    colorClass.classList.remove('selected');
-    event.target.classList.add('selected');
 
-    
-}
-let verde = document.getElementById('green');
-verde.addEventListener('click', eventsColor);
 
-let azul = document.getElementById('blue');
-azul.addEventListener('click', eventsColor);
 
-let vermelho = document.getElementById('red');
-vermelho.addEventListener('click', eventsColor);
-
-let preto = document.getElementById('black');
-preto.addEventListener('click', eventsColor);
 
 
