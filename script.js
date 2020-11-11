c0bd35a11469355c4c9249f70e6cf88d85cac7ba
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-11 13:28:53 -0300

    add rq2 -4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// 1
 function createDivs() {
     let newPallete = document.getElementById('color-pallete')
     for (i = 0; i < 4; i += 1) {
@@ -8,3 +8,17 @@ function createDivs() {
     }
     document.getElementsByClassName("color").style.border = "1px solid"
 }
+
+// 2
+let colorBlack = document.getElementsByClassName("color")
+function getColors(array) {
+    for (i = 0; i < array.length; i += 1) {
+            colorBlack.style.background-color == array[i]
+    }
+}
+getColors(["black", "green", "purple", "yellow"]);
+
+// 3
+
+
+
