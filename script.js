3edf88f8b06a86d04f1cb23e24d8bba464eea210
Rafael <rafaeljavaro750@gmail.com>
2020-11-11 14:45:25 -0300

    Req 7 - Ao clicar em uma das cores da paleta, a cor selecionada é que vai ser usada para preencher os pixels no quadro. As seguintes verificações serão feitas: A classe selected deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
    Somente uma das cores da paleta deve ter a classe selected de cada vez;
    
    Note que os elementos que deverão receber a classe selected devem ser os mesmos elementos que possuem a classe color, como especificado no requisito 2

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ function randomColor() {
   return `rgb(${c1},${c2},${c3})`;
 }
 
-function colorsOptions() {
+function creatColorsOptions() {
   const options = document.getElementById('color-palette');
   const colors = [];
   for (let i = 0; i < 4; i += 1) {
     colors[i] = document.createElement('ul');
     colors[i].classList.add('color');
+    colors[i].addEventListener('click', (e) => {
+      removeSelected();
+      colors[i].classList.add('selected');
+    });
     if (i === 0) {
       colors[i].style.backgroundColor = 'black';
       colors[i].classList.add('selected');
@@ -28,14 +32,43 @@ function creatPixelBoard() {
     pixelsLine.className = 'line';
     board.appendChild(pixelsLine);
     for (let i2 = 0; i2 < 5; i2 += 1) {
-      const pixelsColumn = document.createElement('div');
-      pixelsColumn.classList.add('pixel');
-      board.appendChild(pixelsColumn);
+      const pixels = document.createElement('div');
+      pixels.classList.add('pixel');
+      pixels.addEventListener('click', (e) => {
+        console.log('paintThePixel');
+        //e.target.style.backgroundColor = paintThePixel;
+      });
+      board.appendChild(pixels);
     }
   }
 }
 
+function removeSelected() {
+  const colors = document.getElementsByTagName('ul');
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].classList.remove('selected');
+  }
+}
+function printColorsOptions() {
+  const colorsOptions = document.querySelectorAll('.color');
+  console.log(colorsOptions);
+}
+
+function printPixelBoard() {
+  const piXelBoard = document.querySelectorAll('.pixel');
+  console.log(piXelBoard);
+}
+
+function printColorSelected() {
+  const colorSelected = document.querySelector('.selected').style.backgroundColor;
+  console.log(colorSelected);
+}
+
+
 window.onload = function () {
-  colorsOptions();
+  creatColorsOptions();
   creatPixelBoard();
-};
+  printColorsOptions();
+  printPixelBoard();
+  printColorSelected();
+}
