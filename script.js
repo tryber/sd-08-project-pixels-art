afa040cb621ef4e75f1e7ad9f07a8b9ef48864e6
Nícolas Tozato <ntozato9@gmail.com>
2020-11-11 11:59:03 -0300

    teste

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.onload = function () {
     document.querySelectorAll('.color')[0].classList.add('selected');
+
+}
+
+function eventsColor(event) {
+    const colorClass = document.querySelectorAll('.selected');
+    colorClass.classList.remove('selected');
+    event.target.classList.add('selected');
+
     
 }
+
+
