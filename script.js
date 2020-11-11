2f05231a68db3a37aeb133bd7f6b17212069a842
Aline Debastiani <debastianialine@gmail.com>
2020-11-11 20:19:54 -0300

    adiciona cores de forma dinamica

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ window.onload = function () {
     document.querySelector('.color').className = 'color selected'
 }
 
+function initialColors () {
+  let colors = document.querySelectorAll('.color');
+  
+  colors[0].style.backgroundColor = 'black';
+  colors[1].style.backgroundColor = 'blue';
+  colors[2].style.backgroundColor = 'red';
+  colors[3].style.backgroundColor = 'yellow';
+}
+initialColors();
+
 function pixelBoard () {
   let divBoard = document.querySelector('#pixel-board');
   for (let index = 1; index < 26; index += 1) {
