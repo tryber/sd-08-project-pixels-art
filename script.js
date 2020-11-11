3d9e24905b1cd4e4c65235a3c6ce98534c90af42
Victor Miwa <victoryukimiwa@hotmail.com>
2020-11-10 23:49:24 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -11,3 +11,43 @@ function addTable() {
   }
 
 addTable();
+
+function blackSelected() {
+    const black = document.querySelector('.color');
+    black.className = 'selected';
+}
+
+blackSelected();
+
+function colorSelected() {
+    const colorPalette = document.querySelector('#color-palette');
+    let black = document.querySelector('#black');
+    let red = document.querySelector('#red');
+    let green = document.querySelector('#green');
+    let blue = document.querySelector('#blue');
+    const reset = 'color';
+
+    colorPalette.addEventListener('click', function(event) {
+      black.className = reset;
+      red.className = reset;
+      green.className = reset;
+      blue.className = reset;
+
+      event.target.className = 'selected';
+    });
+}
+
+colorSelected();
+
+function colorPaint() {
+    const table = document.querySelector('#pixel-board');
+
+    table.addEventListener('click', function(event) {
+        const selected = document.querySelector('.selected');
+        const color = selected.id;
+        
+        event.target.style.backgroundColor = color;
+    });
+}
+
+colorPaint();
