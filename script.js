a72a437a870a47a53d270fb6e8ddb9b669cf922b
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-10 23:29:19 -0300

    criaçao quadro de pixel

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//Paleta de cores
-const colorList = ['black', 'purple', 'gold', 'navy'];
+//Criação da paleta de 4 cores
+
+const colorList = ['black', 'Brown', 'gold', 'turquoise'];
 const colorPalette = document.getElementById('color-palette');
 
 for (let i = 0; i < colorList.length; i += 1) {
@@ -7,4 +8,18 @@ for (let i = 0; i < colorList.length; i += 1) {
     div.className = 'color';
     div.style.backgroundColor = colorList[i];
     colorPalette.appendChild(div);
+}
+
+//Criação do quadro de pixel
+
+const elementNum = 5;
+
+for (let i = 0; i < elementNum; i += 1) {
+    const pixelContainer = document.getElementById('pixel-board');
+    for (let j = 0; j < elementNum; j += 1) {
+        const div = document.createElement('div');
+        div.className = 'pixel';
+        div.style.backgroundColor = 'rgb(255, 255, 255)'
+        pixelContainer.appendChild(div);
+    }
 }
