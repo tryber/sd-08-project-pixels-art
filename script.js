80c22010bde0373518f880fe21e407348ca5cde4
Nícolas Tozato <ntozato9@gmail.com>
2020-11-10 21:07:50 -0300

    nova tentativa requisito 5

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = funcaoInicial;
+
+function funcaoInicial() {
+    let corPixels = document.querySelectorAll('.pixel');
+    corPixel[0].style.backgroundColor = 'white';
+    corPixel[1].style.backgroundColor = 'white';
+    corPixel[2].style.backgroundColor = 'white';
+    corPixel[3].style.backgroundColor = 'white';
+    corPixel[4].style.backgroundColor = 'white';
+    corPixel[5].style.backgroundColor = 'white';
+    corPixel[6].style.backgroundColor = 'white';
+    corPixel[7].style.backgroundColor = 'white';
+    corPixel[8].style.backgroundColor = 'white';
+    corPixel[9].style.backgroundColor = 'white';
+    corPixel[10].style.backgroundColor = 'white';
+    corPixel[11].style.backgroundColor = 'white';
+    corPixel[12].style.backgroundColor = 'white';
+    corPixel[13].style.backgroundColor = 'white';
+    corPixel[14].style.backgroundColor = 'white';
+    corPixel[15].style.backgroundColor = 'white';
+    corPixel[16].style.backgroundColor = 'white';
+    corPixel[17].style.backgroundColor = 'white';
+    corPixel[18].style.backgroundColor = 'white';
+    corPixel[19].style.backgroundColor = 'white';
+    corPixel[20].style.backgroundColor = 'white';
+    corPixel[21].style.backgroundColor = 'white';
+    corPixel[22].style.backgroundColor = 'white';
+    corPixel[23].style.backgroundColor = 'white';
+    corPixel[24].style.backgroundColor = 'white';
+
+}
