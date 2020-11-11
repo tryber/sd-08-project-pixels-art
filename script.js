553e1b7f10bc0e267a042a1832bc78792cc7403e
siebenei5 <siebenei7@gmail.com>
2020-11-11 18:15:37 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
+// adiciona color-palette
+function createElements(nome)
+{
+    let div = document.createElement("div");
+    div.className="color";
+    if(nome === "black")
+        div.className = "color selected"
+    document.getElementById("color-palette").appendChild(div);
+    div.id = nome;
+    div.style.backgroundColor = nome;
+}
+createElements("black");
+createElements("red");
+createElements("blue");
+createElements("green");
+
+
 //adiciona quadro de 5x5 pixels
 for(let i = 0; i < 5; i += 1)
 {
@@ -12,21 +31,17 @@ for(let i = 0; i < 5; i += 1)
 }
 
 
-
 // Cor selecionada do color-palette
-
 let findColor = document.getElementsByClassName("color");
-let findSelected = document.querySelector("selected");
 
 for(let i of findColor)
 {
-
     i.addEventListener("click", function()
     {
         for(let i of findColor)
     {
         if(i.classList.contains("selected"))
-            i.classList.remove("selected")
+            i.classList.remove("selected");
     }
         if(!this.classList.contains("selected"))
             this.classList.add("selected");
@@ -34,6 +49,18 @@ for(let i of findColor)
 }
 
 // colorir quadro clicado
+let findPixel = document.getElementsByClassName("pixel");
+
+for(let p of findPixel)
+{
+
+    p.addEventListener("click", function()
+    {
+        let para = document.getElementsByClassName('selected')[0];
+        let compStyles = window.getComputedStyle(para);
+        this.style.backgroundColor = compStyles.getPropertyValue('background-color');
+    })
+}
 
 
 
@@ -58,4 +85,8 @@ for(let i of findColor)
 
 
 
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 // https://www.youtube.com/watch?v=kmW857my2fo
