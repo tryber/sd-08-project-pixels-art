eca65d2853ef2c2ca0617391b53acb1bafe787f9
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-11 22:20:19 -0300

    add correction rq4/5

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function createDivs() {
 }
 createDivs();
 
+const pixelsLength = 5;
+function createPixels() {
+    let pixelBoard = document.getElementById('pixel-board');
+    for (i = 0; i < pixelsLength; i += 1) {
+        let line = document.createElement("tr");
+        line.className = `linha${i}`;
+        pixelBoard.appendChild(line)
+     }
+     for (index = 0; index < pixelsLength; index += 1) {
+         for (i = 0; i < pixelsLength; i += 1) {
+            let columPixel = document.querySelector(`.linha${i}`);
+            let pixel = document.createElement("td");
+            pixel.style.border = "1px solid"
+            pixel.className = "pixel";
+            columPixel.appendChild(pixel)  
+         }
+     }
+}
+createPixels();
