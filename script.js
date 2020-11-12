8d09c4f78615ac915468d9e9672eaf3e760db3a8
Marcela <marcela9908@gmail.com>
2020-11-12 09:55:15 -0300

    desafio 7

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function selectedColor() {
+window.onload = function () {
+  document.querySelector(".color");
+};
+
+let corSelecionada = "black";
+
+function CreateEventsColor(event) {
   const colorClass = document.querySelector(".selected");
-  colorClass.classList.remove(".selected");
-  Event.target.classList.add("selected");
+  colorClass.classList.remove("selected");
+  event.classList.add("selected");
+  corSelecionada = event.style.backgroundColor;
 }
