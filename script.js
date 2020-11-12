23b200f7d3d5b49697d098be4e8205f175ce2d8f
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-11 22:40:55 -0300

    Req 10 e 11

--- script.js
+++ script.js
@@ -1,116 +1,90 @@
 window.onload = () => {
-    document.getElementsByClassName('color')[0].classList.add('selected');
+  document.getElementsByClassName("color")[0].classList.add("selected");
 
-    startDefault();
+  startDefault();
 };
 
-let selectedColor = document.getElementsByClassName('color');
-for(let x of selectedColor){
-    x.addEventListener('click', (event) => {
-        let selectColor = document.querySelector('.selected');
-        if(x.classList.contains('selected') == false){
-            // Ref element.classList -> https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
-            selectColor.classList.remove('selected');
-            x.classList.add('selected');
-        }
-    })
-}
-
-function startDefault(){
-    for(let x = 0 ; x < 5; x += 1){
-        const pixelRow = document.createElement('tr');
-        document.querySelector('#pixel-board').appendChild(pixelRow);
-        pixelRow.className = `rowNumber${x}`;
-        for(let y = 0; y < 5; y += 1){
-            const pixelColumn = document.createElement('td');
-            document.querySelector(`.rowNumber${x}`).appendChild(pixelColumn);
-            pixelColumn.className = 'pixel';
-        }
-        
+let selectedColor = document.getElementsByClassName("color");
+for (let item of selectedColor) {
+  item.addEventListener("click", (event) => {
+    let selectColor = document.querySelector(".selected");
+    if (item.classList.contains("selected") == false) {
+      // Ref element.classList -> https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+      selectColor.classList.remove("selected");
+      item.classList.add("selected");
     }
-
-    startGame();
+  });
 }
 
+function startDefault() {
+  for (let x = 0; x < 5; x += 1) {
+    const pixelRow = document.createElement("tr");
+    document.querySelector("#pixel-board").appendChild(pixelRow);
+    pixelRow.className = `row rowNumber${x}`;
+    for (let y = 0; y < 5; y += 1) {
+      const pixelColumn = document.createElement("td");
+      document.querySelector(`.rowNumber${x}`).appendChild(pixelColumn);
+      pixelColumn.className = "pixel";
+    }
+  }
 
+  startGame();
+}
 
 function startGame() {
+  let pixels = document.getElementsByClassName("pixel");
 
-    let pixels = document.getElementsByClassName('pixel');
-
-    for(let x of pixels){
-        x.addEventListener('click', (pixelSelected) => {
-            let color = document.querySelector('.selected');
-            color = window.getComputedStyle(color,null);
-            // Ref window.getComputedStyle() -> https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-            pixelSelected.target.style.backgroundColor = color.backgroundColor;
-            console.log(pixelSelected.target);
-        })
-    }
+  for (let x of pixels) {
+    x.addEventListener("click", (pixelSelected) => {
+      let color = document.querySelector(".selected");
+      color = window.getComputedStyle(color, null);
+      // Ref window.getComputedStyle() -> https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+      pixelSelected.target.style.backgroundColor = color.backgroundColor;
+      console.log(pixelSelected.target);
+    });
+  }
 }
 
-let clearButton = document.querySelector('#clear-board');
+let clearButton = document.querySelector("#clear-board");
 
-clearButton.addEventListener('click', () => {
-    let pixels = document.getElementsByClassName('pixel');
-    for(let x of pixels){
-        x.style.backgroundColor = 'white';
-    }
+clearButton.addEventListener("click", () => {
+  let pixels = document.getElementsByClassName("pixel");
+  for (let x of pixels) {
+    x.style.backgroundColor = "white";
+  }
 });
 
+let generateBoard = document.querySelector("#generate-board");
+let valueField = document.querySelector("#board-size");
 
-let generateBoard = document.querySelector('#generate-board');
-let valueField = document.querySelector('#board-size');
+generateBoard.addEventListener("click", () => {
+  if (Number(valueField.value) >= 5 && Number(valueField.value) <= 50) {
+    createPixels(Number(valueField.value));
+  }
 
+  valueField.value = "";
+  startGame();
+});
 
+function createPixels(value) {
+  clearPixels();
 
-generateBoard.addEventListener('click', () =>{
-    if(Number(valueField.value) >=  5 && Number(valueField.value) <= 50){
-        createPixel(Number(valueField.value));
+  for (let x = 0; x < value; x += 1) {
+    const pixelRow = document.createElement("tr");
+    document.querySelector("#pixel-board").appendChild(pixelRow);
+    pixelRow.className = `row rowNumber${x}`;
+    for (let y = 0; y < value; y += 1) {
+      const pixelColumn = document.createElement("td");
+      document.querySelector(`.rowNumber${x}`).appendChild(pixelColumn);
+      pixelColumn.className = "pixel";
     }
-
-    valueField.value = '';
-    startGame();
-})
-
-
-function createPixel(value) {
-    
-    
-    for(let x = 0 ; x < value; x += 1){
-        const pixelRow = document.createElement('tr');
-        document.querySelector('#pixel-board').appendChild(pixelRow);
-        pixelRow.className = `rowNumber${x}`;
-        for(let y = 0; y < value; y += 1){
-            const pixelColumn = document.createElement('td');
-            document.querySelector(`.rowNumber${x}`).appendChild(pixelColumn);
-            pixelColumn.className = 'pixel';
-        }
-        
-    }
+  }
 }
 
 function clearPixels() {
-
+  let boardPixels = document.querySelectorAll(".row");
+  for (let index = 0; index < boardPixels.length; index += 1) {
+    boardPixels[index].parentNode.removeChild(boardPixels[index]);
+    // Ref Node -> https://developer.mozilla.org/pt-BR/docs/Web/API/Node
+  }
 }
-
-
-
-// sColor.addEventListener('click', function() {
-//     let selectedColor = document.querySelector('.selected');
-//     selectedColor.classList.remove('selected');
-//     sColor.classList.add('selected');
-//     console.log(sColor);
-// } );
-// tColor.addEventListener('click', function() {
-//     let selectedColor = document.querySelector('.selected');
-//     selectedColor.classList.remove('selected');
-//     tColor.classList.add('selected');
-//     console.log(tColor);
-// } );
-// fColor.addEventListener('click', function() {
-//     let selectedColor = document.querySelector('.selected');
-//     selectedColor.classList.remove('selected');
-//     fColor.classList.add('selected');
-//     console.log(fColor);
-// } );
