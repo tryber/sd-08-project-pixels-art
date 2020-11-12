ec7280c7816f45797da785d863c6e2191324a417
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-11 22:13:16 -0300

    add corretion -2 rq2/3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 2/3
 const colors = ["black", "green", "purple", "yellow"];
 function createDivs() {
     let newPalette = document.getElementById('color-palette')
@@ -9,28 +8,7 @@ function createDivs() {
         newDivs.style.border = "1px solid"
         newDivs.style.backgroundColor = color
         newPalette.appendChild(newDivs)
-
     }
 }
 createDivs();
 
-// 4
-const pixelsLength = 5;
-function createPixels() {
-    let pixelBoard = document.getElementById('pixel-board');
-    for (i = 0; i < pixelsLength; i += 1) {
-        let line = document.createElement("tr");
-        line.className = `linha${i}`;
-        pixelBoard.appendChild(line)
-     }
-     for (index = 0; index < pixelsLength; index += 1) {
-         for (i = 0; i < pixelsLength; i += 1) {
-            let columPixel = document.querySelector(`.linha${i}`);
-            let pixel = document.createElement("td");
-            pixel.style.border = "1px solid"
-            pixel.className = "pixel";
-            columPixel.appendChild(pixel)  
-         }
-     }
-}
-createPixels();
