d7a1e129da728c111772dcacae7124a243c92fe4
rodolfo_oliveira <rod.vgo@gmail.com>
2020-11-11 22:16:39 -0300

    color selected

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-//  Criação da paleta de 4 cores
+window.onload = function () {
+    let initialColor = document.getElementsByTagName('button')[0];
+    initialColor.className = 'color selected'
+};
 
+//  Criação da paleta de 4 cores
 const colorList = ['black', 'Brown', 'gold', 'turquoise'];
 const colorPalette = document.getElementById('color-palette');
 
 for (let i = 0; i < colorList.length; i += 1) {
-    const div = document.createElement('div');
+    const div = document.createElement('button');
     div.className = 'color';
-    if (colorList[i] == 'black') {
-        div.className = 'color selected'
-    }
     div.style.backgroundColor = colorList[i];
     colorPalette.appendChild(div);
 }
 
 //  Criação do quadro de pixel
-
 const elementNum = 5;
 
 for (let i = 0; i < elementNum; i += 1) {
@@ -26,3 +26,24 @@ for (let i = 0; i < elementNum; i += 1) {
         pixelContainer.appendChild(div);
     }
 }
+
+
+// Requisito
+
+function handleSelected (event) {
+    const setColorSelected = document.querySelectorAll('.selected')
+    for (let index = 0; index < setColorSelected.length; index += 1) {
+        setColorSelected[index].classList.remove('selected')
+    }
+    event.target.classList.add('selected')
+    
+}
+
+ function setColorSelected() {
+    const colorSelected = document.querySelectorAll('.color');
+    for (let index = 0; index < colorSelected.length; index += 1) {
+        const currentColor = colorSelected[index];
+        currentColor.addEventListener('click', handleSelected )
+    }
+ }
+setColorSelected()
