67ab4044f3f6c253ef0684047e4416e90e202f64
Uibirá <usbarreto@gmail.com>
2020-11-12 16:20:22 -0300

    Req.7: input inicial

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-window.onload = function() {
-    let palletColor = document.querySelector("#color-palette");
-    let pixel = document.querySelector(".pixel");
+let pixelBox = document.querySelector("#pixel-board");
+//Cria 25 caixas de pixels
+let boardSize = 25;
+for (let index = 0; index < boardSize; index += 1) {   
+let pxItem = document.createElement('div');
+pxItem.className = "pixel"
+pixelBox.appendChild(pxItem);
+};
 
-    function paintPixel (){
-        pixel.style.backgroundColor = 'black';
-    }
 
-    pixel.addEventListener("click", paintPixel);
+let color = document.querySelectorAll(".color");
+
+for (let i = 0; i< color.length; i++){
+    let  pallet = color[i];
+    pallet.addEventListener('click', function(event){
+        let selectedColor = document.querySelector(".selected");
+        if (pallet.className === "color" ){
+        if (selectedColor !== null){
+            selectedColor.classList.remove("selected")
+        }          
+            event.target.classList.add("selected")
+        }
+    })
 }
+
+/*document.addEventListener('click', function(event){
+    //Seleciona a cor na paleta / alert ("Make it so!")
+    if (event.target.classList.contains("color")){
+        selectedColor[0].className = "color"
+        event.target.classList.add("selected");           
+        
+        console.log(selectedColor)            
+    }
+    //Pinta os pixels
+    else if (event.target.classList.contains("pixel")){
+        event.target.style.background = selectedColor.style.background;
+    }
+});*/
