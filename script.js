bccd30059d171ccfdbdeba44ff70874a9e71132b
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-11-13 20:56:10 -0300

    requisito 7 remoção da class selected segunda tentativa

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ function backgroundWhite(){
 }
 window.onload=backgroundWhite;
 
-let corSelecionada = document.querySelectorAll('.color');
 
-for (let i = 0; i < corSelecionada.length; i += 1) {
-  let atual = corSelecionada[i];
-  atual.addEventListener('click', function (event) {
-    for (let j = 0; j < corSelecionada.length; j += 1) {
-      let marcado = corSelecionada[j];
-      if (marcado.classList.contains('selected')) {
-        marcado.classList.remove('selected');
-      }
-    }
-    event.target.classList.add('selected');
-  });
+let selectedColor = document.getElementById('#strt-color');
+const paletteColors = document.querySelectorAll('.color');
+
+function switchSelectedColor(event) {
+  selectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
+  selectedColor = event.target;
+}
+
+for (const colorInPalette of paletteColors) {
+  colorInPalette.addEventListener('click', switchSelectedColor);
 }
