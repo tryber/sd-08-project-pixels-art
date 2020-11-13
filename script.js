79ba78a0b1f9ea64a053819d1c55d01acedd3667
Nícolas Tozato <ntozato9@gmail.com>
2020-11-13 20:05:38 -0300

    refazendo do inicio - tentativa requisito 9

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function selector (event) {
 const palette = document.getElementById('color-palette');
 palette.addEventListener('click', selector);
 
+
+function clearBoard () {
+    let board = document.querySelectorAll('.pixel');
+    
+    for (let i = 0; i < board.length; i++) {
+        board[i].style.backgroundColor = 'white';
+    }        
+
+    
+}
+
+
+let clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', clearBoard)
