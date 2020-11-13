6e5a490737d03fb83c252304a7f51bb29c1547c3
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-11-13 20:31:23 -0300

    Requisito 6 e 7

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function backgroundWhite(){
     pixel.style.backgroundColor='white';
 }
 window.onload=backgroundWhite;
+
+let allColors=document.querySelector('#collor-palette');
+let color=allColors.childNodes;
+
+function removeSelected(){
+    for (let inedx=0;index <color.length;index++){
+        if (color[index].className ='color selected'){
+            color[index].className ='color';
+        }
+    }
+}
+
+function addSelected(){
+    allColors.addEventListener('click',function(event){
+        removeSelected();
+        event.target.className = 'color selected';
+    })
+}
+
+addSelected();
