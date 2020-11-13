f173980d5d5100b9ad2905625e55c9d4f632e718
Marcela <marcela9908@gmail.com>
2020-11-13 12:50:21 -0300

    Correçao do codigo

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-window.onload = function () {
-  document.querySelector(".color");
-};
+let colors = ["black", "red", "yellow", "blue"];
 
-let corSelecionada = "black";
+let colorPalet = document.querySelector("#color-palette");
+let pixelBoard = document.querySelector("#pixel-board");
 
-function CreateEventsColor(event) {
-  const colorClass = document.querySelector(".selected");
-  colorClass.classList.remove("selected");
-  event.classList.add("selected");
-  corSelecionada = event.style.backgroundColor;
+for (let index = 0; index < colors.length; index += 1) {
+  let divPalet = document.createElement("div");
+  colorPalet.appendChild(divPalet);
+  divPalet.className = "color";
+  divPalet.style.backgroundColor = colors[index];
+}
+
+for (let index = 0; index < 25; index += 1) {
+  let divTable = document.createElement("div");
+  pixelBoard.appendChild(divTable);
+  divTable.className = "pixel";
+  divTable.style.backgroundColor = "white";
 }
