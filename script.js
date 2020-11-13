cfb6def4fd321b2b5cbaabf1163ba845b8d61ca6
Pedro Piquini <piquini.pedro@gmail.com>
2020-11-13 16:09:17 -0300

    adicionado o select na paleta preta

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.onload = createTable(), coloringPallet();
-
+window.onload = createTable(), coloringPallet(), blackInitialSelect();
+// Função para criar a paleta de cores
 function coloringPallet() {
     const palletColors = ['black', 'purple', 'green', 'pink'];
     const pallet = document.getElementsByClassName('color');
@@ -8,6 +8,7 @@ function coloringPallet() {
     }
 }
 
+// Função para criar a tabela de pixels
 function createTable() {
     let pixelBoard = document.getElementById('pixel-board');
     console.log(pixelBoard)
@@ -15,7 +16,14 @@ function createTable() {
         for (let j = 0; j < 5; j += 1) {
             let pixel = document.createElement('div');
             pixel.className = 'pixel';
+            pixel.backgroundColor = 'white';
             document.getElementById('pixel-board').appendChild(pixel);
         }
     }
+}
+
+//Função para selecionar cores da paleta
+function blackInitialSelect () {
+    const colorSelect = document.getElementsByClassName('color')[0];
+    colorSelect.classList.add('selected');
 }
