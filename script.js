6841851ca8409904127324a482ae24968e21ac70
Marcela <marcela9908@gmail.com>
2020-11-14 16:57:40 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let colors = ["black", "red", "yellow", "blue"];
 let colorPalet = document.querySelector("#color-palette");
 let pixelBoard = document.querySelector("#pixel-board");
+
 //crição paleta de cores
 for (let index = 0; index < colors.length; index += 1) {
   let divPalet = document.createElement("div");
@@ -19,16 +20,39 @@ for (let index = 0; index < 25; index += 1) {
   divTable.className = "pixel";
   divTable.style.backgroundColor = "white";
 }
-//criação evento
+//criação evento remover e adicionar selected
+
 colorPalet.addEventListener("click", alteraSelected);
 
 const removederSeletect = document.getElementsByClassName("selected");
+
+let cor = "black";
 
 function alteraSelected(event) {
   for (let i = 0; i < removederSeletect.length; i++) {
     removederSeletect[i].classList.remove("selected");
   }
   const evento = event.target;
-  corDaDiv = evento.style.backgroundColor;
+  cor = evento.style.backgroundColor;
   evento.classList.add("selected");
+  console.log(cor);
+}
+//Criação de evento para pintar os pixels
+pixelBoard.addEventListener("click", pintaPixel);
+
+function pintaPixel(event) {
+  let evento = event.target;
+  evento.style.backgroundColor = cor;
+}
+
+//Criação de evento para limpar
+const limparPixels = document.querySelector("#clear-board");
+let cadaPixel = document.querySelectorAll(".pixel");
+
+limparPixels.addEventListener("click", limpaTabela);
+
+function limpaTabela() {
+  for (let valor of cadaPixel) {
+    valor.style.backgroundColor = "white";
+  }
 }
