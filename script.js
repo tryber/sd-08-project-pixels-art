4693d383a44b33d2bde5ed180daafe7fea857589
Giovanna Betti <giovannabetti@gmail.com>
2020-11-14 19:23:47 -0300

    Funcao forEach funcionando para paleta de cores

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
+    const pixelBoard = document.getElementById('pixel-board');
+    const colorsArray = ["black", "yellow", "orange", "red"];
+
+    colorsArray.forEach(function(color) {
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.className = 'color';
+      button.style.height = '40px';
+      button.style.width = '40px';
+      button.style.backgroundColor = color;
+      button.style.border = "1px solid #000";
+      button.addEventListener('click', handler(button));
+      pixelBoard.appendChild(button);
+});
+
 window.onload = function() {
   document.querySelectorAll('.color')[0].classList.add("selected");
 };
 
-function palette() {
-  const colorsArray = ["#000", "#FF9933", "#FF6600", "#FF3300",];
-  const colors = document.getElementById('color-palette');
-  colors.forEach = function(cor) {
-    const paletteColorDiv = document.getElementById("color-palette");
-    paletteColorDiv.addEventListener("click", createEventsColor);
-    for(let index = 0; index < colorsArray.length; index++) {
-      const colorDiv = document.createElement("div");
-      colorDiv.style.backgroundColor = colorsArray[index];
-      paletteColorDiv.appendChild(colorDiv);
-      colorDiv.className = "color";
-    };
-
-
-    let button = '';
-      button.document.createElement('button');
-      button.value = cor;
-      button.type = 'button';
-      button.className = 'color';
-      button.style.width = '40px';
-      button.style.height = '40px';
-      button.style.backgroundColor = cor;
-      button.style.border = "1px solid #000";
-      button.addEventListener('click', handler(button));
-      colors.appendChild(button);
-  });
+function handler(el) {
+    return function() {
+        pixelBoard.style.backgroundColor = el.value;
+    }
 };
-
-button[0].classList.add('selected');
-
-const colorPalette = document.querySelector('.color-palette');
-
-const pixelBoard = document.querySelector('.pixel-board');
-
-let boardSize = 5;
-
-function createBoard(n) {
-
-}
-
