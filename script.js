2ade12be39fc8942a2c29bad2fdaae19ecaacabd
Marcela <marcela9908@gmail.com>
2020-11-14 16:13:29 -0300

    Correçao

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let colors = ["black", "red", "yellow", "blue"];
-
 let colorPalet = document.querySelector("#color-palette");
 let pixelBoard = document.querySelector("#pixel-board");
-
 //crição paleta de cores
 for (let index = 0; index < colors.length; index += 1) {
   let divPalet = document.createElement("div");
   colorPalet.appendChild(divPalet);
   divPalet.className = "color";
   divPalet.style.backgroundColor = colors[index];
-
   //adicionando o selected para o primeiro elemento do array
   if (index == 0) {
     divPalet.classList.add("selected");
   }
 }
-
 //criação da tabela
 for (let index = 0; index < 25; index += 1) {
   let divTable = document.createElement("div");
@@ -24,15 +20,15 @@ for (let index = 0; index < 25; index += 1) {
   divTable.style.backgroundColor = "white";
 }
 //criação evento
-
 colorPalet.addEventListener("click", alteraSelected);
 
-const removerSelected = document.querySelectorAll(".selected");
+const removederSeletect = document.getElementsByClassName("selected");
 
 function alteraSelected(event) {
-  let evento = event.target;
-  for (index = 0; index < removerSelected.length; index += 1) {
-  removerSelected[index].classList.remove("selected");
-  
+  for (let i = 0; i < removederSeletect.length; i++) {
+    removederSeletect[i].classList.remove("selected");
+  }
+  const evento = event.target;
+  corDaDiv = evento.style.backgroundColor;
   evento.classList.add("selected");
 }
