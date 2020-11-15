176e14994d995ae5b747f5a404f873baced9074d
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-15 16:15:54 -0300

    realizado requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Variáveis
 const boxPalette = document.getElementsByClassName('color'); //elementos com class='color'
 const colors = ["black", "red", "orange", "yellow"]; // array de cores
+const palette = document.getElementById('color-palette'); // seleciona o palette
+const table = document.getElementById('pixel-board'); // seleciona a table
 
 //roda as funções quando a página é carregada
 window.onload = criateColorsPalette(boxPalette,colors), firstSelectedColor(boxPalette);
@@ -18,15 +20,18 @@ function firstSelectedColor(boxPalette) {
 }
 
 //pega a cor selecionada do palette
-let palette = document.getElementById('color-palette');
-
-palette.addEventListener('click', function click(event) {
+palette.addEventListener('click', function clickPalette(event) {
     let selected = document.getElementsByClassName('selected'); // cor selecionada
     selected[0].className = 'color';
-    
+
     selectColor = event.target;
     selectColor.className = 'color selected';
 })
 
+//Pinta os boxes da table
+table.addEventListener('click', function clickTable(event) {
+    let selected = document.getElementsByClassName('selected'); // cor selecionada
+    event.target.style.backgroundColor = selected[0].style.backgroundColor;
+})
 
 
