50b80e6eba2249ab1bb2aa9510f2ae9f83f5556c
Victor Costa <victordiricoo@gmail.com>
2020-11-14 22:44:07 -0300

    testando evaluator

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function pixelBoard(){
-    const pixelList = document.querySelector('#pixel-board');
+//function pixelBoard(){
+   // const pixelList = document.querySelector('#pixel-board');
 
-    for (let index = 1; index < 25; index += 1) {
-        const pixelElement = document.createElement('li');
+  //  for (let index = 1; index < 25; index += 1) {
+      //  const pixelElement = document.createElement('li');
 
-        pixelList.appendChild(pixelElement);
-        pixelElement.className('pixel')
-    }
-}
-pixelBoard();
+     //   pixelList.appendChild(pixelElement);
+     //   pixelElement.className('pixel')
+  //  }
+//}
+//pixelBoard();
