75c1b267812e025f27cfbc740ea714f529663029
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-15 01:54:52 -0300

    criado tabela

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-window.onload = criatePalette();
+window.onload = criateColorsPalette(), createBoxes();
 
-function criatePalette() {
+function criateColorsPalette() {
     const colors = ["black", "red", "orange", "yellow"];
     const div = document.getElementsByClassName('color');
   
     for (key in colors) {
         div[key].style.backgroundColor = colors[key];
     }
+}
+
+function createBoxes() {
+
 }
