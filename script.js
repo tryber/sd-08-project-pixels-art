1f519036ce4563b334724fe18c258e3f8b44fcf3
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-15 15:40:06 -0300

    selecionado black como primeira cor

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-window.onload = criateColorsPalette(), createBoxes();
+//Variáveis
+const boxPalette = document.getElementsByClassName('color'); //elementos com class='color'
+const colors = ["black", "red", "orange", "yellow"]; // array de cores
 
-function criateColorsPalette() {
-    const colors = ["black", "red", "orange", "yellow"];
-    const div = document.getElementsByClassName('color');
-  
+//roda as funções quando a página é carregada
+window.onload = criateColorsPalette(boxPalette,colors), firstSelectedColor(boxPalette);
+
+//Preenche as cores do palette
+function criateColorsPalette(boxPalette,colors) {    
     for (key in colors) {
-        div[key].style.backgroundColor = colors[key];
+        boxPalette[key].style.backgroundColor = colors[key];
     }
 }
 
-function createBoxes() {
-
+//seleciona cor black
+function firstSelectedColor(boxPalette) {
+    boxPalette[0].className = 'color selected'
 }
+
+//pega a cor selecionada do palette
+let palette = document.getElementById('color-palette');
+
+palette.addEventListener('click', function (event) {
+    selectedColor = event.target;
+    
+    selectedColor.className = 'color selected';
+    return selectedColor;
+})
+
+
+
