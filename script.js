58f3c91b0069d93ee1f80651c53f0b7e46fa2a6a
Andre Sampaio <andresampaiolm@gmail.com>
2020-11-14 21:04:39 -0300

    input[type:range] trocado para input[type:number]

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 
-let colors = [ 'rgb( 0 , 0 , 0 )' ];
+let colors = ['rgb( 0 , 0 , 0 )'];
 let colorSelected = 'rgb( 0 , 0 , 0 )';
 let boardSize = 25;
 
-let palettePlayground = {
+const palettePlayground = {
   paletteGenerator() {
     for (let palette = 0; palette < 3; palette += 1) {
       colors.push(this.colorGenerator());
     }
 
     if (new Set(colors).size !== colors.length) {
-      colors = [ 'rgb( 0 , 0 , 0 )' ];
-      palleteGenerator();
+      colors = ['rgb( 0 , 0 , 0 )'];
+      this.paletteGenerator();
     }
 
     this.paletteDivsGenerator();
   },
   colorGenerator() {
     let color;
-      for (let rgb = 0; rgb < 3; rgb += 1) {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        color = `rgb( ${r} , ${g} , ${b} )`;
-      }
-    
+
+    for (let rgb = 0; rgb < 3; rgb += 1) {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      color = `rgb( ${r} , ${g} , ${b} )`;
+    }
+
     return color;
   },
   paletteDivsGenerator() {
-    let colorPalette = document.querySelector('#color-palette');
-  
+    const colorPalette = document.querySelector('#color-palette');
+
     for (let index = 0; index < colors.length; index += 1) {
-      let div = document.createElement('div');
-      let node = document.createTextNode('');
+      const div = document.createElement('div');
+      const node = document.createTextNode('');
       div.appendChild(node);
       if (index === 0) {
         div.classList = 'color selected';
@@ -48,48 +49,49 @@ let palettePlayground = {
     colorSelected = event.target.style.backgroundColor;
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
-  }
-}
+  },
+};
 
-palettePlayground.paletteGenerator()
+palettePlayground.paletteGenerator();
 
-let pixelBoardPlayground = {
+const pixelBoardPlayground = {
   setNewBoardSize() {
     let inputSize = document.querySelector('#board-size').value;
-    
+
     if (inputSize === null) {
-      alert('Board inválido!')
+      alert('Board inválido!');
     }
-  
+
+    inputSize = inputSize < 5 ? 5 : inputSize;
+    inputSize = inputSize > 50 ? 50 : inputSize;
+
     boardSize = inputSize * inputSize;
-  
+
     pixelBoardPlayground.pixelBoardGenerator();
   },
   pixelBoardGenerator() {
-    let pixelBoard = document.querySelector('#pixel-board');
-  
+    const pixelBoard = document.querySelector('#pixel-board');
+
     pixelBoard.innerHTML = '';
     pixelBoard.style.gridTemplateColumns = `repeat(${Math.sqrt(boardSize)}, 40px)`;
     pixelBoard.style.gridTemplateRows = `repeat(${Math.sqrt(boardSize)}, 40px)`;
-  
+
     for (let index = 0; index < boardSize; index += 1) {
-      let pixel = document.createElement('div');
-      let node = document.createTextNode('');
+      const pixel = document.createElement('div');
+      const node = document.createTextNode('');
       pixel.appendChild(node);
       pixel.classList.add('pixel');
       pixel.addEventListener('click', pixelBoardPlayground.colorPixel);
-      pixelBoard.appendChild(pixel)
+      pixelBoard.appendChild(pixel);
     }
   },
   colorPixel(event) {
     event.target.style.backgroundColor = colorSelected;
   },
   clearBoard() {
-    let pixels = document.querySelectorAll('.pixel');
-    for (let pixel of pixels) {
-      pixel.style.backgroundColor = 'rgb( 255 , 255 , 255 )';
-    }
-  }
-}
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => { pixel.style.backgroundColor = 'rgb( 255 , 255 , 255 )'; });
+  },
+};
 
-pixelBoardPlayground.pixelBoardGenerator()
+pixelBoardPlayground.pixelBoardGenerator();
