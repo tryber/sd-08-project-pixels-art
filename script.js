b81952b0bf32c0f827f3fd0e3f48589a3f8aebd5
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-15 17:29:08 -0300

    cria estilizaçoes e conserta o codeClimate errors

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const pixelBoard = document.getElementById('pixel-board');
 function generateBoard(num) {
   for (let index = 1; index <= num; index += 1) {
     const divLine = document.createElement('div');
+    divLine.className = 'board-line';
     pixelBoard.appendChild(divLine);
 
     for (let j = 1; j <= num; j += 1) {
@@ -22,11 +23,12 @@ function createPalette() {
 
   const arrayColors = [];
   for (let i = 0; i < 3; i += 1) {
+    const rgbRandom = 'rgb(';
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
 
-    arrayColors.push('rgb(' + r + ',' + g + ',' + b + ')');
+    arrayColors.push(rgbRandom.concat(r, ' ,', g, ' ,', b, ')'));
   }
 
   firstColor.style.backgroundColor = 'black';
@@ -36,43 +38,38 @@ function createPalette() {
 }
 createPalette();
 
-function selectedColor() {
-  const colorPalette = document.querySelector('#color-palette');
+const colorPalette = document.querySelector('#color-palette');
 
-  function changeStatus(event) {
-    const selectedPalette = document.querySelector('.color.selected');
+
+function changeStatus(event) {
+  if (event.target.className === 'color') {
+    const selectedPalette = document.querySelector('.selected');
     selectedPalette.className = 'color';
     event.target.className = 'color selected';
   }
-
-  colorPalette.addEventListener('click', changeStatus);
 }
-selectedColor();
 
-function fillPixelColor() {
-  const pixelUnit = document.querySelector('#pixel-board');
+colorPalette.addEventListener('click', changeStatus);
 
-  function changeColor(event) {
-    const selectedColor = document.querySelector('.selected');
-    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+const pixelUnit = document.querySelector('#pixel-board');
+
+function changeColor(event) {
+  if (event.target.className === 'pixel') {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
   }
-
-  pixelUnit.addEventListener('click', changeColor);
 }
-fillPixelColor();
+pixelUnit.addEventListener('click', changeColor);
 
-function clearBoard() {
-  const clearPixels = document.getElementById('clear-board');
-  const pixelUnits = document.querySelectorAll('.pixel');
+const clearPixels = document.getElementById('clear-board');
+const pixelUnits = document.querySelectorAll('.pixel');
 
-  function clearPixelsColor() {
-    for (let i = 0; i < pixelUnits.length; i += 1) {
-      pixelUnits[i].style.backgroundColor = 'white';
-    }
+function clearPixelsColor() {
+  for (let i = 0; i < pixelUnits.length; i += 1) {
+    pixelUnits[i].style.backgroundColor = 'white';
   }
-  clearPixels.addEventListener('click', clearPixelsColor);
 }
-clearBoard();
+clearPixels.addEventListener('click', clearPixelsColor);
+
 
 function generateNewBoard() {
   const VQV = document.getElementById('generate-board');
