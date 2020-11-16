060587036792a7e6ff198801f5b6b8f89c39c20b
Lucas Duque <lucas3rios2014@gmail.com>
2020-11-16 16:22:20 -0300

    Fazendo requisito 6 e 7.

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ function fillPalete() {
   for (let index = 0; index < 4; index += 1) {
     const paleteItem = document.createElement('li');
     paleteItem.className = 'color';
+    paleteItem.addEventListener('click', changeSelected);
     paleteItem.style.backgroundColor = colorList[index];
+    firstSelect(paleteItem);
     palete.appendChild(paleteItem);
   }
 }
 
+function firstSelect(element) {
+  if (element.style.backgroundColor === 'black') {
+    element.className = 'color selected';
+  }
+}
+
 function fillBoard() {
-  const board = document.querySelector("#pixel-board");
+  const board = document.querySelector('#pixel-board');
   for (let rowIndex = 0; rowIndex < 5; rowIndex += 1) {
-    let row = document.createElement('tr');
+    const row = document.createElement('tr');
     for (let colIndex = 0; colIndex < 5; colIndex += 1) {
-      let col = document.createElement('td');
+      const col = document.createElement('td');
       col.className = 'pixel';
+      col.addEventListener('click', paint);
       row.appendChild(col);
     }
     board.appendChild(row);
   }
+}
+
+function changeSelected(event) {
+    let lastSelected = document.querySelector('.selected')
+    lastSelected.className = 'color';
+    let newSelected = event.target;
+    newSelected.className = 'color selected';
+}
+
+function paint(event) {
+  let selectedColor = document.querySelector('.selected');
+  let color = selectedColor.style.backgroundColor;
+  let selectedPixel = event.target;
+  selectedPixel.style.backgroundColor = color;
 }
 
 fillPalete();
