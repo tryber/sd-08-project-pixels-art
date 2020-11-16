cd58f3458de02f7e942327edbd2694b0de14c81a
Victor Costa <victordiricoo@gmail.com>
2020-11-15 23:01:30 -0300

    desafio 9

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+let button = document.getElementById('clear-board');
+
+button.addEventListener('click', evento);
+
+}
+
+function evento () {
+    const eventClearBoard = document.getElementsByClassName('pixel');
+    pixel.style.backgroundColor = "#FFFFFF";
+
+}
