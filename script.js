d6c67c95bead004476a00ec96b02dda5544807a8
maym4y <jennim.almeida@gmail.com>
2020-11-15 21:39:36 -0300

    Feito até requisito 5

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 function criaPixelBoard(number) {
     for (let linha = 0; linha < number; linha +=1) {
+        let row = document.createElement('div');
+        row.className = 'tr';
+        let board = document.querySelector('#pixel-board');
+        board.appendChild(row);
         for (let col = 0; col < number; col += 1) {
-            let pixel = document.createElement('div');
-            pixel.className = 'pixel';
-            let board = document.querySelector('#pixel-board');
-            board.appendChild(pixel);
-            
+            const pixels = document.createElement('div');
+            pixels.className = 'pixel';
+            row.appendChild(pixels);
         }
     }
 };
 
-criaPixelBoard(5);
+window.onload = criaPixelBoard(5);
+
+function colorirPixel(e) {
+    if (e.target.className === 'color') {
+    e.target.className += ' selected';
+    } else if (e.target.className === 'selected') {
+        e.target.removeAttribute('className', ' selected');
+    }
+
+    if (e.target.className === 'pixel') {
+        let cor = document.querySelector('.selected').id;
+        e.target.id = cor;
+    }
+
+
+    
+}
