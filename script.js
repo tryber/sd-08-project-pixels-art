16f841d013d008faf5086a7768a8a1b665d4edba
Lucas Duque <lucas3rios2014@gmail.com>
2020-11-16 12:41:26 -0300

    Concluindo requisito 2

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-
 function fillPalete() {
-    let colorList = ['black','red','green','blue'];
-    let palete = document.querySelector("#color-palete");
-    for(let index = 0; index < 4; index += 1) {
-        let paleteItem = document.createElement('li');
-        paleteItem.className = "color";
-        paleteItem.style.backgroundColor = colorList[index];
-        palete.appendChild(paleteItem);
+  const colorList = ['black', 'red', 'green', 'blue'];
+  const palete = document.querySelector('#color-palette');
+    for (let index = 0; index < 4; index += 1) {
+      const paleteItem = document.createElement('li');
+      paleteItem.className = 'color';
+      paleteItem.style.backgroundColor = colorList[index];
+      palete.appendChild(paleteItem);
     }
 }
-
 fillPalete();
