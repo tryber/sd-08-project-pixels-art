3a816dc334246d86749b2afed15fd442c1eebad8
Vinicius <viniciussilvaribeiro@gmail.com>
2020-11-16 20:22:55 -0300

    cores aleatorias

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 //Requisito 2
-function criacao() {
-const paiDiv = document.getElementById('color-palette');
-const cores = ["black", "red", "blue", "green"];
+// function criacao() {
+// const paiDiv = document.getElementById('color-palette');
+// const cores = ["black", "red", "blue", "green"];
 
-for (let index = 0; index < 4; index += 1) {
-    let filhaDiv = document.createElement("div");
-    paiDiv.appendChild(filhaDiv);
-    filhaDiv.className = "color";
-    colorDiv = cores[index];
-    filhaDiv.style.backgroundColor = colorDiv;
-    }
+// for (let index = 0; index < 4; index += 1) {
+//     let filhaDiv = document.createElement("div");
+//     paiDiv.appendChild(filhaDiv);
+//     filhaDiv.className = "color";
+//     colorDiv = cores[index];
+//     filhaDiv.style.backgroundColor = colorDiv;
+//     }
+// }
+// criacao();
+
+
+//Cores aleatoriamente
+const first = document.querySelector("#black")
+first.style.backgroundColor = "black";
+const second = document.querySelector("#red");
+const third = document.querySelector("#blue");
+const fourth = document.querySelector("#green");
+
+function corAleatoria() {
+    const red = Math.ceil(Math.random() * 255);
+    const green = Math.ceil(Math.random() * 255);
+    const blue = Math.ceil(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
+function aleatorias() {
+    second.style.backgroundColor = corAleatoria();
+    third.style.backgroundColor = corAleatoria();
+    fourth.style.backgroundColor = corAleatoria();
 }
-criacao();
+aleatorias();
 
 //requisito 6
 //document.querySelector('color').classList.add("selected");
