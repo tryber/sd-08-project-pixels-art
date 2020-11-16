80123c46470c4c58a07987aea10391c41f0dd2b9
WagnerMor <wagnermor@outlook.com>
2020-11-16 18:55:44 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 const colors = ['black', 'red', 'green', 'blue'];
+const classColor = 'color';
+const classColorSelected = 'color selected';
 
 // Cria a paleta de cores
 function creatColorPalette() {
     const colorPaletteList = document.getElementById("color-palette-list");
-    
     for (let index = 0; index < colors.length; index += 1) {
         const color = document.createElement('li');
         color.className = 'color';
-        color.style.display = 'inline-block';
-        color.style.width = '40px';
-        color.style.height = '40px';
-        color.style.border = 'solid 1px #000';
         color.style.background = colors[index];
-        
         colorPaletteList.appendChild(color);
     }
 }creatColorPalette();
-
+// Adiciona a classe selected ao primeiro filho da ul
+let ulFirstChild = document.querySelector('#color-palette-list>li');
+ulFirstChild.className = 'color selected';
 // Cria o pixel board
 function creatPixelBoard() {
     const pixelBoardSize = 5;
     const pixelBoard = document.getElementById('pixel-board');
     for (let index = 0; index < pixelBoardSize; index += 1) {
         const lineList = document.createElement('ul');
-        lineList.style.fontSize = '0';
         pixelBoard.appendChild(lineList);
         for (let indexItem = 0; indexItem < pixelBoardSize; indexItem += 1) {
             const pixel = document.createElement('li');
             pixel.className = 'pixel';
-            pixel.style.width = '40px';
-            pixel.style.height = '40px';
-            pixel.style.border = 'solid 1px #000';
-            pixel.style.display = 'inline-block';
-            pixel.style.background = '#fff';
             lineList.appendChild(pixel);
         }
     }
 }creatPixelBoard();
-
-// Seleciona cor
-function selectedColor() {
-    let selectedColor = document.querySelector('.color');
-    console.log(selectedColor);
-    selectedColor.className = 'color selected';
-}selectedColor();
+const palettes = document.querySelectorAll('.color');
+// Remove e adiciona a classe Selected
+function inputOutputSelected() {
+    for (let index = 0; index < palettes.length; index += 1) {
+        palettes[index].addEventListener('click',function(event){
+            for (let i = 0; i < palettes.length; i += 1){
+                palettes[i].classList = 'color';
+            }
+            event.target.classList.toggle('selected');
+            console.log(palettes[index]);// Testando 
+        });
+    }
+}inputOutputSelected();
