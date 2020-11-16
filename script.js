8c0dd3af92f32b77b33dff02cf98cc9971fe91d9
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-11-16 17:39:53 -0300

    Testando funções

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const quadroDePixel = document.getElementById('pixel-board');
+let quadroDePixel = document.getElementById('pixel-board');
 
-
-function popolaQuadroDePixels(){
+// Gera os pixels do quadro principal
+function populaQuadroDePixels(){
     for(let indiceArrayA = 0; indiceArrayA < 5; indiceArrayA += 1){
         let criaPixel = document.createElement('div');
         quadroDePixel.appendChild(criaPixel);
@@ -13,5 +13,53 @@ function popolaQuadroDePixels(){
         }
     }
 }
- let cor = document.getElementsByClassName('color');
- popolaQuadroDePixels();
+let cor = document.getElementsByClassName('color');
+populaQuadroDePixels();
+
+// Torna o preto como cor pre-selecionada no quadro de cores
+function selecionandoCorPreta(){
+    cor[0].classList.add('selected');
+}
+
+selecionandoCorPreta();
+
+function mudaCor(param){
+    for(let indiceArrayC = 0; indiceArrayC < cor.length; indiceArrayC += 1){
+        cor[indiceArrayC].classList.remove('selected');
+        param.target.classList.add('select');
+        cor[indiceArrayC] = param.target;
+    }
+}
+
+function clique(){
+    for(const cor1 of cor){
+        cor1.addEventListener('click', mudaCor);
+    }
+}
+
+clique();
+
+function escolhendoACor(){
+    const corSelecionada = document.getElementsByClassName('pixel');
+    for(let indiceArrayD = 0; indiceArrayD < corSelecionada.length; indiceArrayD += 1){
+        corSelecionada[indiceArrayD].addEventListener('click', function (param){
+        let pegaCor = window.getComputedStyle(
+            document.querySelector('.color.selected')
+        ).backgroundColor;
+        param.target.style.backgroundColor = pegaCor;
+        });
+    }
+}
+
+escolhendoACor();
+
+function resetaPixel(){
+    let botao = document.getElementById('clear-board');
+    botao.addEventListener('click', () => {
+        let selecionaCor = document.getElementsByClassName('pixel');
+        for(let pixel of selecionaCor){
+            pixel.style.backgroundColor = 'white';
+        }
+    })
+}
+resetaPixel();
