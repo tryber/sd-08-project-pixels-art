4e74aab800150b3c58d754d3e224d2fae9a7f781
Bruno <brunocmoraes97@gmail.com>
2020-11-16 19:22:06 -0300

    Requisito 7. Preenche os pixeis com o a cor selecionada.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let colorPalette = document.querySelector('#color-palette');
-let pallets = document.querySelectorAll('.color')
+let pallets = document.querySelectorAll('.color');
+let pixelBoard = document.querySelector('#pixel-board')
+let colorChoose = document.querySelector('.selected')
+document.querySelector('#colorBlack').style.backgroundColor = 'black';
+document.querySelector('#colorBlue').style.backgroundColor = '#36467C';
+document.querySelector('#colorGray').style.backgroundColor = '#B0B0B0';
+document.querySelector('#colorBrown').style.backgroundColor = '#CCBAAA';
+
+
 
 function selectedColor(event) {
     for(let index = 0; index < pallets.length; index +=1){
@@ -10,3 +18,20 @@ function selectedColor(event) {
 }
 colorPalette.addEventListener('click', selectedColor);
 
+function colorize (event) {
+    let pickColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = pickColor;
+    console.log(pickColor);
+}
+
+pixelBoard.addEventListener('click', colorize);
+
+
+
+
+/*
+#36467C
+#B0B0B0
+#CCBAAA
+
+*/
