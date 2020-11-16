5c4b7522aadd785020f8082169845d960f957d08
Bruno <brunocmoraes97@gmail.com>
2020-11-16 19:48:55 -0300

    Requisito 9. Adiciona botão de limpar as cores pintadas no quadro.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let colorPalette = document.querySelector('#color-palette');
 let pallets = document.querySelectorAll('.color');
 let pixelBoard = document.querySelector('#pixel-board')
 let colorChoose = document.querySelector('.selected')
+let button = document.querySelector('#clear-board');
+let pixel = document.querySelectorAll('.pixel') ;
+
 document.querySelector('#colorBlack').style.backgroundColor = 'black';
 document.querySelector('#colorBlue').style.backgroundColor = '#36467C';
 document.querySelector('#colorGray').style.backgroundColor = '#B0B0B0';
 document.querySelector('#colorBrown').style.backgroundColor = '#CCBAAA';
-
-
 
 function selectedColor(event) {
     for(let index = 0; index < pallets.length; index +=1){
@@ -27,11 +28,9 @@ function colorize (event) {
 pixelBoard.addEventListener('click', colorize);
 
 
-
-
-/*
-#36467C
-#B0B0B0
-#CCBAAA
-
-*/
+function clear () {
+    for(let index = 0; index < pixel.length; index +=1){
+    pixel[index].style.backgroundColor = 'white';
+    }  
+}
+button.addEventListener('click', clear)
