99febbcc965c7a8415a7b4b0004ac3d237ebe6d9
Maurice Alves <maurice.estevam@gmail.com>
2020-11-15 22:18:01 -0300

    Requisito - 5. Cada elemento do quadro de pixels deve possuir 40 pixels de largura e 40 pixels de altura e ser delimitado por uma borda preta de 1 pixel.

--- script.js
+++ script.js
@@ -4,22 +4,17 @@ let corAzul = document.getElementsByClassName('color')[2]; corAzul.style.backgro
 let corVerde = document.getElementsByClassName('color')[3]; corVerde.style.background = 'green';
 
 let tbl = document.getElementById('pixel-board');
-/*var col = document.createElement('tr');
-tbl.appendChild(col);*/
-
 function criandoElemento(){
     for(let index = 0;index < 5; index++){
         let col = document.createElement('tr');
         col.classList.add('pixel');
-        col.style.backgroundColor='white'
+        col.style.backgroundColor='white';
         tbl.appendChild(col);
-        col.innerHTML = "x";
-       for(let i = 0; i < 4; i++){
+       for(let i = 0; i < 5; i++){
             let linha = document.createElement('td');
             linha.classList.add('pixel');
-            linha.style.backgroundColor='white'
+            linha.style.backgroundColor='white';
             col.appendChild(linha);
-            linha.innerHTML = "x";
         }
     }
 }
