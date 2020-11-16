54de2bf79c8eec82fb03b9aadd5c4bf9c9561c6f
maym4y <jennim.almeida@gmail.com>
2020-11-15 22:08:38 -0300

    Corrigido tabela de pixels

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function criaPixelBoard(number) {
     for (let linha = 0; linha < number; linha +=1) {
-        let row = document.createElement('div');
-        row.className = 'tr';
-        let board = document.querySelector('#pixel-board');
-        board.appendChild(row);
+        const board = document.querySelector('#pixel-board');
+        let line = document.createElement('div');
+        board.appendChild(line);
         for (let col = 0; col < number; col += 1) {
             const pixels = document.createElement('div');
             pixels.className = 'pixel';
-            row.appendChild(pixels);
+            line.appendChild(pixels);
         }
     }
-};
+}
 
 window.onload = criaPixelBoard(5);
 
@@ -25,7 +24,5 @@ function colorirPixel(e) {
         let cor = document.querySelector('.selected').id;
         e.target.id = cor;
     }
-
-
     
 }
