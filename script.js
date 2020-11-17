d2fb2c8d4247388a07dd042fa4698a3a2db6794b
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-17 16:05:37 -0300

    ajustes na tabela

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ selecionarCor.addEventListener('click', function (evento) {
 	if (evento.target.classList.contains('color')) {
 		evento.target.classList.add('selected');
 
+	}else if (evento.target.classList.contains('selected')) {
+		evento.target.classList.remove('selected')
 	}
+		
+	
 	console.log(evento.target);
 
 })
