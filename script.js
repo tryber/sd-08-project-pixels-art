dca8afcdc275e2e37a170538a157b23057f1eac5
ok <fabnhm@hotmail.com>
2020-11-17 11:42:45 -0300

    Feito correção requisito 2

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let colorPalette = document.getElementById('color-palette');
 let colors = ['black', 'red', 'purple', 'green'];
 
 
-for (let index =0; index < 4; index += 1){
-    let createBoxColors =document.createElement('li');
-    createBoxColors.classeName = 'color';
+for (let index = 0; index < 4; index += 1){
+    let createBoxColors = document.createElement('li');
+    createBoxColors.className = 'color';
     createBoxColors.style.backgroundColor = colors[index];
     colorPalette.appendChild(createBoxColors);
-}
+  }
