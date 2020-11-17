4b18d6526454ec482f29d330ec869f0cb7ec1f89
Daniel <marcio.daniel@msn.com>
2020-11-16 21:00:12 -0300

    adicionando 2 bonus

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-const cores = ['black', 'red', ' blue', 'green'];
-const divColorPalette = document.getElementById("color-palette");
+let colors = ["black", "red", "yellow", "blue"];
+let tamanhoDoQuadro = document.getElementById('board-sizer');
+let botaGerarQuadrados = document.getElementById('generate-board');
+let paletaDeCores = document.querySelector("#color-palette");
+let pixelBoard = document.querySelector("#pixel-board");
+let cor = "black";
+let limparPixels = document.querySelector("#clear-board");
+botaGerarQuadrados.addEventListener('click', gerarTabela);
+limparPixels.addEventListener("click", limpaTabela);
+paletaDeCores.addEventListener("click", alteraSelected);
+pixelBoard.addEventListener("click", pintaPixel);
 
-for (let i in cores) {
-  let diviCores = document.createElement('div');
-  diviCores.style = `background-color:${cores[i]};`;
-  diviCores.className = 'color';
-  if (i == 0) {
-    diviCores.classList.add('selected');
+function geraPaletaDeCores() { // Função que gera paleta de cores
+  for (let index = 0; index < colors.length; index += 1) {
+    let divPalet = document.createElement("div");
+    paletaDeCores.appendChild(divPalet);
+    divPalet.className = "color";
+    divPalet.style.backgroundColor = colors[index];
+    if (index == 0) {
+      divPalet.classList.add("selected");
+    }
   }
-  divColorPalette.appendChild(diviCores);
 }
-
-const pixelBoard = document.getElementById('pixel-board');
-
-for (let i = 1; i <= 25; i++) {
-  const divSpan = document.createElement('div');
-  divSpan.className = 'pixel';
-  pixelBoard.appendChild(divSpan);
-}
-
-divColorPalette.addEventListener('click', pegarCor);
-pixelBoard.addEventListener('click', pintarDivis);
-
-let botao = document.getElementById('clear-board');
-botao.addEventListener('click', limparDivs);
-
-let corDaDiv = 'black';
-let divAnterior =document.getElementsByClassName('selected').item(0);
-
-function pegarCor(event) {
-  divAnterior.classList.remove('selected');
-  const evento = event.target;
-  corDaDiv = evento.style.backgroundColor;
-  evento.classList.add('selected');
-  divAnterior=evento;
+geraPaletaDeCores();
+function gerarTabela() {
+  let numeroDeQuadros = tamanhoDoQuadro.value;
+  if (tamanhoDoQuadro.value === '') {
+    numeroDeQuadros = 5;
   }
-
-function pintarDivis(event) {
-  const evento = event.target;
-  evento.style.backgroundColor = corDaDiv;
+  if (numeroDeQuadros < 5) {
+    numeroDeQuadros = 5;
+  }
+  if (numeroDeQuadros > 50) {
+    numeroDeQuadros = 50;
+  }
+  pixelBoard.innerHTML = '';
+  for (let index = 0; index < numeroDeQuadros; index++) {
+    let maePixexl = document.createElement('div');
+    pixelBoard.appendChild(maePixexl);
+    for (let j = 0; j < numeroDeQuadros; j++) {
+      let divTable = document.createElement("div");
+      maePixexl.appendChild(divTable);
+      divTable.className = "pixel";
+      divTable.style.backgroundColor = "white";
+    }
+  }
 }
+gerarTabela();
 
-function limparDivs (){
-  for(let i = 0; i < 25; i++ ) {
-    let limparDiv = document.getElementsByClassName('pixel');
-    limparDiv[i].style.backgroundColor= 'white';
-   }
-
-
+function alteraSelected(event) {
+  let removederSeletect = document.getElementsByClassName("selected");
+  for (let i = 0; i < removederSeletect.length; i++) {
+    removederSeletect[i].classList.remove("selected");
+  }
+  let evento = event.target;
+  cor = evento.style.backgroundColor;
+  evento.classList.add("selected");
+}
+function pintaPixel(event) {
+  let evento = event.target;
+  evento.style.backgroundColor = cor;
+}
+function limpaTabela() {
+  let cadaPixel = document.getElementsByClassName('pixel');
+  for (let i = 0; i < cadaPixel.length; i++) {
+    cadaPixel[i].style.backgroundColor = "white";
+  }
 }
