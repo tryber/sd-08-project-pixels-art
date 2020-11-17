b4be18ba4af3d5fd56bba6844c0cb5edc38eda7c
Víctor Cançado <victor.cancado@gmail.com>
2020-11-17 03:34:55 -0300

    Atualiza botão

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
 
 //6 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.
+
+function returnWhite() {
+    let cleanBoard = document.getElementsByClassName("pixel");
+    cleanBoard.style.background-color = "white";
+}
+
 window.onload = selectBlack;
 function selectBlack() {
   
 }
+
