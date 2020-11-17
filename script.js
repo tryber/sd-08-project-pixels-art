b99691a8943a53e918098ba51b6396d8248a5133
ok <fabnhm@hotmail.com>
2020-11-17 11:07:02 -0300

    Feito o Requisito 2

---
+++ script.js
@@ -0,0 +1,10 @@
+let colorPalette = document.getElementById('color-palette');
+let colors = ['black', 'red', 'purple', 'green'];
+
+
+for (let index =0; index < 4; index += 1){
+    let createBoxColors =document.createElement('li');
+    createBoxColors.classeName = 'color';
+    createBoxColors.style.backgroundColor = colors[index];
+    colorPalette.appendChild(createBoxColors);
+}
