c1e6072a485e9befb937f993c7b8ee7040b9acb4
Víctor Cançado <victor.cancado@gmail.com>
2020-11-17 12:46:17 -0300

    Botão atualizado de forma definitiva

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 
 //6 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.
 
-// function returnWhite() {
-    
-// }
+function createButton () {
+   let clearButton = document.getElementById("clear-board");
+   
+   clearButton.addEventListener("click", newEvent);
+}
+createButton();
+
+function newEvent () {
+   let enventHappening = document.querySelectorAll(".pixel");
+   for(let index = 0; index < enventHappening.length; index += 1) {
+    enventHappening[index].style.backgroundColor = "white";
+   }
+   
+}
 
 window.onload = selectBlack;
 function selectBlack() {
