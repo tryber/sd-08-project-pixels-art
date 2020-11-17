6f0b65b5766a6971bef628ba0a839dffb5fd7c87
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-17 18:20:42 -0300

    Pixel board

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function clearAllPixels() {
-  document.getElementsByClassName('pixel').style.backgroundColor = "white";
+function createPixelBoard() {
+    
 }
+
+function clearAllPixels() {
+    let arrayPixels = document.getElementsByClassName('pixel');
+    let clearPixels = [];
+    for (let index = 0; index < 25; index += 1) {
+        clearPixels.push(arrayPixels[index]);
+        clearPixels.style.backgroundColor = 'white';
+    }
+}
+
     
