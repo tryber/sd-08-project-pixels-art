e4ac428a458d6c0324b0c5a17a1bd08222351b44
ok <fabnhm@hotmail.com>
2020-11-17 13:29:17 -0300

    Feito O requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let colorPalette = document.getElementById('color-palette');
 let colors = ['black', 'red', 'purple', 'green'];
+let pixelBoard = document.getElementById('pixel-board');
+
 
 
 for (let index = 0; index < 4; index += 1){
@@ -8,3 +10,15 @@ for (let index = 0; index < 4; index += 1){
     createBoxColors.style.backgroundColor = colors[index];
     colorPalette.appendChild(createBoxColors);
   }
+  for (let index = 0; index < 5; index += 1) {
+    let linePalette = document.createElement('div');
+    linePalette.className = 'line';
+    pixelBoard.appendChild(linePalette);
+    
+    for (let index = 0; index < 5; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      linePalette.appendChild(pixel);
+    }
+  }
+  
