253b26b2e071e8f69ab50c326e50af56b3d0f897
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-11-17 15:10:42 -0300

    adicionando o botao

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const corDeFundo = ["black", "green", "orange", "yellow"];
+const paints = document.getElementsByClassName("color");
+
+
 //Requisito 2 e 3
 function createDivs() {
     let palette = document.getElementById("color-palette");
     
-    for (let index = 0; index < 4; index += 1) {
+    for (let index = 0; index < corDeFundo.length; index += 1) {
         let div = document.createElement("div");
         div.className = "color";
         palette.appendChild(div);
@@ -11,9 +15,6 @@ function createDivs() {
 createDivs();
 
 function colors (){
-    let corDeFundo = ["black", "green", "orange", "yellow"];
-    let paints = document.querySelectorAll(".color");
-    
     for (let c = 0; c < corDeFundo.length; c += 1){
         paints[c].style.backgroundColor = corDeFundo[c];
     }
@@ -24,24 +25,38 @@ colors ();
 //Requisito 4 e 5
 function createBoard() {
     let board = document.getElementById("pixel-board");
-    
 
     for (let index = 0; index < 5; index += 1) {
         let div = document.createElement("div");
         div.className = "pixel";
         board.appendChild(div);
-
-        for(let index = 0; index < 5; index += 1) {
-            let div = document.createElement("div");
-            div.className = "pixel-coloumn";
-            board.appendChild(div);
-        }
+        
+    for(let index = 0; index < 5; index += 1) {
+        let div = document.createElement("div");
+        div.className = "pixel-coloumn";
+        board.appendChild(div);
+    }    
   }  
 }
 createBoard();
 
 //Requisito 6
 function mousePaint(){
-    let select = document.querySelectorAll(".color")[0];
-    select.className = "selected";
+    paints[0].classList.add("selected");
 }
+mousePaint();
+
+//Requisito 7
+
+
+
+
+//Requisito 8
+
+
+
+//Requisito 9
+function createButton (){
+    let btn = document.getElementById("button");
+}
+createButton ();
