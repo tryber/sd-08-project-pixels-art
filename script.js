864b3fba9f0e406ba8f8ef0fae2d1381f81941ed
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-11-16 22:10:46 -0300

    ajustes refarente ao code climate

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Variáveis
-const boxPalette = document.getElementsByClassName("color"); //elementos com class='color'
-const colors = ["black", "red", "orange", "yellow"]; // array de cores
-const palette = document.getElementById("color-palette"); // seleciona o palette
-const table = document.getElementById("pixel-board"); // seleciona a table
-const clear = document.getElementById("clear-board"); // botão limpar
-const generateBoard = document.getElementById('generate-board'); // Botão de gerar os boxes
-
+const boxPalette = document.getElementsByClassName("color");
+const colors = ["black", "red", "orange", "yellow"];
+const palette = document.getElementById("color-palette");
+const table = document.getElementById("pixel-board");
+const clear = document.getElementById("clear-board");
+const generateBoard = document.getElementById("generate-board");
 
 //roda as funções quando a página é carregada
-window.onload = criateColorsPalette(boxPalette, colors),
-  firstSelectedColor(boxPalette),populaTable();
+(window.onload = criateColorsPalette(boxPalette, colors)),
+  firstSelectedColor(boxPalette),
+  populaTable();
 
 //Preenche as cores do palette
 function criateColorsPalette(boxPalette, colors) {
@@ -24,58 +24,58 @@ function firstSelectedColor(boxPalette) {
 }
 
 //Popula table no onload
-function populaTable () {
-    for (colunas = 1; colunas <= 5; colunas +=1) {
-        tr = document.createElement('tr');
-        table.appendChild(tr);
+function populaTable() {
+  for (colunas = 1; colunas <= 5; colunas += 1) {
+    tr = document.createElement("tr");
+    table.appendChild(tr);
+  }
+  let coluna = document.getElementsByTagName("tr");
+
+  for (let linhas = 0; linhas < coluna.length; linhas += 1) {
+    for (index = 1; index <= 5; index += 1) {
+      td = document.createElement("td");
+      coluna[linhas].appendChild(td);
+      td.className = "pixel";
     }
-    let coluna = document.getElementsByTagName('tr');
-    
-    for (let linhas = 0; linhas < coluna.length; linhas +=1) {
-        for (index = 1; index <= 5; index += 1) {
-            td = document.createElement('td');
-            coluna[linhas].appendChild(td);
-            td.className = 'pixel';
-        }
-    }
+  }
 }
 
 //cria as boxes da table com o tamanho setado
-generateBoard.addEventListener('click', function () {
-    table.innerHTML = '';
+generateBoard.addEventListener("click", function () {
+  table.innerHTML = "";
 
-    let size = document.getElementById('board-size').value;
-    let tr = '';
-    let td = '';
+  let size = document.getElementById("board-size").value;
+  let tr = "";
+  let td = "";
 
-    if (size === ''){
-        alert('Board inválido!');
-    } else if (size > 50) {
-        size = 50;
-    } else if(size < 5){
-        size = 5;
-    }
-        
-    for (colunas = 1; colunas <= size; colunas +=1) {
-        tr = document.createElement('tr');
-        table.appendChild(tr);
-    }
-    let coluna = document.getElementsByTagName('tr');
+  if (size === "") {
+    alert("Board inválido!");
+  } else if (size > 50) {
+    size = 50;
+  } else if (size < 5) {
+    size = 5;
+  }
 
-    for (let linhas = 0; linhas < coluna.length; linhas +=1) {
-        for (index = 1; index <= size; index += 1) {
-            td = document.createElement('td');
-            coluna[linhas].appendChild(td);
-            td.className = 'pixel';
-        }
+  for (colunas = 1; colunas <= size; colunas += 1) {
+    tr = document.createElement("tr");
+    table.appendChild(tr);
+  }
+  let coluna = document.getElementsByTagName("tr");
+
+  for (let linhas = 0; linhas < coluna.length; linhas += 1) {
+    for (index = 1; index <= size; index += 1) {
+      td = document.createElement("td");
+      coluna[linhas].appendChild(td);
+      td.className = "pixel";
     }
-        
-    document.getElementById('board-size').value = '';
-})
+  }
+
+  document.getElementById("board-size").value = "";
+});
 
 //pega a cor selecionada do palette
 palette.addEventListener("click", function clickPalette(event) {
-  let selected = document.getElementsByClassName("selected"); // cor selecionada
+  let selected = document.getElementsByClassName("selected");
   selected[0].className = "color";
 
   selectColor = event.target;
@@ -84,7 +84,7 @@ palette.addEventListener("click", function clickPalette(event) {
 
 //Pinta os boxes da table
 table.addEventListener("click", function clickTable(event) {
-  let selected = document.getElementsByClassName("selected"); // cor selecionada
+  let selected = document.getElementsByClassName("selected");
   if (event.target.className == "pixel") {
     event.target.style.backgroundColor = selected[0].style.backgroundColor;
     event.target.className = "pixel";
@@ -99,4 +99,3 @@ clear.addEventListener("click", function () {
     boxes[index].style.backgroundColor = "white";
   }
 });
-
