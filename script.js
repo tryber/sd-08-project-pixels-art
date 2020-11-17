e3ef3cae1ea22cb73d8ad9771c308d99d3a0159d
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-16 23:07:25 -0300

    color palettes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let cores = document.querySelectorAll('.color')
 
 
-let selecionarCor = document.getElementById ('color-palette')
-    selecionarCor.addEventListener('click', trocaCor)
+let selecionarCor = document.getElementById('color-palette')
+selecionarCor.addEventListener('click', function (evento) {
+	if (evento.target.classList.contains('color')) {
+		evento.target.classList.add('selected')
+
+	}
+	console.log(evento.target)
+
+})
 
 // let antigoSelected = document.querySelector('.selected')
-
-function trocaCor(evento) {
-    if (evento.target.className == 'color') {
-        evento.target.className = 'color selected'
-    }
-}
