79673de3c67d34abcb98e95e7c817bd4e3b0b428
Thais Quintela <thais.s.quintela@gmail.com>
2020-11-17 19:05:42 -0300

    Requisito 9 novamente

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
+let boardSize = 5;
+
 function createPixels() {
-  let boardSize = 5;
-  let pixelBoard = document.querySelector('#pixel-board');
+  const pixelBoard = document.querySelector('#pixel-board');
 
   for (let index = 0; index < boardSize; index += 1) {
     for (let counter = 0; counter < boardSize; counter += 1) {
-      let pixel = index;
-      let pixelDiv = document.createElement('div');
+      const pixel = index;
+      const pixelDiv = document.createElement('div');
       pixelDiv.className = 'pixel';
 
       pixelBoard.appendChild(pixelDiv);
     }
-    
   }
 }
 
 createPixels();
 
 function selectColor() {
-  
+
 }
 
-let clickButton = document.getElementById('clear-board');
-clickButton.addEventListener('click', clearBoard)
-
 function clearBoard() {
-  for (let counter = 0; counter < boardSize * boardSize; counter += 1) {
-    let turnWhite = document.getElementsByClassName('pixel')[index];
+  for (let index = 0; index < boardSize * boardSize; index += 1) {
+    const turnWhite = document.getElementsByClassName('pixel')[index];
     turnWhite.style.backgroundColor = 'white';
   }
 }
+
+const clickButton = document.getElementById('clear-board');
+clickButton.addEventListener('click', clearBoard);
