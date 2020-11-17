dbfb00a7313d15e2fa169993b7cb6d10f67c9762
Thais Quintela <thais.s.quintela@gmail.com>
2020-11-17 17:06:56 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,17 @@
+function createPixels() {
+  let boardSize = 5;
+  let pixelBoard = document.querySelector('#pixel-board');
+
+  for (let index = 0; index < boardSize; index += 1) {
+    for (let counter = 0; counter < boardSize; counter += 1) {
+      let pixel = index;
+      let pixelDiv = document.createElement('div');
+      pixelDiv.className = 'pixel';
+
+      pixelBoard.appendChild(pixelDiv);
+    }
+    
+  }
+}
+
+createPixels();
