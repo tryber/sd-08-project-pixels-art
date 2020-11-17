c5e6744733d63dfcb7c75444b6ea33470e4f1fc3
Bruno <brunocmoraes97@gmail.com>
2020-11-17 20:38:53 -0300

    tentativa de corrigir tudo kk

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let colorPalette = document.querySelector('#color-palette');
 let pallets = document.querySelectorAll('.color');
-let pixelBoard = document.querySelector('#pixel-board')
-let colorChoose = document.querySelector('.selected')
+let pixelBoard = document.querySelector('#pixel-board');
+let colorChoose = document.querySelector('.selected');
 let button = document.querySelector('#clear-board');
-let pixel = document.querySelectorAll('.pixel') ;
+let pixel = document.querySelectorAll('.pixel');
 
 document.querySelector('#colorBlack').style.backgroundColor = 'black';
 document.querySelector('#colorBlue').style.backgroundColor = '#36467C';
@@ -11,25 +11,19 @@ document.querySelector('#colorGray').style.backgroundColor = '#B0B0B0';
 document.querySelector('#colorBrown').style.backgroundColor = '#CCBAAA';
 
 function createPixel () {
-    for(let index = 0; index < 5; index +=1){
-        let row = document.createElement('div');
-        row.className = 'pixel row';
-        pixelBoard.appendChild(row);
-        for(let index2 = 0; index2 < 5; index2 +=1){
-            let column = document.createElement('div');
-            column.className = 'pixel column'
-            row.appendChild(column);
-        }
-    }
-    
+    for(let index = 0; index < 25; index +=1){
+     let row = document.createElement('div');
+     row.className = 'pixel';
+     pixelBoard.appendChild(row);
+    } 
 }
 createPixel()
 
 function selectedColor (event) {
-    for(let index = 0; index < pallets.length; index +=1){
+    for (let index = 0; index < pallets.length; index +=1){
         pallets[index].className = 'color';
     }
-     event.target.className = 'color selected'
+    event.target.className = 'color selected';
 
 }
 colorPalette.addEventListener('click', selectedColor);
@@ -44,7 +38,7 @@ pixelBoard.addEventListener('click', colorize);
 
 function clear () {
     let pixel = document.querySelectorAll('.pixel');
-    for(let index = 0; index < pixel.length; index +=1){
+    for (let index = 0; index < pixel.length; index +=1){
         pixel[index].style.backgroundColor = 'white';
     }
 }
