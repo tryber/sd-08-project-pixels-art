1c0680861e28338204a2f1d9088da29c59707f29
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2020-11-17 20:41:21 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ function createDivs() {
   const arrayColor = ["black", "blue", "gray", "red"];
   const palletDiv = document.getElementById("color-palette");
   for (let index = 0; index < arrayColor.length; index += 1) {
-    let divColor = arrayColor[index];
     const myDivs = document.createElement("div");
+    myDivs.style.backgroundColor = arrayColor[index]
     palletDiv.appendChild(myDivs);
     myDivs.className = "color";
-    myDivs.style.backgroundColor = divColor;
   }
 }
 createDivs();
+
+function createBoxPixels() {
+  const boxPixels = document.getElementById("pixel-board");
+  for (let colun = 0; colun < 5; colun += 1) {
+    const createPixel = document.createElement("div");
+    boxPixels.appendChild(createPixel);
+    for (let line = 0; line < 5; line += 1) {
+      const createPixel = document.createElement("div");
+      createPixel.className = "pixel";
+      boxPixels.appendChild(createPixel);
+    }
+  }
+}
+createBoxPixels();
