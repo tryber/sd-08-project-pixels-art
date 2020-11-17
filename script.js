7eab66630f162154cae95860cd4f9d6ca3fd8209
Víctor Cançado <victor.cancado@gmail.com>
2020-11-17 16:28:33 -0300

    Novas atualizações gerais

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+// let divList = document.getElementById("color-palette");
+// for(let index = 0; index < 4; index += 1) {
+//    let newDivs = document.createElement("div");
+//    newDivs[index];
+//    divList.appendChild(newDivs);
+//  } 
+//  document.querySelectorAll[0]className = "color black";
+//  document.querySelectorAll[1]className = "color red";
+//  document.querySelectorAll[2]className = "color blue";
+//  document.querySelectorAll[3]className = "color green";
 
 
+
+window.onload = selectBlack;
+function selectBlack() {
+   let selectBlackPalette = document.getElementById("black");
+   selectBlackPalette.className = "color selected"
+}
+
+// function createBlackButton () {
+//    let blackButton = document.getElementById("black");
+//     blackButton.addEventListener("click", newBlackButton);
+// }
+
+// function newBlackButton () {
+//    let enventHappening = document.querySelectorAll(".pixel");
+//    for(let index = 0; index < enventHappening.length; index += 1) {
+//     enventHappening[index].style.backgroundColor = "black"
+// }
 
 
 
@@ -17,10 +44,5 @@ function newEvent () {
    }
    
 }
-//6 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.
-window.onload = selectBlack;
-function selectBlack() {
-   let selectBlackPalette = document.getElementById("black");
-   selectBlackPalette.className = "color selected"
-}
+
 
