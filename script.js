332007f9f4eea119520c0343c71ae93b731ed7e0
Lucas Neiva <lucassneiva.code@gmail.com>
2020-11-17 22:30:08 -0300

    add geral correction

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const colors = ["black", "green", "purple", "yellow"];
 function createDivs() {
     let newPalette = document.getElementById('color-palette');
-    for (i = 0; i < colors.length; i += 1) {
-        let color = colors[i];
+    for (let i = 0; i < colors.length; i += 1) {
         let newDivs = document.createElement("div");
+        if (i === 0) {
+            newDivs.className = "selected";
+        }
         newDivs.className = "color";
-        newDivs.style.border = "1px solid";
-        newDivs.style.backgroundColor = color;
+        newDivs.style.border = "1px solid black";
+        newDivs.style.backgroundColor = colors[i];
         newPalette.appendChild(newDivs)
-        colors[0].className = "selected";
     }
 }
 createDivs();
@@ -18,7 +19,7 @@ function createPixels() {
     let pixelBoard = document.getElementById('pixel-board');
     for (let index = 0; index < pixelsLength; index += 1) {
         let line = document.createElement("tr");
-        line.className = `linha${i}`;
+        line.className = "lines";
         pixelBoard.appendChild(line)
          for (let i = 0; i < pixelsLength; i += 1) {
             let pixel = document.createElement("td");
