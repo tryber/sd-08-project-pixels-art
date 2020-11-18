ac4d248dde1ba66b002453cb374d215c33666cc7
Victor Moraes <moraesvictorgk@gmail.com>
2020-11-18 02:05:11 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let cores = document.querySelectorAll('.color');
-
-
 let selecionarCor = document.getElementById('color-palette');
+let cor = document.getElementById('black').style.backgroundColor
+
+// requisito 7
 selecionarCor.addEventListener('click', function (evento) {
 	if (evento.target.classList.contains('color')) {
 		evento.target.classList.add('selected');
-
-	}else if (evento.target.classList.contains('selected')) {
-		evento.target.classList.remove('selected')
 	}
-		
-	
 	console.log(evento.target);
-
 })
 
-// let antigoSelected = document.querySelector('.selected')
+let pixel = document.getElementById('pixel-board');
+
+//Requisito 8
+pixel.addEventListener('click', function (event) {
+	if (event.target.classList.contains('pixel')) {
+		event.target.classList.add('selected')
+	}
+})
+//Requisito 10 
+let button = document.getElementById('clear-board');
+let quadroPixel = document.getElementsByClassName('pixel')
+button.addEventListener('click', function(){
+	for(let i = 0; i < quadroPixel.length; i += 1) {
+		if (quadroPixel[i].style.backgroundColor != "white") {
+			quadroPixel[i].style.backgroundColor = "white"
+		}
+	}
+	
+	
+})
