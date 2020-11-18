190a7e98c79e8ea461e72a198eb4ebd7fc5635ad
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-18 12:30:19 -0300

    More fixes to clear board

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 let getColor = document.getElementsByClassName('color');
 
 function firstColor(){
-    getColor[0].classList.add('selected');
+  getColor[0].classList.add('selected');
 }
 firstColor();
 
-function changeColor(event){
-    for (let indice = 0; indice < getColor.length; indice += 1){
-        getColor[indice].classList.remove('selected');
-        event.target.classList.add('selected');
-        getColor[indice] = event.target;
-    }
-}
-
-function clickChangeColor(){
-    for (const colors of getColor) {
-        colors.addEventListener('click', changeColor);
-    }
-}
-
-
-
 let button = document.getElementById('clear-board');
-button.addEventListener('click', function(){
-    let arrayPixels = document.getElementsByClassName('pixel');
-    for (let indexClear = 0; indexClear < arrayPixels.length; indexClear += 1){
-        arrayPixels[indexClear].style.backgroundColor = 'white';
-    }
+button.addEventListener('click', function (){
+  let arrayPixels = document.getElementsByClassName('pixel');
+  for (let indexClear = 0; indexClear < arrayPixels.length; indexClear += 1){
+    arrayPixels[indexClear].style.backgroundColor = '#ffffff';
+  }
 })
