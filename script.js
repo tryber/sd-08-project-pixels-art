2192b244b24b13bfe57081bd2d3565f7f1c5af10
William Vieira <william.marangon@gmail.com>
2020-11-18 09:01:35 -0300

    ex 02-05 tt 01

---
+++ script.js
@@ -0,0 +1,35 @@
+/*function criaDivs() {
+    let paleta = document.getElementById ("color-palette")
+    let cores  = ["black", "blue", "red", "green"]
+    
+        for (let i = 0; i < cores.length; i += 1) {
+            let cor =cores[i]
+            let box =document.createElement("div")
+            box.addEventListener("click", function() {
+                box.classNAme = "color selected"
+            })
+            box.className = "color"
+            box.style
+        }
+
+}*/
+
+let pixel = document.querySelector('#pixel-board')
+
+function criaPixel() {
+    for (let coluna = 0; coluna < 5; coluna += 1) {
+
+        for (let linha = 0; linha < 5; linha += 1) {
+            let px = document.createElement('div')
+            px.className = 'pixel'
+            pixel.appendChild(px)
+        }
+    }
+} 
+
+criaPixel()
+
+window.onload = function() {
+    document.getElementById('black').classList.add('selected')
+}
+
