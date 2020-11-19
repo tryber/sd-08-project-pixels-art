5c2ec82f55aa499a909d79acfe0d8d020107c31e
Alvaro Braga <alvaro.braga.job@gmail.com>
2020-11-18 23:57:51 -0300

    Fixing clear button

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let getColor = document.getElementsByClassName('color');
+let allPixels = document.getElementsByClassName('pixel');
 
 function firstColor(){
   getColor[0].classList.add('selected');
 }
 firstColor();
 
-function addEventSelected (){
-    for (let index = 0; index < getColor.length; index += 1) {
-        getColor[index].addEventListener('click', eventSelected);
-    }
+function addEventSelected(){
+  for (let index = 0; index < getColor.length; index += 1) {
+    getColor[index].addEventListener('click', eventSelected);
+  }
 }
 addEventSelected();
 
 function eventSelected (event){
-    for (let i = 0; i < getColor.length; i += 1) {
-        getColor[i].classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+  let elementSelected = document.querySelector('.selected');
+  elementSelected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
+
+function paintingPixels(){
+  for (let i = 0; i < allPixels.length; i += 1){
+    allPixels[i].addEventListener('click', function (event){
+
+    });
+  }
+}
+paintingPixels();
+
 
 
 function clearButton(){
@@ -26,6 +36,7 @@ let arrayPixels = document.getElementsByClassName('pixel');
     arrayPixels[indexClear].style.backgroundColor = 'white';
   }
 }
+clearButton();
 
 let button = document.getElementById('clear-board');
 button.addEventListener('click', clearButton);
