77bdf951d97bce550611bae411cceb8115b17590
Marconi <marconieng@gmail.com>
2020-11-22 14:51:59 -0300

    Corrige codeclimate no script

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//Requisito 1,2
+//Requisito 1,2//
 function criaDivs() {
-let paleta = document.getElementById("color-palette");
+  let paleta = document.getElementById('color-palette');
 
-let cores = ["black", "blue", "red", "green"];
+  let cores = ['black', 'blue', 'red', 'green'];
 
 
-  for(let index = 0; index < cores.length; index += 1) {
+  for (let index = 0; index < cores.length; index += 1) {
     let cor = cores[index];
-    let box = document.createElement("div");
-    box.addEventListener("click", function(){
-        box.className = "color selected";
+    let box = document.createElement('div');
+    box.addEventListener('click', function(){
+        box.className = 'color selected';
       })
-    box.className = "color";
+    box.className = 'color';
     box.style.backgroundColor = cor;
     paleta.appendChild(box);
   }
@@ -21,15 +21,15 @@ criaDivs();
 
 
 function criaQuadro() {
-  for(let index = 0; index < 5; index += 1) {
-    let pixel = document.createElement("tr");
-    document.querySelector("#pixel-board").appendChild(pixel);
+  for (let index = 0; index < 5; index += 1) {
+    let pixel = document.createElement('tr');
+    document.querySelector('#pixel-board').appendChild(pixel);
     pixel.className = `linha${index}`;
 
-    for(let i = 0; i < 5; i += 1) {
-        let coluna = document.createElement("td");
-        document.querySelector(`.linha${index}`).appendChild(coluna);
-        coluna.className = "pixel";
+  for (let i = 0; i < 5; i += 1) {
+    let coluna = document.createElement('td');
+    document.querySelector(`.linha${index}`).appendChild(coluna);
+    coluna.className = 'pixel';
     }
   }
 }
