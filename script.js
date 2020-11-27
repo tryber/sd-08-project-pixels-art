efe592d7fe560da046bc3ec18019cc4fd89b7b4d
Wendrick <wendrick.b.c1@gmail.com>
2020-11-27 14:40:20 -0300

    Tentando mais uma vez

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ function gridCreation(size) {
 // Clear Button
 const clearButton = document.querySelector('#clear-board');
 clearButton.addEventListener('click', function () {
-  let linha = document.querySelectorAll(".tr");
-  let coluna = document.querySelectorAll(".td");
-  for (let i = 0; i < linha.length; i++) {
-      linha[i].style.backgroundColor = "white";
-      for (let j = 0; j < coluna.length; j++) {
-          coluna[j].style.backgroundColor = "white";
-      }
+  const linha = document.querySelectorAll('.tr');
+  const coluna = document.querySelectorAll('.td');
+  for (let i = 0; i < linha.length; i += 1) {
+    linha[i].style.backgroundColor = 'white';
+    for (let j = 0; j < coluna.length; j += 1) {
+      coluna[j].style.backgroundColor = 'white';
+    }
   }
 });
 
