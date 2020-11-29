d93e88c49e40c8c6366bf8cd9a6e5cb90f6cf03f
Akirakiraa <andretaki@2gmail.com>
2020-11-29 19:33:23 -0300

    using window.onload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//const pPixels = document.getElementsByClassName('color');
 const colors = ['black','blue','red','yellow'];
-
+const pixels = document.querySelectorAll('.pixel')
 //document.getElementsByClassName('color')[0].style.backgroundColor = 'red'
+
+window.onload = createPalette(colors),createBoard();
 
 function createPalette(colors){
     for (let pos = 0; pos < 4;pos++){
@@ -13,3 +14,14 @@ function createPalette(colors){
     }
 }
 
+//function clear(){ for (let div of pixels){document.getElementsByClassname("pixel-board).")}} 
+
+
+function createBoard(){
+    for (let pos = 0; pos < 25;pos++){
+        const pPixel = document.createElement('div');
+        pPixel.className = 'pixel';
+        document.getElementById("pixel-board").appendChild(pPixel);
+        
+    }
+}
