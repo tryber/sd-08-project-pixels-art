61b5afe6f5efb43aeaaf039d89cdcd0393d77b7a
Akirakiraa <andretaki@2gmail.com>
2020-12-01 11:19:04 -0300

    making by divs

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ const pixelPalette = document.getElementById('color-palette');
 const palettes = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('pixel-board');
 const colors = document.getElementsByClassName('.color');
+const vqvBtn = document.getElementById('generate-board');
+const pixels = document.getElementsByc('.pixel');
 
+
+
+function randomMaker(){
+    let cor1 =  (Math.round(Math.random()*255));
+    let cor2 =  (Math.round(Math.random()*255));
+    let cor3 =  (Math.round(Math.random()*255));
+    let rColors =  ("rgb("+cor1+", "+cor2+", "+cor3+")");
+    return rColors;
+}
+
+
+for (let pos = 1; pos < colors.length; pos++){
+    let aux = colors[pos];
+    
+    aux.style.backgroundColor = randomMaker();
+}
 
 pixelPalette.addEventListener('click',function(event) {
     if (event.target.className !== 'color selected') {
@@ -19,63 +37,3 @@ pixelBoard.addEventListener('click', function (event) {
     }
   }
 })
-
-function generatorRgb(){
-    let cor1 =  (Math.round(Math.random()*255));
-    let cor2 =  (Math.round(Math.random()*255));
-    let cor3 =  (Math.round(Math.random()*255));
-    let rColors =  ("rgb("+cor1+", "+cor2+", "+cor3+")");
-    return rColors;
-
-/* 
-const pixels = document.querySelectorAll('.pixel');
-const clearBtn = document.getElementById('clear-board');
-
-const paint = document.querySelector('.selected')
-const allPixels =document.querySelectorAll('.pixel')
-//document.getElementsByClassName('color')[0].style.backgroundColor = 'red'
-
-window.onload = createPalette(colors),createBoard();
-
-function createPalette(colors){
-    for (let pos = 0; pos < 4;pos++){
-        const pPixel = document.createElement('div');
-        pPixel.className = 'color';
-        if(colors[pos] === 'black'){
-            pPixel.className = 'color selected';
-        }
-        pPixel.style.backgroundColor = colors[pos];
-        document.getElementById("color-palette").appendChild(pPixel);
-        
-    }
-}
-
-//function clear(){ for (let div of pixels){document.getElementsByClassname("pixel-board).")}} 
-
-
-function createBoard(){
-    for (let pos = 0; pos < 25;pos++){
-        const pPixel = document.createElement('div');
-        pPixel.className = 'pixel';
-    document.getElementById("pixel-board").appendChild(pPixel); //Aqui eu tive que usar o getElementById pra localizar a tag que seri o pai ,dessa forma usei o append child pra dizer que "paletapixel" seria filha dessa
-
-        
-    }
-}
-
-clearBtn.addEventListener('click', function(){
-    allPixels.forEach((pixel)=> {
-        pixel.style.backgroundColor = 'white';
-    });
-});
-
-//seleciona paletta
-pixelPalette.addEventListener('click',(event) => {
-    colorSel.classList.toggle('selected');
-    event.target.classList.add('selected');
-});
-
-pixelBoard.addEventListener('click',(event)=>{
-    event.target.backgroundColor = colorSel.style.backgroundColor;
-})
- */
