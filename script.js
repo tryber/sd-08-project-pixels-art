d9d1b64f77c6aa2f2f29ac7b90100700c301740f
GabrielLennox <meunomeelennox@gmail.com>
2020-12-01 09:16:09 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const colors = document.getElementsByClassName('color');
- for (let index = 0; index < colors.length; index += 1) {
-     const item = colors[index];
-     item.addEventListener('click', function(event){
+const pixelBoard = document.getElementById('pixel-board');
+
+for (let index = 0; index < colors.length; index += 1) {
+    const item = colors[index];
+    item.addEventListener('click', function(event){
         const selected = document.querySelector('.selected');
         selected.className = 'color';
         event.target.className = 'color selected'; 
-     })
- }
+    })
+}
+
+pixelBoard.addEventListener('click', function(event) {
+    const selected = document.querySelector('.selected');
+    let compStyles = window.getComputedStyle(selected);
+    event.target.style.backgroundColor = compStyles.getPropertyValue('background-color');
+})
