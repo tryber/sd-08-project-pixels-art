80a61ec6f4783f58601105c412e3684d406ec11a
Rosiele David <rosieleserpa@hotmail.com>
2020-12-14 11:00:03 -0300

    requisito 2: adiciona paleta de quatro cores distintas

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ function geraCoresRandomRGB() {
   return `rgb( ${red} , ${green} , ${blue})`;
 }
 
-function geraPaletaDeCores() {
-
+function geraArrayDeCores() {
+  let cores = ["black"];
+  while (cores.length < 4) {
+    cores.push(geraCoresRandomRGB());
+  }
+  return cores;
 }
 
+function geraPaletaDeCores() {
+  const cores = geraArrayDeCores();
+  for(let index = 0; index < cores.length; index += 1) {
+    const paletaDeCores = document.createElement('div');
+    paletaDeCores.style.backgroundColor = cores[index];
+    paletaDeCores.className = 'color';
+    colorPalette.appendChild(paletaDeCores);
+  }
+}
 
+geraPaletaDeCores();
