6cc4cf69ee7c8b53e95c969098dc442aaf6f4e2a
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-16 15:06:22 -0300

    r4 e r12

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const main = document.getElementById("main");
 const paletaDeCores = document.getElementById("color-palette");
 const coresDaPaleta = document.getElementsByClassName("color");
+const red = Math.floor(Math.random() * 0);
+const green = Math.floor(Math.random() * 0);
+const blue = Math.floor(Math.random() * 0);
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
 const quadroDePixels = document.createElement("ul");
-const pixels = document.createElement("li");
 
 function criaAsCoresDaPaleta() {
-  coresDaPaleta[0].style.backgroundColor = "rgb(0,0,0)";
+  coresDaPaleta[0].style.backgroundColor = `rgb(${red},${green},${blue})`;
   for (let i = 1; i <= 3; i += 1) {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
@@ -17,8 +22,17 @@ criaAsCoresDaPaleta();
 
 function criaQuadroDePixels() {
   quadroDePixels.id = "pixel-board";
-  pixels.className = "pixel";
-  pixels.style.backgroundColor = "rgb(255,255,255)";
+
   main.appendChild(quadroDePixels);
-  quadroDePixels.appendChild(pixels);
+  for (let i = 1; i <= 5; i += 1) {
+    for (let j = 1; j <= 5; j += 1) {
+      const pixels = document.createElement("li");
+      pixels.className = "pixel";
+      pixels.style.backgroundColor = "rgb(255,255,255)";
+      quadroDePixels.appendChild(pixels);
+    }
+    const quebraDeLinha = document.createElement("br");
+    quadroDePixels.appendChild(quebraDeLinha);
+  }
 }
+criaQuadroDePixels();
