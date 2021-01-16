a48d6214b76a82e3d52f8e762c0ca646a6770e8a
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-16 17:24:26 -0300

    r10 04

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const coresDaPaleta = document.getElementsByClassName("color");
 const red = Math.floor(Math.random() * 0);
 const green = Math.floor(Math.random() * 0);
 const blue = Math.floor(Math.random() * 0);
-const r = Math.floor(Math.random() * 255);
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
+const r = Math.floor(Math.random() * 256);
+const g = Math.floor(Math.random() * 256);
+const b = Math.floor(Math.random() * 256);
 const quadroDePixels = document.createElement("ul");
 const botaoLimpar = document.getElementById("clear-board");
 
@@ -14,9 +14,9 @@ function criaAsCoresDaPaleta() {
   coresDaPaleta[0].style.backgroundColor = `rgb(${red},${green},${blue})`;
   coresDaPaleta[0].className = "color selected";
   for (let i = 1; i <= 3; i += 1) {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     coresDaPaleta[i].style.backgroundColor = `rgb(${r},${g},${b})`;
   }
 }
@@ -29,7 +29,7 @@ function criaQuadroDePixels() {
     for (let j = 1; j <= 5; j += 1) {
       const pixels = document.createElement("li");
       pixels.className = "pixel";
-      pixels.style.backgroundColor = "rgb(255,255,255)";
+      pixels.style.backgroundColor = "rgb(256,256,256)";
       quadroDePixels.appendChild(pixels);
     }
     const quebraDeLinha = document.createElement("br");
@@ -60,7 +60,7 @@ function limpaQuadroDePixels() {
   botaoLimpar.addEventListener("click", function () {
     const pixels = document.getElementsByClassName("pixel");
     for (let i = 0; i < pixels.length; i += 1) {
-      pixels[i].style.backgroundColor = "white";
+      pixels[i].style.backgroundColor = "rgb(256,256,256)";
     }
   });
 }
