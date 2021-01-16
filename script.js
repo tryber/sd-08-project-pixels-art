1a6d571a1979f6ae9f2d25458c1e07c704bdf7d8
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-16 14:28:07 -0300

    r2

---
+++ script.js
@@ -0,0 +1,15 @@
+const main = document.getElementById("main");
+const paletaDeCores = document.getElementById("color-palette");
+const coresDaPaleta = document.getElementsByClassName("color");
+
+function criaAsCoresDaPaleta() {
+  coresDaPaleta[0].style.backgroundColor = "rgb(0,0,0)";
+  coresDaPaleta[1].style.backgroundColor = "rgb(0,0,255)";
+  for (let i = 2; i <= 3; i += 1) {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    coresDaPaleta[i].style.backgroundColor = `rgb(${r},${g},${b})`;
+  }
+}
+criaAsCoresDaPaleta();
